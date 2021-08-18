a9808046958254be0c4f4362dcd0b68bf759c027
Christian Baker <christianbaker@utah.gov>
2021-08-18 16:37:27 -0600

    Added QueryExtensions.IfCondition and tests

---
+++ ATSPM/ApplicationCore/Repositories/IControllerEventLogRepository.cs
@@ -0,0 +1,58 @@
+﻿using ATSPM.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATSPM.Application.Repositories
+{
+    public interface IControllerEventLogRepository
+    {
+        double GetTmcVolume(DateTime startDate, DateTime endDate, string signalId, int phase);
+        IReadOnlyList<ControllerEventLog> GetSplitEvents(string signalId, DateTime startTime, DateTime endTime);
+
+        IReadOnlyList<ControllerEventLog> GetSignalEventsByEventCode(string signalId,DateTime startTime, DateTime endTime, int eventCode);
+
+        IReadOnlyList<ControllerEventLog> GetSignalEventsByEventCodes(string signalId,DateTime startTime, DateTime endTime, IReadOnlyList<int> eventCodes);
+
+        IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParam(string signalId,DateTime startTime, DateTime endTime, IReadOnlyList<int> eventCodes, int param);
+
+        IReadOnlyList<ControllerEventLog> GetTopEventsAfterDateByEventCodesParam(string signalId, DateTime timestamp,IReadOnlyList<int> eventCodes, int param, int top);
+
+        int GetEventCountByEventCodesParamDateTimeRange(string signalId,DateTime startTime, DateTime endTime, int startHour, int startMinute, int endHour, int endMinute,IReadOnlyList<int> eventCodes, int param);
+
+        IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParamDateTimeRange(string signalId,DateTime startTime, DateTime endTime, int startHour, int startMinute, int endHour, int endMinute,IReadOnlyList<int> eventCodes, int param);
+
+        IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParamWithOffsetAndLatencyCorrection(string signalId,DateTime startTime, DateTime endTime, IReadOnlyList<int> eventCodes, int param, double offset,double latencyCorrection);
+
+        IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParamWithLatencyCorrection(string signalId,DateTime startTime, DateTime endTime, IReadOnlyList<int> eventCodes, int param,double latencyCorrection);
+
+        ControllerEventLog GetFirstEventBeforeDate(string signalId,int eventCode, DateTime date);
+
+        IReadOnlyList<ControllerEventLog> GetSignalEventsBetweenDates(string signalId,DateTime startTime, DateTime endTime);
+
+        IReadOnlyList<ControllerEventLog> GetTopNumberOfSignalEventsBetweenDates(string signalId, int numberOfRecords,DateTime startTime, DateTime endTime);
+
+        int GetDetectorActivationCount(string signalId,DateTime startTime, DateTime endTime, int detectorChannel);
+
+        int GetRecordCount(string signalId, DateTime startTime, DateTime endTime);
+
+        int GetRecordCountByParameterAndEvent(string signalId, DateTime startTime, DateTime endTime,IReadOnlyList<int> eventParameters, IReadOnlyList<int> events);
+
+        IReadOnlyList<ControllerEventLog> GetRecordsByParameterAndEvent(string signalId, DateTime startTime, DateTime endTime,IReadOnlyList<int> eventParameters, IReadOnlyList<int> eventCodes);
+
+        IReadOnlyList<ControllerEventLog> GetAllAggregationCodes(string signalId, DateTime startTime, DateTime endTime);
+
+        ControllerEventLog GetFirstEventBeforeDateByEventCodeAndParameter(string signalId, int eventCode,int eventParam, DateTime date);
+
+        IReadOnlyList<ControllerEventLog> GetEventsBetweenDates(DateTime startTime, DateTime endTime);
+
+        int GetSignalEventsCountBetweenDates(string signalId, DateTime startTime, DateTime endTime);
+
+        int GetApproachEventsCountBetweenDates(int approachId, DateTime startTime, DateTime endTime,int phaseNumber);
+
+        DateTime GetMostRecentRecordTimestamp(string signalID);
+
+        [Obsolete("This method isn't currently being used, Also can't use this becuase it has times not dates")]
+        bool CheckForRecords(string signalId, DateTime startTime, DateTime endTime);
+    }
+}
--- ATSPM/ApplicationCore/Repositories/ISignalRepository.cs
+++ ATSPM/ApplicationCore/Repositories/ISignalRepository.cs
@@ -9,28 +9,28 @@ namespace ATSPM.Application.Repositories
 {
     public interface ISignalRepository : IRepository<Signal>
     {
-        List<Signal> GetAllSignals();
+        IReadOnlyList<Signal> GetAllSignals();
         string GetSignalDescription(string signalId);
-        List<Signal> GetAllEnabledSignals();
-        List<Signal> EagerLoadAllSignals();
+        IReadOnlyList<Signal> GetAllEnabledSignals();
+        IReadOnlyList<Signal> EagerLoadAllSignals();
         Signal GetLatestVersionOfSignalBySignalID(string signalID);
         //SignalFTPInfo GetSignalFTPInfoByID(string signalID);
-        //List<SignalFTPInfo> GetSignalFTPInfoForAllFTPSignals();
+        //IReadOnlyList<SignalFTPInfo> GetSignalFTPInfoForAllFTPSignals();
         void AddOrUpdate(Signal signal);
-        List<Pin> GetPinInfo();
+        IReadOnlyList<Pin> GetPinInfo();
         string GetSignalLocation(string signalID);
-        void AddList(List<Signal> signals);
+        void AddIReadOnlyList(IReadOnlyList<Signal> signals);
         Signal CopySignalToNewVersion(Signal originalVersion);
-        List<Signal> GetAllVersionsOfSignalBySignalID(string signalID);
-        List<Signal> GetLatestVersionOfAllSignals();
-        List<Signal> GetLatestVersionOfAllSignalsForFtp();
+        IReadOnlyList<Signal> GetAllVersionsOfSignalBySignalID(string signalID);
+        IReadOnlyList<Signal> GetLatestVersionOfAllSignals();
+        IReadOnlyList<Signal> GetLatestVersionOfAllSignalsForFtp();
         int CheckVersionWithFirstDate(string signalId);
-        List<Signal> GetLatestVerionOfAllSignalsByControllerType(int controllerTypeId);
+        IReadOnlyList<Signal> GetLatestVerionOfAllSignalsByControllerType(int controllerTypeId);
         Signal GetVersionOfSignalByDate(string signalId, DateTime startDate);
         Signal GetSignalVersionByVersionId(int versionId);
         void SetVersionToDeleted(int versionId);
         void SetAllVersionsOfASignalToDeleted(string id);
-        List<Signal> GetSignalsBetweenDates(string signalId, DateTime startDate, DateTime endDate);
+        IReadOnlyList<Signal> GetSignalsBetweenDates(string signalId, DateTime startDate, DateTime endDate);
         bool Exists(string signalId);
         Signal GetVersionOfSignalByDateWithDetectionTypes(string signalId, DateTime startDate);
     }
