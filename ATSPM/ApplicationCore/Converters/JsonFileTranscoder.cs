68f657a42c3153cebb079a832818f56f4943f70b
Christian Baker <christianbaker@utah.gov>
2022-01-03 13:59:35 -0700

    Added options to FileRepo base and IFileTranscoder

---
+++ ATSPM/ApplicationCore/Converters/JsonFileTranscoder.cs
@@ -0,0 +1,23 @@
+﻿using ATSPM.Domain.Common;
+using ATSPM.Domain.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATSPM.Application.Converters
+{
+    public class JsonFileTranscoder : IFileTranscoder
+    {
+        public string FileExtension => ".json";
+
+        public T DecodeItem<T>(byte[] data) where T : class
+        {
+            return data.FromEncodedJson<T>();
+        }
+
+        public byte[] EncodeItem<T>(T item) where T : class
+        {
+            return item.ToEncodedJson();
+        }
+    }
+}
---
+++ ATSPM/Infrastructure/Extensions/EntityFrameworkCoreExtensions.cs
@@ -0,0 +1,17 @@
+﻿using ATSPM.Application.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ATSPM.Infrasturcture.Extensions
+{
+    public static class EntityFrameworkCoreExtensions
+    {
+        public static string CreateKeyValueName(this DbContext db, ATSPMModelBase item)
+        {
+            return item.GetType().Name + "_" + string.Join("_", db.Model.FindEntityType(item.GetType()).FindPrimaryKey().Properties.Select(p => String.Format(p.FindAnnotation("KeyNameFormat") != null ? "{0:" + p.FindAnnotation("KeyNameFormat")?.Value.ToString() + "}" : "{0}", p.PropertyInfo.GetValue(item, null))));
+        }
+    }
+}
