acd124facfe324064b4376fad6fa41849b351f6a
Christian Baker <christianbaker@utah.gov>
2022-01-04 14:59:58 -0700

    Moved IControllerEventLogRepository methods to be extensions for reusablility

--- ATSPM/ApplicationCore/Repositories/IControllerEventLogRepository.cs
+++ ATSPM/ApplicationCore/Repositories/IControllerEventLogRepository.cs
@@ -2,71 +2,79 @@
 using ATSPM.Domain.Services;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ATSPM.Application.Repositories
 {
     public interface IControllerEventLogRepository : IAsyncRepository<ControllerLogArchive>
     {
-        [Obsolete("This method isn't currently being used")]
-        bool CheckForRecords(string signalId, DateTime startTime, DateTime endTime);
+        IQueryable<ControllerEventLog> GetSignalEventsBetweenDates(string signalId, DateTime startTime, DateTime endTime);
 
-        IReadOnlyList<ControllerEventLog> GetAllAggregationCodes(string signalId, DateTime startTime, DateTime endTime);
+        #region ExtensionMethods
 
-        int GetApproachEventsCountBetweenDates(int approachId, DateTime startTime, DateTime endTime, int phaseNumber);
+        //int GetSignalEventsCountBetweenDates(string signalId, DateTime startTime, DateTime endTime);
 
-        int GetDetectorActivationCount(string signalId, DateTime startTime, DateTime endTime, int detectorChannel);
+        //IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParam(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventCodes, int param);
 
-        [Obsolete("This method isn't currently being used")]
-        int GetEventCountByEventCodesParamDateTimeRange(string signalId, DateTime startTime, DateTime endTime, int startHour, int startMinute, int endHour, int endMinute, IEnumerable<int> eventCodes, int param);
+        //IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParam(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventCodes, int param, double latencyCorrection);
 
-        [Obsolete("This method isn't currently being used")]
-        IReadOnlyList<ControllerEventLog> GetEventsBetweenDates(DateTime startTime, DateTime endTime);
+        //IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParam(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventCodes, int param, double offset, double latencyCorrection);
 
-        //Christian added overloads to this
-        IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParam(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventCodes, int param);
-        IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParam(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventCodes, int param, double latencyCorrection);
-        IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParam(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventCodes, int param, double offset, double latencyCorrection);
+        //IReadOnlyList<ControllerEventLog> GetRecordsByParameterAndEvent(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventParameters, IEnumerable<int> eventCodes);
 
-        [Obsolete("This method isn't currently being used")]
-        IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParamDateTimeRange(string signalId, DateTime startTime, DateTime endTime, int startHour, int startMinute, int endHour, int endMinute, IEnumerable<int> eventCodes, int param);
+        //int GetRecordCountByParameterAndEvent(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventParameters, IEnumerable<int> eventCodes);
 
-        [Obsolete("User GetEventsByEventCodesParam overload", true)]
-        IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParamWithLatencyCorrection(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventCodes, int param, double latencyCorrection);
+        //IReadOnlyList<ControllerEventLog> GetAllAggregationCodes(string signalId, DateTime startTime, DateTime endTime);
 
-        [Obsolete("User GetEventsByEventCodesParam overload", true)]
-        IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParamWithOffsetAndLatencyCorrection(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventCodes, int param, double offset, double latencyCorrection);
+        //int GetApproachEventsCountBetweenDates(int approachId, DateTime startTime, DateTime endTime, int phaseNumber);
 
-        ControllerEventLog GetFirstEventBeforeDate(string signalId, int eventCode, DateTime date);
+        //int GetDetectorActivationCount(string signalId, DateTime startTime, DateTime endTime, int detectorChannel);
 
-        ControllerEventLog GetFirstEventBeforeDateByEventCodeAndParameter(string signalId, int eventCode, int eventParam, DateTime date);
+        //ControllerEventLog GetFirstEventBeforeDate(string signalId, int eventCode, DateTime date);
 
-        [Obsolete("Depreciated, use a specification instead")]
-        DateTime GetMostRecentRecordTimestamp(string signalID);
+        //ControllerEventLog GetFirstEventBeforeDateByEventCodeAndParameter(string signalId, int eventCode, int eventParam, DateTime date);
 
-        [Obsolete("This is redundant with GetSignalEventsCountBetweenDates")]
-        int GetRecordCount(string signalId, DateTime startTime, DateTime endTime);
+        //IReadOnlyList<ControllerEventLog> GetSignalEventsByEventCode(string signalId, DateTime startTime, DateTime endTime, int eventCode);
 
-        int GetRecordCountByParameterAndEvent(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventParameters, IEnumerable<int> eventCodes);
+        //IReadOnlyList<ControllerEventLog> GetSignalEventsByEventCodes(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventCodes);
 
-        IReadOnlyList<ControllerEventLog> GetRecordsByParameterAndEvent(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventParameters, IEnumerable<int> eventCodes);
+        //IReadOnlyList<ControllerEventLog> GetSplitEvents(string signalId, DateTime startTime, DateTime endTime);
 
-        [Obsolete("This method isn't currently being used")]
-        IReadOnlyList<ControllerEventLog> GetSignalEventsBetweenDates(string signalId, DateTime startTime, DateTime endTime);
+        //double GetTmcVolume(DateTime startDate, DateTime endDate, string signalId, int phase);
 
-        IReadOnlyList<ControllerEventLog> GetSignalEventsByEventCode(string signalId, DateTime startTime, DateTime endTime, int eventCode);
+        //IReadOnlyList<ControllerEventLog> GetTopEventsAfterDateByEventCodesParam(string signalId, DateTime timestamp, IEnumerable<int> eventCodes, int param, int top);
 
-        IReadOnlyList<ControllerEventLog> GetSignalEventsByEventCodes(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventCodes);
+        //IReadOnlyList<ControllerEventLog> GetTopNumberOfSignalEventsBetweenDates(string signalId, int numberOfRecords, DateTime startTime, DateTime endTime);
 
-        [Obsolete("This is redundant with GetRecordCount")]
-        int GetSignalEventsCountBetweenDates(string signalId, DateTime startTime, DateTime endTime);
+        #endregion
 
-        IReadOnlyList<ControllerEventLog> GetSplitEvents(string signalId, DateTime startTime, DateTime endTime);
+        #region Obsolete
 
-        double GetTmcVolume(DateTime startDate, DateTime endDate, string signalId, int phase);
+        //[Obsolete("This method isn't currently being used")]
+        //bool CheckForRecords(string signalId, DateTime startTime, DateTime endTime);
 
-        IReadOnlyList<ControllerEventLog> GetTopEventsAfterDateByEventCodesParam(string signalId, DateTime timestamp, IEnumerable<int> eventCodes, int param, int top);
+        //[Obsolete("This method isn't currently being used")]
+        //int GetEventCountByEventCodesParamDateTimeRange(string signalId, DateTime startTime, DateTime endTime, int startHour, int startMinute, int endHour, int endMinute, IEnumerable<int> eventCodes, int param);
 
-        IReadOnlyList<ControllerEventLog> GetTopNumberOfSignalEventsBetweenDates(string signalId, int numberOfRecords, DateTime startTime, DateTime endTime);
+        //[Obsolete("This method isn't currently being used")]
+        //IReadOnlyList<ControllerEventLog> GetEventsBetweenDates(DateTime startTime, DateTime endTime);
+
+        //[Obsolete("This method isn't currently being used")]
+        //IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParamDateTimeRange(string signalId, DateTime startTime, DateTime endTime, int startHour, int startMinute, int endHour, int endMinute, IEnumerable<int> eventCodes, int param);
+
+        //[Obsolete("Use GetEventsByEventCodesParam overload", true)]
+        //IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParamWithLatencyCorrection(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventCodes, int param, double latencyCorrection);
+
+        //[Obsolete("Use GetEventsByEventCodesParam overload", true)]
+        //IReadOnlyList<ControllerEventLog> GetEventsByEventCodesParamWithOffsetAndLatencyCorrection(string signalId, DateTime startTime, DateTime endTime, IEnumerable<int> eventCodes, int param, double offset, double latencyCorrection);
+
+        //[Obsolete("Use GetSignalEventsCountBetweenDates instead", true)]
+        //int GetRecordCount(string signalId, DateTime startTime, DateTime endTime);
+
+        //[Obsolete("Depreciated, use a specification instead")]
+        //DateTime GetMostRecentRecordTimestamp(string signalID);
+
+        #endregion
     }
 }
