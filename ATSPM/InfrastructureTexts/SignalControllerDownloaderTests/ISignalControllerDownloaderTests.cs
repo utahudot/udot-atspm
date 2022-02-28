efbba96117d31f27ec3a9d807fbe587b4b62b562
Christian Baker <christianbaker@utah.gov>
2022-02-28 10:19:46 -0700

    Finished basic unit tests for ISignalControllerDownloader

--- ATSPM/InfrastructureTexts/SignalControllerDownloaderTests/ISignalControllerDownloaderTests.cs
+++ ATSPM/InfrastructureTexts/SignalControllerDownloaderTests/ISignalControllerDownloaderTests.cs
@@ -1,3 +1,4 @@
+using ATSPM.Application.Common;
 using ATSPM.Application.Configuration;
 using ATSPM.Application.Exceptions;
 using ATSPM.Application.Models;
@@ -28,27 +29,14 @@ namespace SignalControllerLoggerTests
 
         private StringWriter _consoleOut = new StringWriter();
 
-        //private readonly IServiceCollection _serviceCollection;
-        //private ILogger _nullLogger;
-        //private IOptions<SignalControllerDownloaderConfiguration> _nullOptions;
-
         public ISignalControllerDownloaderTests(ITestOutputHelper output)
         {
             _output = output;
             
-            Console.SetOut(_consoleOut);
-
-            //_loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder.SetMinimumLevel(LogLevel.Trace).AddProvider(new XUnitLoggerProvider(_consoleOut)).AddSystemdConsole());
-
-            _loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder.SetMinimumLevel(LogLevel.Trace).AddProvider(new GenericLoggerProvider<StringWriter>(_consoleOut)).AddProvider(new GenericLoggerProvider<ITestOutputHelper>(output)));
-
-
-            //_serviceCollection = new ServiceCollection();
-
-            ////downloaders
-            //_serviceCollection.AddScoped<ISignalControllerDownloader, FTPSignalControllerDownloader>();
-            //_serviceCollection.AddScoped<ISignalControllerDownloader, MaxTimeSignalControllerDownloader>();
-            //_serviceCollection.AddScoped<ISignalControllerDownloader, SFTPSignalControllerDownloader>();
+            _loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder
+            .SetMinimumLevel(LogLevel.Trace)
+            .AddProvider(new GenericLoggerProvider<StringWriter>(_consoleOut))
+            .AddProvider(new GenericLoggerProvider<ITestOutputHelper>(output)));
         }
 
         #region ISignalControllerDownloader
@@ -110,30 +98,12 @@ namespace SignalControllerLoggerTests
             Assert.False(condition);
         }
 
-        //[Fact]
-        //public async void ExecuteAsyncCanExecuteInvalid()
-        //{
-        //    var client = new Mock<ISFTPDownloaderClient>();
-        //    client.Setup()
-
-
-
-        //    Signal signal = new Signal()
-        //    {
-        //        ControllerType = new ControllerType() { ControllerTypeId = 4 }
-        //    };
-
-        //    _output.WriteLine($"ControllerTypeId: {signal.ControllerType.ControllerTypeId}");
-
-        //    var condition = _downloader.CanExecute(signal);
-
-        //    Assert.True(condition);
-        //}
-
         [Theory]
         [SignalControllerDownloaders]
         public async void ExecuteWithNullParameter(Type downloader, IDownloaderClient mockClient, ILogger log, IOptionsSnapshot<SignalControllerDownloaderConfiguration> mockConfig)
         {
+            Mock.Get(mockClient).Setup(s => s.Dispose());
+
             var d = (ISignalControllerDownloader)Activator.CreateInstance(downloader, new object[] { mockClient, log, mockConfig });
 
             await Assert.ThrowsAsync<ArgumentNullException>(async () =>
@@ -146,6 +116,8 @@ namespace SignalControllerLoggerTests
         [SignalControllerDownloaders]
         public async void ExecuteWithInvalidIPAddress(Type downloader, IDownloaderClient mockClient, ILogger log, IOptionsSnapshot<SignalControllerDownloaderConfiguration> mockConfig)
         {
+            Mock.Get(mockClient).Setup(s => s.Dispose());
+
             Mock.Get(mockConfig).Setup(s => s.Value).Returns(new SignalControllerDownloaderConfiguration() { PingControllerToVerify = false });
 
             var d = (ISignalControllerDownloader)Activator.CreateInstance(downloader, new object[] { mockClient, log, mockConfig });
@@ -153,6 +125,9 @@ namespace SignalControllerLoggerTests
             var signal = new Signal()
             {
                 Ipaddress = "Invalid Address",
+                Enabled = true,
+                PrimaryName = "Controller",
+                SignalId = "999",
                 ControllerType = new ControllerType() { ControllerTypeId = d.ControllerType }
             };
 
@@ -162,76 +137,55 @@ namespace SignalControllerLoggerTests
             });
         }
 
-        [Theory]
-        [SignalControllerDownloaders]
-        public async void ExecuteWithControllerConnectionException(Type downloader, IDownloaderClient mockClient, ILogger log, IOptionsSnapshot<SignalControllerDownloaderConfiguration> mockConfig)
-        {
-            var signal = new Signal()
-            {
-                Ipaddress = "127.0.0.1"
-            };
+        //[Theory]
+        //[SignalControllerDownloaders]
+        //public async void ExecuteWithControllerConnectionException(Type downloader, IDownloaderClient mockClient, ILogger log, IOptionsSnapshot<SignalControllerDownloaderConfiguration> mockConfig)
+        //{
+        //    var signal = new Signal()
+        //    {
+        //        Ipaddress = "127.0.0.1"
+        //    };
 
-            Mock.Get(mockConfig).Setup(s => s.Value).Returns(new SignalControllerDownloaderConfiguration() { PingControllerToVerify = false });
+        //    Mock.Get(mockConfig).Setup(s => s.Value).Returns(new SignalControllerDownloaderConfiguration() { PingControllerToVerify = false });
 
-            Mock.Get(mockClient).Setup(s => s.ConnectAsync(It.IsAny<NetworkCredential>(), 0, 0, default))
-                .ThrowsAsync(new ControllerConnectionException(It.Is<string>(s => s == signal.Ipaddress), null, null))
-                .Verifiable();
+        //    Mock.Get(mockClient).Setup(s => s.ConnectAsync(It.IsAny<NetworkCredential>(), 0, 0, default))
+        //        .ThrowsAsync(new ControllerConnectionException(It.Is<string>(s => s == signal.Ipaddress), mockClient, null))
+        //        .Verifiable();
 
-            Mock.Get(mockClient).SetupGet(s => s.IsConnected).Returns(false).Verifiable();
+        //    Mock.Get(mockClient).SetupGet(s => s.IsConnected).Returns(false).Verifiable();
 
-            //var logger = _loggerFactory.CreateLogger<FTPSignalControllerDownloader>();
-            var logger = GenericLogger<ITestOutputHelper>.CreateLogger<FTPSignalControllerDownloader>(_output);
+        //    Mock.Get(mockClient).Setup(s => s.DisconnectAsync(default)).Returns(Task.CompletedTask).Verifiable();
+        //    Mock.Get(mockClient).Setup(s => s.Dispose()).Verifiable();
 
-            if (logger is GenericLogger<ITestOutputHelper, FTPSignalControllerDownloader> testing)
-            {
-                testing.OnLogged += (o, e) =>
-                {
-                    _output.WriteLine($"just wrote this!!! {e.LogLevel} : {e.EventId.Id}|{e.EventId.Name}");
+        //    var d = (ISignalControllerDownloader)Activator.CreateInstance(downloader, new object[] { mockClient, log, mockConfig });
 
-                };
-            }
+        //    signal.ControllerType = new ControllerType() { ControllerTypeId = d.ControllerType };
 
-            var d = (ISignalControllerDownloader)Activator.CreateInstance(downloader, new object[] { mockClient, logger, mockConfig });
+        //    await foreach (var file in d.Execute(signal)) { }
 
-            signal.ControllerType = new ControllerType() { ControllerTypeId = d.ControllerType };
+        //    Mock.Verify();
+        //}
 
-            await foreach (var file in d.Execute(signal)) { }
+        //[Theory]
+        //[SignalControllerDownloaders]
+        //public async void ExecuteWithControllerConnectionTokenCancelled(Type downloader, IDownloaderClient mockClient, ILogger log, IOptionsSnapshot<SignalControllerDownloaderConfiguration> mockConfig)
+        //{
 
-            Mock.Verify();
+        //}
 
-            //_output.WriteLine($"{_consoleOut}");
+        //[Theory]
+        //[SignalControllerDownloaders]
+        //public async void ExecuteWithControllerDownloadFileException(Type downloader, IDownloaderClient mockClient, ILogger log, IOptionsSnapshot<SignalControllerDownloaderConfiguration> mockConfig)
+        //{
 
-            //var exception = await Assert.ThrowsAsync<ControllerConnectionException>(async () =>
-            //{
-            //    await foreach (var file in d.Execute(signal)) { }
-            //});
+        //}
 
-            //var expected = signal.Ipaddress;
-            //var actual = exception.Host;
+        //[Theory]
+        //[SignalControllerDownloaders]
+        //public async void ExecuteWithControllerDownloadFileTokenCancelled(Type downloader, IDownloaderClient mockClient, ILogger log, IOptionsSnapshot<SignalControllerDownloaderConfiguration> mockConfig)
+        //{
 
-            //Assert.Equal(expected, actual);
-        }
-
-        [Theory]
-        [SignalControllerDownloaders]
-        public async void ExecuteWithControllerConnectionTokenCancelled(Type downloader, IDownloaderClient mockClient, ILogger log, IOptionsSnapshot<SignalControllerDownloaderConfiguration> mockConfig)
-        {
-
-        }
-
-        [Theory]
-        [SignalControllerDownloaders]
-        public async void ExecuteWithControllerDownloadFileException(Type downloader, IDownloaderClient mockClient, ILogger log, IOptionsSnapshot<SignalControllerDownloaderConfiguration> mockConfig)
-        {
-
-        }
-
-        [Theory]
-        [SignalControllerDownloaders]
-        public async void ExecuteWithControllerDownloadFileTokenCancelled(Type downloader, IDownloaderClient mockClient, ILogger log, IOptionsSnapshot<SignalControllerDownloaderConfiguration> mockConfig)
-        {
-
-        }
+        //}
 
         [Theory]
         [SignalControllerDownloaders]
@@ -271,7 +225,6 @@ namespace SignalControllerLoggerTests
             Mock.Get(mockClient).Setup(s => s.Dispose()).Verifiable();
 
             signal.ControllerType = new ControllerType() { ControllerTypeId = d.ControllerType, Ftpdirectory = ftpDirectory };
-
             var files = new List<FileInfo>();
 
             await foreach (var file in d.Execute(signal)) 
@@ -291,14 +244,66 @@ namespace SignalControllerLoggerTests
         [SignalControllerDownloaders]
         public async void ExecuteWithControllerDownloadFileWithProgressValid(Type downloader, IDownloaderClient mockClient, ILogger log, IOptionsSnapshot<SignalControllerDownloaderConfiguration> mockConfig)
         {
+            var signal = new Signal()
+            {
+                Ipaddress = "127.0.0.1",
+                Enabled = true,
+                PrimaryName = "Controller",
+                SignalId = "999"
+            };
 
-        }
+            Mock.Get(mockConfig).Setup(s => s.Value).Returns(new SignalControllerDownloaderConfiguration()
+            {
+                LocalPath = "C:\\TestPath",
+                PingControllerToVerify = false
+            });
 
-        [Theory]
-        [SignalControllerDownloaders]
-        public async void ExecuteWithControllerDownloadFileWithProgressNoFiles(Type downloader, IDownloaderClient mockClient, ILogger log, IOptionsSnapshot<SignalControllerDownloaderConfiguration> mockConfig)
-        {
+            var verifyPath = Path.Combine(mockConfig.Value.LocalPath, signal.SignalId);
+            var ftpDirectory = "\\dir";
 
+            Mock.Get(mockClient).Setup(s => s.ConnectAsync(It.IsAny<NetworkCredential>(), 0, 0, default)).Returns(Task.CompletedTask).Verifiable();
+
+            Mock.Get(mockClient).SetupGet(s => s.IsConnected).Returns(true).Verifiable();
+
+            var returnValue = Enumerable.Range(1, 10).Select(s => $"{s}.txt");
+
+            var d = (ISignalControllerDownloader)Activator.CreateInstance(downloader, new object[] { mockClient, log, mockConfig });
+
+            Mock.Get(mockClient).Setup(s => s.ListDirectoryAsync(It.Is<string>(i => i == ftpDirectory), default, It.Is<string[]>(i => i == d.FileFilters))).ReturnsAsync(returnValue).Verifiable();
+
+            Mock.Get(mockClient).Setup(v => v.DownloadFileAsync(It.Is<string>(i => i.StartsWith(verifyPath)), It.IsIn<string>(returnValue), default))
+                .ReturnsAsync((string localPath, string remotePath, CancellationToken token) => new FileInfo(localPath)).Verifiable();
+
+            Mock.Get(mockClient).Setup(s => s.DisconnectAsync(default)).Returns(Task.CompletedTask).Verifiable();
+            Mock.Get(mockClient).Setup(s => s.Dispose()).Verifiable();
+
+            signal.ControllerType = new ControllerType() { ControllerTypeId = d.ControllerType, Ftpdirectory = ftpDirectory };
+
+            var progressList = new List<ControllerDownloadProgress>();
+
+            Progress<ControllerDownloadProgress> progress = new Progress<ControllerDownloadProgress>(p =>
+            {
+                progressList.Add(p);
+                _output.WriteLine($"Progress: {p.File.FullName} {p}");
+            });
+
+            var files = new List<FileInfo>();
+
+            await foreach (var file in d.Execute(signal, progress))
+            {
+                files.Add(file);
+            }
+
+            Mock.Verify();
+
+            var condition = (progressList.Where(p => p.IsSuccessful).Count() == files.Count);
+
+            Assert.True(condition);
+
+            var expected = files;
+            var actual = progressList.OrderBy(o => o.Current).Select(s => s.File);
+
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
