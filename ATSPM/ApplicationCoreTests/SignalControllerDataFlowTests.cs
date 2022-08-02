9fb1aa5ba358a14b28be67f1ac48c2fa0239ca73
Christian Baker <christianbaker@utah.gov>
2022-08-02 13:16:43 -0600

    Upgraded to net6 and updated nuget packages

--- ATSPM/ApplicationCoreTests/SignalControllerDataFlowTests.cs
+++ ATSPM/ApplicationCoreTests/SignalControllerDataFlowTests.cs
@@ -29,13 +29,7 @@ namespace ApplicationCoreTests
         [Fact]
         public void Test1()
         {
-            var downloader = new Mock<ISignalControllerDownloader>();
-            downloader.Setup(s => s.CanExecute(It.Is<Signal>(p => true))).Returns(true);
-            downloader.Setup(s => s.Execute(It.Is<Signal>(p => true), default, default)).Returns(new List<FileInfo>(new FileInfo[] { new FileInfo(Path.GetTempFileName()) }).ToAsyncEnumerable());
-            
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddScoped<ISignalControllerDownloader>(sp => downloader.Object);
-            var sut = new SignalControllerDataFlow(new NullLogger<SignalControllerDataFlow>(), serviceCollection.BuildServiceProvider());
+
 
 
         }
