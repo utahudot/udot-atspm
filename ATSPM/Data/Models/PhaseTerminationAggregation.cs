8ea95b58504415de5154f30a9b109512b6ce2b58
Christian Baker <christianbaker@utah.gov>
2022-08-08 08:22:24 -0600

    Added web projects

---
+++ ATSPM/Data/Models/PhaseTerminationAggregation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.Models
+{
+    public partial class PhaseTerminationAggregation
+    {
+        public DateTime BinStartTime { get; set; }
+        public string SignalId { get; set; } = null!;
+        public int PhaseNumber { get; set; }
+        public int GapOuts { get; set; }
+        public int ForceOffs { get; set; }
+        public int MaxOuts { get; set; }
+        public int Unknown { get; set; }
+    }
+}
---
+++ ATSPM/Data/Models/SignalPlanAggregation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.Models
+{
+    public partial class SignalPlanAggregation
+    {
+        public string SignalId { get; set; } = null!;
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public int PlanNumber { get; set; }
+    }
+}
