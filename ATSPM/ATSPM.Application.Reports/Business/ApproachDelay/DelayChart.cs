e5f93b0b463cc31ad183068a7f33ed7ec3a05eec
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2022-11-24 12:37:34 -0700

    ATSPM-29 Determine return format for Approach Delay Chart - Moving files

--- ATSPM/ATSPM.Application.Reports/Business/ApproachDelay/DelayChart.cs
+++ ATSPM/ATSPM.Application.Reports/Business/ApproachDelay/DelayChart.cs
@@ -1,56 +1,114 @@
-﻿using ATSPM.Application.Reports.ViewModels.ApproachDelay;
-using ATSPM.Application.Reports.ViewModels.ApproachVolume;
-using AutoFixture;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using MOE.Common.Business.WCFServiceLibrary;
 
-namespace ATSPM.Application.Reports.Controllers
+namespace MOE.Common.Business
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ApproachDelayController : ControllerBase
+    public class DelayChart
     {
-        [HttpGet("test")]
-        public ApproachDelayChart Test()
+        public Chart Chart;
+
+
+        public DelayChart(ApproachDelayOptions options, SignalPhase signalPhase)
         {
-            Fixture fixture = new();
-            ApproachDelayChart viewModel = fixture.Create<ApproachDelayChart>();
-            return viewModel;
+            Options = options;
+            //Set the chart properties
+            Chart = ChartFactory.CreateDefaultChart(options);
+            ChartFactory.SetImageProperties(Chart);
+
+
+            //Create the chart legend
+            var chartLegend = new Legend();
+            chartLegend.Name = "MainLegend";
+            chartLegend.Docking = Docking.Left;
+            Chart.Legends.Add(chartLegend);
+
+
+            
+
+            //Primary Y axis (delay per vehicle)
+            if (Options.ShowDelayPerVehicle)
+            {
+                if (Options.YAxisMax != null)
+                    Chart.ChartAreas[0].AxisY.Maximum = Options.YAxisMax.Value;
+                Chart.ChartAreas[0].AxisY.Minimum = 0;
+                Chart.ChartAreas[0].AxisY.Enabled = AxisEnabled.True;
+                Chart.ChartAreas[0].AxisY.MajorTickMark.Enabled = true;
+                Chart.ChartAreas[0].AxisY.MajorGrid.Enabled = true;
+                Chart.ChartAreas[0].AxisY.Interval = 5;
+                Chart.ChartAreas[0].AxisY.TitleForeColor = Color.Blue;
+                Chart.ChartAreas[0].AxisY.Title = "Delay Per Vehicle (Seconds) ";
+            }
+
+            //secondary y axis (total delay)
+            if (Options.ShowDelayPerVehicle)
+            {
+                if (Options.Y2AxisMax != null && Options.Y2AxisMax > 0)
+                    Chart.ChartAreas[0].AxisY2.Maximum = Options.Y2AxisMax.Value;
+                else
+                    Chart.ChartAreas[0].AxisY2.Maximum = 10;
+                Chart.ChartAreas[0].AxisY2.Minimum = 0;
+                Chart.ChartAreas[0].AxisY2.Enabled = AxisEnabled.True;
+                Chart.ChartAreas[0].AxisY2.MajorTickMark.Enabled = true;
+                Chart.ChartAreas[0].AxisY2.MajorGrid.Enabled = false;
+                Chart.ChartAreas[0].AxisY2.Interval = 5;
+                Chart.ChartAreas[0].AxisY2.Title = "Delay per Hour (hrs) ";
+                Chart.ChartAreas[0].AxisY2.TitleForeColor = Color.Red;
+            }
+
+            //Add the point series
+
+            var delayPerVehicleSeries = new Series();
+            delayPerVehicleSeries.ChartType = SeriesChartType.Line;
+            delayPerVehicleSeries.Color = Color.Blue;
+            delayPerVehicleSeries.Name = "Approach Delay Per Vehicle";
+            delayPerVehicleSeries.YAxisType = AxisType.Primary;
+            delayPerVehicleSeries.XValueType = ChartValueType.DateTime;
+
+            var delaySeries = new Series();
+            delaySeries.ChartType = SeriesChartType.Line;
+            delaySeries.Color = Color.Red;
+            delaySeries.Name = "Approach Delay";
+            delaySeries.YAxisType = AxisType.Secondary;
+            delaySeries.XValueType = ChartValueType.DateTime;
+
+
+            var pointSeries = new Series();
+            pointSeries.ChartType = SeriesChartType.Point;
+            pointSeries.Color = Color.White;
+            pointSeries.Name = "Posts";
+            pointSeries.XValueType = ChartValueType.DateTime;
+            pointSeries.IsVisibleInLegend = false;
+
+
+            Chart.Series.Add(pointSeries);
+            Chart.Series.Add(delaySeries);
+            Chart.Series.Add(delayPerVehicleSeries);
+
+
+            //Add points at the start and and of the x axis to ensure
+            //the graph covers the entire period selected by the user
+            //whether there is data or not
+            Chart.Series["Posts"].Points.AddXY(Options.StartDate, 0);
+            Chart.Series["Posts"].Points.AddXY(Options.EndDate, 0);
+
+            AddDataToChart(Chart, signalPhase, Options.SelectedBinSize, Options.ShowTotalDelayPerHour,
+                Options.ShowDelayPerVehicle);
+            if (Options.ShowPlanStatistics)
+                SetPlanStrips(signalPhase.Plans, Chart, Options.StartDate, Options.ShowPlanStatistics);
         }
 
-        [HttpPost("chart")]
-        public ApproachDelayChart Chart(
-            string signalId,
-            int phaseNumber,
-            DateTime start,
-            DateTime end,
-
-            )
-        {
-            var signalPhase = new SignalPhase
-            var delayChart = new ApproachDelayChart(
-                "Approach Delay",
-                signalId,
-                "",
-                phaseNumber,
-                "",
-                start,
-                end,
-                );
-            Fixture fixture = new();
-            ApproachDelayChart viewModel = fixture.Create<ApproachDelayChart>();
-            return viewModel;
-        }
+        public ApproachDelayOptions Options { get; set; }
 
         private void SetChartTitles(SignalPhase signalPhase, Dictionary<string, string> statistics)
         {
             Chart.Titles.Add(ChartTitleFactory.GetChartName(Options.MetricTypeID));
             Chart.Titles.Add(ChartTitleFactory.GetSignalLocationAndDateRange(
                 Options.SignalID, Options.StartDate, Options.EndDate));
-            Chart.Titles.Add(ChartTitleFactory.GetPhaseAndPhaseDescriptions(
-                signalPhase.Approach, signalPhase.GetPermissivePhase));
+                Chart.Titles.Add(ChartTitleFactory.GetPhaseAndPhaseDescriptions(
+                    signalPhase.Approach, signalPhase.GetPermissivePhase));
             Chart.Titles.Add(ChartTitleFactory.GetStatistics(statistics));
             Chart.Titles.Add(ChartTitleFactory.GetTitle(
                 "Simplified Approach Delay. Displays time between approach activation during the red phase and when the phase turns green."
@@ -66,8 +124,8 @@ namespace ATSPM.Application.Reports.Controllers
             while (dt < signalPhase.EndDate)
             {
                 var pcdsInBin = from item in signalPhase.Cycles
-                                where item.StartTime >= dt && item.StartTime < dt.AddMinutes(binSize)
-                                select item;
+                    where item.StartTime >= dt && item.StartTime < dt.AddMinutes(binSize)
+                    select item;
 
                 var binDelay = pcdsInBin.Sum(d => d.TotalDelay);
                 var binVolume = pcdsInBin.Sum(d => d.TotalVolume);
@@ -79,7 +137,7 @@ namespace ATSPM.Application.Reports.Controllers
                 else
                     bindDelaypervehicle = 0;
 
-                bindDelayperhour = binDelay * (60 / binSize) / 60 / 60;
+                bindDelayperhour = binDelay * (60 / binSize) /60/60;
 
                 if (showDelayPerVehicle)
                     chart.Series["Approach Delay Per Vehicle"].Points.AddXY(dt, bindDelaypervehicle);
@@ -90,7 +148,7 @@ namespace ATSPM.Application.Reports.Controllers
             }
             var statistics = new Dictionary<string, string>();
             statistics.Add("Average Delay Per Vehicle (AD)", Math.Round(signalPhase.AvgDelay) + " seconds");
-            statistics.Add("Total Delay For Selected Period (TD)", Math.Round(signalPhase.TotalDelay / 60 / 60, 1) + " hours");
+            statistics.Add("Total Delay For Selected Period (TD)", Math.Round(signalPhase.TotalDelay/60/60,1) + " hours");
             SetChartTitles(signalPhase, statistics);
         }
 
@@ -152,7 +210,7 @@ namespace ATSPM.Application.Reports.Controllers
                     var aogLabel = new CustomLabel();
                     aogLabel.FromPosition = plan.StartTime.ToOADate();
                     aogLabel.ToPosition = plan.EndTime.ToOADate();
-                    aogLabel.Text = Math.Round(totalDelay / 60 / 60, 1) + " TD";
+                    aogLabel.Text = Math.Round(totalDelay / 60 /60, 1) + " TD";
                     aogLabel.LabelMark = LabelMarkStyle.LineSideMark;
                     aogLabel.ForeColor = Color.Red;
                     aogLabel.RowIndex = 1;
@@ -171,6 +229,5 @@ namespace ATSPM.Application.Reports.Controllers
                 backGroundColor++;
             }
         }
-
     }
 }
---
+++ ATSPM/ATSPM.Application.Reports/Business/Plan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ATSPM.Application.Reports.Business
+{
+    public class Plan
+    {
+        public Plan(DateTime start, DateTime end, int planNumber)
+        {
+            StartTime = start;
+            EndTime = end;
+            PlanNumber = planNumber;
+        }
+
+        public DateTime StartTime { get; }
+        public DateTime EndTime { get; }
+        public int PlanNumber { get; }
+    }
+}
