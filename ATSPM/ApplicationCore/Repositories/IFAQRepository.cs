92c3b1a67f2538f0a317f63f72d567bc5a09ac53
Christian Baker <christianbaker@utah.gov>
2022-12-08 14:18:14 -0700

    Completed IFaqRepository and EF Implementation

--- ATSPM/ApplicationCore/Repositories/IFAQRepository.cs
+++ ATSPM/ApplicationCore/Repositories/IFAQRepository.cs
@@ -8,21 +8,7 @@ using System.Threading.Tasks;
 
 namespace ATSPM.Application.Repositories
 {
-    public interface IFAQRepository : IAsyncRepository<Faq>
+    public interface IFaqRepository : IAsyncRepository<Faq>
     {
-        [Obsolete("Use GetList instead")]
-        List<Faq> GetAll();
-        
-        [Obsolete("Use Lookup instead")]
-        Faq GetbyID(int id);
-        
-        [Obsolete("Use Add in the BaseClass")]
-        void Add(Faq item);
-        
-        [Obsolete("Use Remove in the BaseClass")]
-        void Remove(int id);
-        
-        [Obsolete("Use Update in the BaseClass")]
-        void Update(Faq item);
     }
 }
