ed848f1a53ed2ef79f2dfdeb7c7ad1938034c448
Christian Baker <christianbaker@utah.gov>
2022-12-12 09:48:58 -0700

    Completed IRoutePhaseDirectionRepository with EF implementation

--- ATSPM/ApplicationCore/Repositories/IRoutePhaseDirectionRepository.cs
+++ ATSPM/ApplicationCore/Repositories/IRoutePhaseDirectionRepository.cs
@@ -8,21 +8,28 @@ using System.Threading.Tasks;
 
 namespace ATSPM.Application.Repositories
 {
+    /// <summary>
+    /// Routed Phase Direction Repository
+    /// </summary>
     public interface IRoutePhaseDirectionRepository : IAsyncRepository<RoutePhaseDirection>
     {
-        [Obsolete("Use GetList instead")]
-        IReadOnlyList<RoutePhaseDirection> GetAll();
-        
-        [Obsolete("Use Lookup instead")]
-        RoutePhaseDirection GetByID(int routeID);
-        
-        [Obsolete("Use Delete in the BaseClass")]
-        void DeleteByID(int id);
-        
-        [Obsolete("Use Update in the BaseClass")]
-        void Update(RoutePhaseDirection routePhaseDirection);
-        
-        [Obsolete("Use Add in the BaseClass")]
-        void Add(RoutePhaseDirection newRRoutePhaseDirection);
+        #region Obsolete
+
+        //[Obsolete("Use GetList instead")]
+        //IReadOnlyList<RoutePhaseDirection> GetAll();
+
+        //[Obsolete("Use Lookup instead")]
+        //RoutePhaseDirection GetByID(int routeID);
+
+        //[Obsolete("Use Delete in the BaseClass")]
+        //void DeleteByID(int id);
+
+        //[Obsolete("Use Update in the BaseClass")]
+        //void Update(RoutePhaseDirection routePhaseDirection);
+
+        //[Obsolete("Use Add in the BaseClass")]
+        //void Add(RoutePhaseDirection newRRoutePhaseDirection);
+
+        #endregion
     }
 }
---
+++ ATSPM/Infrastructure/Repositories/RoutePhaseDirectionEFRepository.cs
@@ -0,0 +1,37 @@
+﻿using ATSPM.Data.Models;
+using ATSPM.Application.Repositories;
+using ATSPM.Application.ValueObjects;
+using ATSPM.Domain.Common;
+using ATSPM.Domain.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using ATSPM.Application.Specifications;
+using ATSPM.Domain.Services;
+using ATSPM.Data;
+
+namespace ATSPM.Infrastructure.Repositories
+{
+    public class RoutePhaseDirectionEFRepository : ATSPMRepositoryEFBase<RoutePhaseDirection>, IRoutePhaseDirectionRepository
+    {
+        public RoutePhaseDirectionEFRepository(ConfigContext db, ILogger<RoutePhaseDirectionEFRepository> log) : base(db, log) { }
+
+        #region Overrides
+
+        public override IQueryable<RoutePhaseDirection> GetList()
+        {
+            return base.GetList()
+                .Include(i => i.DirectionType)
+                .Include(i => i.RouteSignal);
+        }
+
+        #endregion
+
+        #region IRoutePhaseDirectionRepository
+
+        #endregion
+    }
+}
