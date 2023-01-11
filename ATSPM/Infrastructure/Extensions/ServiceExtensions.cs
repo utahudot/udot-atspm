1c5b6b981e9476986859d6d7bf7acc80d2f23495
Christian Baker <christianbaker@utah.gov>
2023-01-11 12:14:29 -0700

    Updated Migrations for legacycontext

--- ATSPM/Infrastructure/Extensions/ServiceExtensions.cs
+++ ATSPM/Infrastructure/Extensions/ServiceExtensions.cs
@@ -18,6 +18,8 @@ namespace ATSPM.Infrastructure.Extensions
 
             services.AddDbContext<SpeedContext>(db => db.UseSqlServer(host.Configuration.GetConnectionString(nameof(SpeedContext)), opt => opt.MigrationsAssembly(typeof(ServiceExtensions).Assembly.FullName)).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking).EnableSensitiveDataLogging(host.HostingEnvironment.IsDevelopment()));
 
+            services.AddDbContext<LegacyEventLogContext>(db => db.UseSqlServer(host.Configuration.GetConnectionString(nameof(LegacyEventLogContext)), opt => opt.MigrationsAssembly(typeof(ServiceExtensions).Assembly.FullName)).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking).EnableSensitiveDataLogging(host.HostingEnvironment.IsDevelopment()));
+
             return services;
         }
     }
