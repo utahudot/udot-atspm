1ad0191b1ecd515d6cb96926eceadee2d1720154
Christian Baker <christianbaker@utah.gov>
2023-02-21 12:33:36 -0700

    Added AggregationCommand

--- ATSPM/EventLogUtility/Commands/EventLogCommands.cs
+++ ATSPM/EventLogUtility/Commands/EventLogCommands.cs
@@ -11,11 +11,13 @@ namespace ATSPM.EventLogUtility.Commands
     {
         public EventLogCommands()
         {
+            this.AddCommand(AggregationCommand);
             this.AddCommand(LogCommand);
             this.AddCommand(ExtractCommand);
             this.AddCommand(SignalInfoCommand);
         }
 
+        public AggregationCommand AggregationCommand { get; set; } = new AggregationCommand();
         public LogConsoleCommand LogCommand { get; set; } = new LogConsoleCommand();
         public ExtractConsoleCommand ExtractCommand { get; set; } = new ExtractConsoleCommand();
         public SignalInfoCommand SignalInfoCommand { get; set; } = new SignalInfoCommand();
