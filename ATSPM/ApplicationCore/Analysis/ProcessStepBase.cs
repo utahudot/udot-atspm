b0830418eb2cfc0ebcca005f03eb5207d4263d56
Christian Baker <christianbaker@utah.gov>
2023-04-10 19:26:01 -0600

    Made ProcessStepBase

---
+++ ATSPM/ApplicationCore/Analysis/ProcessStepBase.cs
@@ -0,0 +1,131 @@
+﻿using ATSPM.Data.Enums;
+using ATSPM.Data.Models;
+using ATSPM.Domain.Common;
+using ATSPM.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using System.Windows.Input;
+
+namespace ATSPM.Application.Analysis
+{
+    public abstract class ProcessStepBase<T1, T2> : IExecuteAsync<T1, T2>, IPropagatorBlock<T1, T2>, IDataflowBlock, ISourceBlock<T2>, ITargetBlock<T1>
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private readonly IPropagatorBlock<T1, T2> _workflowProcess;
+
+        public ProcessStepBase(ExecutionDataflowBlockOptions dataflowBlockOptions = default)
+        {
+            dataflowBlockOptions.NameFormat = this.GetType().Name;
+            _workflowProcess = new TransformBlock<T1, T2>(p => ExecuteAsync(p, dataflowBlockOptions.CancellationToken), dataflowBlockOptions);
+        }
+
+        #region IPropagatorBlock
+
+        #region IDataflowBlock
+
+        public Task Completion => _workflowProcess.Completion;
+
+        public void Complete()
+        {
+            _workflowProcess.Complete();
+        }
+
+        public void Fault(Exception exception)
+        {
+            _workflowProcess.Fault(exception);
+        }
+
+        #endregion
+
+        #region ISourceBlock
+
+        public T2? ConsumeMessage(DataflowMessageHeader messageHeader, ITargetBlock<T2> target, out bool messageConsumed)
+        {
+            return _workflowProcess.ConsumeMessage(messageHeader, target, out messageConsumed);
+        }
+
+        public IDisposable LinkTo(ITargetBlock<T2> target, DataflowLinkOptions linkOptions)
+        {
+            return _workflowProcess.LinkTo(target, linkOptions);
+        }
+
+        public void ReleaseReservation(DataflowMessageHeader messageHeader, ITargetBlock<T2> target)
+        {
+            _workflowProcess?.ReleaseReservation(messageHeader, target);
+        }
+
+        public bool ReserveMessage(DataflowMessageHeader messageHeader, ITargetBlock<T2> target)
+        {
+            return _workflowProcess.ReserveMessage(messageHeader, target);
+        }
+
+        #endregion
+
+        #region ITargetBlock
+
+        public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, T1 messageValue, ISourceBlock<T1>? source, bool consumeToAccept)
+        {
+            return _workflowProcess.OfferMessage(messageHeader, messageValue, source, consumeToAccept);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region IExecuteAsyncWithProgress
+
+        public virtual bool CanExecute(T1 parameter)
+        {
+            return true;
+        }
+
+        public virtual async Task<T2> ExecuteAsync(T1 parameter, CancellationToken cancelToken = default)
+        {
+            if (cancelToken.IsCancellationRequested)
+                return await Task.FromCanceled<T2>(cancelToken);
+
+            if (!CanExecute(parameter))
+                return await Task.FromException<T2>(new ExecuteException());
+
+            try
+            {
+                return await Process(parameter, cancelToken);
+            }
+            catch (Exception e)
+            {
+                return await Task.FromException<T2>(e);
+            }
+        }
+
+        Task? IExecuteAsync.ExecuteAsync(object parameter)
+        {
+            if (parameter is T1 p)
+                return Task.Run(() => ExecuteAsync(p, default));
+            return default;
+        }
+
+        bool ICommand.CanExecute(object? parameter)
+        {
+            if (parameter is T1 p)
+                return CanExecute(p);
+            return default;
+        }
+
+        void ICommand.Execute(object? parameter)
+        {
+            if (parameter is T1 p)
+                Task.Run(() => ExecuteAsync(p, default));
+        }
+
+        #endregion
+
+        public abstract Task<T2> Process(T1 input, CancellationToken cancelToken = default);
+    }
+}
--- ATSPM/EventLogUtility/ATSPMProcessWorkflow.cs
+++ ATSPM/EventLogUtility/ATSPMProcessWorkflow.cs
@@ -5,135 +5,66 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using System.Windows.Input;
+using ATSPM.Application.Analysis;
 using ATSPM.Data.Enums;
 using ATSPM.Data.Models;
+using ATSPM.Domain.BaseClasses;
 using ATSPM.Domain.Common;
 using ATSPM.Domain.Exceptions;
+using Google.Protobuf.Collections;
 using Google.Protobuf.WellKnownTypes;
 
 namespace ATSPM.EventLogUtility
 {
-    public abstract class ProcessWorkflowStepBase<T1, T2, T3> : IExecuteAsyncWithProgress<T1, T2, T3>, IPropagatorBlock<T1, T2>, IDataflowBlock, ISourceBlock<T2>, ITargetBlock<T1>
+    public class IdentifyUnknownTerminationTypes : ProcessStepBase<List<ControllerEventLog>, List<ControllerEventLog>>
     {
-        public event EventHandler? CanExecuteChanged;
+        public IdentifyUnknownTerminationTypes(ExecutionDataflowBlockOptions? dataflowBlockOptions = default) : base(dataflowBlockOptions){}
 
-        private readonly IPropagatorBlock<T1, T2> _workflowProcess;
-
-        public ProcessWorkflowStepBase(ExecutionDataflowBlockOptions dataflowBlockOptions = default, IProgress<T3> progress = default)
+        public override Task<List<ControllerEventLog>> Process(List<ControllerEventLog> input, CancellationToken cancelToken = default)
         {
-            dataflowBlockOptions.NameFormat = this.GetType().Name;
-            _workflowProcess = new TransformBlock<T1, T2>(p => ExecuteAsync(p, progress, dataflowBlockOptions.CancellationToken), dataflowBlockOptions);
-        }
-
-        #region IPropagatorBlock
-
-        #region IDataflowBlock
-
-        public Task Completion => _workflowProcess.Completion;
-
-        public void Complete()
-        {
-            _workflowProcess.Complete();
-        }
-
-        public void Fault(Exception exception)
-        {
-            _workflowProcess.Fault(exception);
-        }
-
-        #endregion
-
-        #region ISourceBlock
-
-        public T2? ConsumeMessage(DataflowMessageHeader messageHeader, ITargetBlock<T2> target, out bool messageConsumed)
-        {
-            return _workflowProcess.ConsumeMessage(messageHeader, target, out messageConsumed);
-        }
-
-        public IDisposable LinkTo(ITargetBlock<T2> target, DataflowLinkOptions linkOptions)
-        {
-            return _workflowProcess.LinkTo(target, linkOptions);
-        }
-
-        public void ReleaseReservation(DataflowMessageHeader messageHeader, ITargetBlock<T2> target)
-        {
-            _workflowProcess?.ReleaseReservation(messageHeader, target);
-        }
-
-        public bool ReserveMessage(DataflowMessageHeader messageHeader, ITargetBlock<T2> target)
-        {
-            return _workflowProcess.ReserveMessage(messageHeader, target);
-        }
-
-        #endregion
-
-        #region ITargetBlock
-
-        public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, T1 messageValue, ISourceBlock<T1>? source, bool consumeToAccept)
-        {
-            return _workflowProcess.OfferMessage(messageHeader,messageValue, source, consumeToAccept);
-        }
-
-        #endregion
-
-        #endregion
-
-        #region IExecuteAsyncWithProgress
-
-        public abstract Task<T2> ExecuteAsync(T1 parameter, IProgress<T3>? progress = null, CancellationToken cancelToken = default);
-
-        public virtual bool CanExecute(T1 parameter)
-        {
-            return true;
-        }
-
-        public virtual Task<T2> ExecuteAsync(T1 parameter, CancellationToken cancelToken = default)
-        {
-            return ExecuteAsync(parameter, default, cancelToken);
-        }
-
-        Task? IExecuteAsync.ExecuteAsync(object parameter)
-        {
-            if (parameter is T1 p)
-                return Task.Run(() => ExecuteAsync(p, default, default));
-            return default;
-        }
-
-        bool ICommand.CanExecute(object? parameter)
-        {
-            if (parameter is T1 p)
-                return CanExecute(p);
-            return default;
-        }
-
-        void ICommand.Execute(object? parameter)
-        {
-            if (parameter is T1 p)
-                Task.Run(() => ExecuteAsync(p, default, default));
-        }
-
-        #endregion
-    }
-
-    public class IdentifyUnknownTerminationTypes<T> : ProcessWorkflowStepBase<List<ControllerEventLog>, List<ControllerEventLog>, T>
-    {
-        public IdentifyUnknownTerminationTypes(ExecutionDataflowBlockOptions dataflowBlockOptions = default, IProgress<T> progress = default) : base(dataflowBlockOptions, progress){}
-
-        public override Task<List<ControllerEventLog>> ExecuteAsync(List<ControllerEventLog> parameter, IProgress<T>? progress = null, CancellationToken cancelToken = default)
-        {
-            if (cancelToken.IsCancellationRequested)
-                return Task.FromCanceled<List<ControllerEventLog>>(cancelToken);
-
-            if (!CanExecute(parameter))
-                return Task.FromException<List<ControllerEventLog>>(new ExecuteException());
-
-            return Task.FromResult(parameter.Where(p => p.EventCode == (int)DataLoggerEnum.PhaseGreenTermination).ToList());
+            return Task.FromResult(input.Where(p => p.EventCode == (int)DataLoggerEnum.PhaseGreenTermination).ToList());
         }
     }
 
+    public class IdentifyTerminationTypesAndTimes : ProcessStepBase<List<ControllerEventLog>, List<ControllerEventLog>>
+    {
+        public IdentifyTerminationTypesAndTimes(ExecutionDataflowBlockOptions? dataflowBlockOptions = default) : base(dataflowBlockOptions) { }
 
+        public override Task<List<ControllerEventLog>> Process(List<ControllerEventLog> input, CancellationToken cancelToken = default)
+        {
+            return Task.FromResult(input);
+        }
+    }
 
+    public class IdentifyAllTerminationTypesandTimes : ProcessStepBase<Tuple<List<ControllerEventLog>, List<ControllerEventLog>>, List<ControllerEventLog>>
+    {
+        public IdentifyAllTerminationTypesandTimes(ExecutionDataflowBlockOptions? dataflowBlockOptions = default) : base(dataflowBlockOptions) { }
 
+        public override Task<List<ControllerEventLog>> Process(Tuple<List<ControllerEventLog>, List<ControllerEventLog>> input, CancellationToken cancelToken = default)
+        {
+            return Task.FromResult(input.Item1.Union(input.Item2).ToList());
+        }
+    }
+
+    public class IdentifyPedActivity : ProcessStepBase<List<ControllerEventLog>, List<ControllerEventLog>>
+    {
+        public IdentifyPedActivity(ExecutionDataflowBlockOptions? dataflowBlockOptions = default) : base(dataflowBlockOptions) { }
+
+        public override Task<List<ControllerEventLog>> Process(List<ControllerEventLog> input, CancellationToken cancelToken = default)
+        {
+            return Task.FromResult(input);
+        }
+    }
+
+    public class PhaseTerminationMeasureInformation : ProcessStepBase<Tuple<List<ControllerEventLog>, List<ControllerEventLog>>, AnalysisPhase>
+    {
+        public PhaseTerminationMeasureInformation(ExecutionDataflowBlockOptions? dataflowBlockOptions = default) : base(dataflowBlockOptions) { }
+
+        public override Task<AnalysisPhase> Process(Tuple<List<ControllerEventLog>, List<ControllerEventLog>> input, CancellationToken cancelToken = default)
+        {
+            return Task.FromResult(new AnalysisPhase() { Data = input.Item1.Count });
+        }
+    }
 
     public class AnalysisPhase
     {
@@ -141,17 +72,16 @@ namespace ATSPM.EventLogUtility
     }
 
 
-    public class ATSPMProcessWorkflow
+    public class PhaseTerminationProcess : ServiceObjectBase
     {
-        public TransformBlock<Tuple<List<ControllerEventLog>, List<ControllerEventLog>>, AnalysisPhase> PhaseTerminationMeasureInformation { get; set; }
+        public PhaseTerminationProcess() 
+        {
+        }
 
-        public BroadcastBlock<List<ControllerEventLog>> FilteredTerminationStatus { get; set; } 
-
-
-        public ATSPMProcessWorkflow() 
+        public override void Initialize()
         {
             //Controller event logs EC7
-            FilteredTerminationStatus = new BroadcastBlock<List<ControllerEventLog>>(d => d.Where(i => i.EventCode == (int)DataLoggerEnum.PhaseGreenTermination).ToList());
+            var FilteredTerminationStatus = new BroadcastBlock<List<ControllerEventLog>>(d => d.Where(i => i.EventCode == (int)DataLoggerEnum.PhaseGreenTermination).ToList());
 
             //Controller event logs EC 4,5,6
             var FilteredTerminationsData = new BroadcastBlock<List<ControllerEventLog>>(null);
@@ -160,21 +90,26 @@ namespace ATSPM.EventLogUtility
             var FilteredPedPhases = new BufferBlock<List<ControllerEventLog>>();
 
             //Identify unknown termination types
-            var IdentifyUnknownTerminationTypes = new TransformBlock<List<ControllerEventLog>, List<ControllerEventLog>>(d => IdentifyUnknownTerminationTypesProcess(d));
+            //var IdentifyUnknownTerminationTypes = new TransformBlock<List<ControllerEventLog>, List<ControllerEventLog>>(d => IdentifyUnknownTerminationTypesProcess(d));
+            var IdentifyUnknownTerminationTypes = new IdentifyUnknownTerminationTypes();
 
             //Identify termination types and times
-            var IdentifyTerminationTypesAndTimes = new TransformBlock<List<ControllerEventLog>, List<ControllerEventLog>>(d => IdentifyTerminationTypesAndTimesProcess(d));
+            //var IdentifyTerminationTypesAndTimes = new TransformBlock<List<ControllerEventLog>, List<ControllerEventLog>>(d => IdentifyTerminationTypesAndTimesProcess(d));
+            var IdentifyTerminationTypesAndTimes = new IdentifyTerminationTypesAndTimes();
 
             //Identify all termination types and times
             var IdentifyAllTerminationTypesandTimesJoin = new JoinBlock<List<ControllerEventLog>, List<ControllerEventLog>>();
-            var IdentifyAllTerminationTypesandTimes = new TransformBlock<Tuple<List<ControllerEventLog>, List<ControllerEventLog>>, List<ControllerEventLog>>(d => IdentifyAllTerminationTypesandTimesProcess(d));
+            //var IdentifyAllTerminationTypesandTimes = new TransformBlock<Tuple<List<ControllerEventLog>, List<ControllerEventLog>>, List<ControllerEventLog>>(d => IdentifyAllTerminationTypesandTimesProcess(d));
+            var IdentifyAllTerminationTypesandTimes = new IdentifyAllTerminationTypesandTimes();
 
             //Identify Ped Activity
-            var IdentifyPedActivity = new TransformBlock<List<ControllerEventLog>, List<ControllerEventLog>>(d => IdentifyPedActivityProcess(d));
+            //var IdentifyPedActivity = new TransformBlock<List<ControllerEventLog>, List<ControllerEventLog>>(d => IdentifyPedActivityProcess(d));
+            var IdentifyPedActivity = new IdentifyPedActivity();
 
             //Phase termination measure information
             var PhaseTerminationMeasureInformationJoin = new JoinBlock<List<ControllerEventLog>, List<ControllerEventLog>>();
-            var PhaseTerminationMeasureInformation = new TransformBlock<Tuple<List<ControllerEventLog>, List<ControllerEventLog>>, AnalysisPhase>(d => PhaseTerminationMeasureInformationProcess(d));
+            //var PhaseTerminationMeasureInformation = new TransformBlock<Tuple<List<ControllerEventLog>, List<ControllerEventLog>>, AnalysisPhase>(d => PhaseTerminationMeasureInformationProcess(d));
+            var PhaseTerminationMeasureInformation = new PhaseTerminationMeasureInformation();
 
             //link FilteredTerminationStatus
             FilteredTerminationStatus.LinkTo(IdentifyUnknownTerminationTypes);
@@ -201,94 +136,9 @@ namespace ATSPM.EventLogUtility
 
             PhaseTerminationMeasureInformation.LinkTo(StartCharting);
 
-            
-
-            //FilteredTerminationStatus.Post(list1);
-            //FilteredTerminationsData.Post(list2);
-            //FilteredPedPhases.Post(list3);
+            base.Initialize();
         }
 
-        public List<ControllerEventLog> IdentifyUnknownTerminationTypesProcess(List<ControllerEventLog> list)
-        {
-            foreach (var i in list)
-            {
-                Console.WriteLine($"IdentifyUnknownTerminationTypesProcess: {i}");
-            }
-
-            return list;
-        }
-
-        public List<ControllerEventLog> IdentifyTerminationTypesAndTimesProcess(List<ControllerEventLog> list)
-        {
-            foreach (var i in list)
-            {
-                Console.WriteLine($"IdentifyTerminationTypesAndTimesProcess: {i}");
-            }
-
-            return list;
-        }
-
-        public List<ControllerEventLog> IdentifyPedActivityProcess(List<ControllerEventLog> list)
-        {
-            foreach (var i in list)
-            {
-                Console.WriteLine($"IdentifyPedActivityProcess: {i}");
-            }
-
-            return list;
-        }
-
-        public List<ControllerEventLog> IdentifyAllTerminationTypesandTimesProcess(Tuple<List<ControllerEventLog>, List<ControllerEventLog>> lists)
-        {
-            foreach (var i in lists.Item1)
-            {
-                Console.WriteLine($"IdentifyAllTerminationTypesandTimesProcess: item1 - {i}");
-            }
-
-            foreach (var i in lists.Item2)
-            {
-                Console.WriteLine($"IdentifyAllTerminationTypesandTimesProcess: item2 - {i}");
-            }
-
-            var list = lists.Item1.Union(lists.Item2).ToList();
-
-            foreach (var i in list)
-            {
-                Console.WriteLine($"IdentifyAllTerminationTypesandTimesProcess: {i}");
-            }
-
-            return list;
-        }
-
-        public AnalysisPhase PhaseTerminationMeasureInformationProcess(Tuple<List<ControllerEventLog>, List<ControllerEventLog>> lists)
-        {
-            foreach (var i in lists.Item1)
-            {
-                Console.WriteLine($"PhaseTerminationMeasureInformationProcess: item1 - {i}");
-            }
-
-            foreach (var i in lists.Item2)
-            {
-                Console.WriteLine($"PhaseTerminationMeasureInformationProcess: item2 - {i}");
-            }
-
-            Console.WriteLine($"********************************************************************");
-
-            foreach(var phase in lists.Item1.Union(lists.Item2).Select(s => s.EventParam).Distinct().OrderBy(o => o))
-            {
-                Console.WriteLine($"Events for phase {phase}: {lists.Item1.Where(w => w.EventParam == phase).Count()} - {lists.Item2.Where(w => w.EventParam == phase).Count()}");
-            }
-
-            Console.WriteLine($"********************************************************************");
-
-            foreach (var phase in lists.Item1.Union(lists.Item2).GroupBy(g => g.EventParam).OrderBy(o => o.Key))
-            {
-                Console.WriteLine($"Events for phase {phase.Key}: {phase.Count()}");
-            }
-
-            Console.WriteLine($"********************************************************************");
-
-            return new AnalysisPhase() { Data = lists.Item1.Count};
-        }
+        
     }
 }
