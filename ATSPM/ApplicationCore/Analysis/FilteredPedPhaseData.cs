1c8b65761f91215fa79dec62dc115bb5f6d992a4
Christian Baker <christianbaker@utah.gov>
2023-04-18 10:26:24 -0600

    Added controller event log workflow broadcast filters

---
+++ ATSPM/ApplicationCore/Analysis/FilteredPedPhaseData.cs
@@ -0,0 +1,21 @@
+﻿using ATSPM.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace ATSPM.Application.Analysis
+{
+    public class FilteredPedPhaseData : FilterStepBase
+    {
+        public FilteredPedPhaseData(DataflowBlockOptions? dataflowBlockOptions = default) : base(dataflowBlockOptions)
+        {
+            filteredList.Add((int)DataLoggerEnum.PedestrianBeginWalk);
+            filteredList.Add((int)DataLoggerEnum.PedestrianBeginChangeInterval);
+            filteredList.Add((int)DataLoggerEnum.PedestrianOverlapBeginWalk);
+            filteredList.Add((int)DataLoggerEnum.PedestrianOverlapBeginClearance);
+        }
+    }
+}
---
+++ ATSPM/ApplicationCore/Analysis/FilteredPhaseIntervalChanges.cs
@@ -0,0 +1,21 @@
+﻿using ATSPM.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace ATSPM.Application.Analysis
+{
+    public class FilteredPhaseIntervalChanges : FilterStepBase
+    {
+        public FilteredPhaseIntervalChanges(DataflowBlockOptions? dataflowBlockOptions = default) : base(dataflowBlockOptions)
+        {
+            filteredList.Add((int)DataLoggerEnum.PhaseBeginGreen);
+            filteredList.Add((int)DataLoggerEnum.PhaseBeginYellowChange);
+            filteredList.Add((int)DataLoggerEnum.PhaseEndYellowChange);
+            filteredList.Add((int)DataLoggerEnum.PhaseEndRedClearance);
+        }
+    }
+}
