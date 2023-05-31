a407518c6b0a2fef1fea6c6ff165a7a1a3b72580
Christian Baker <christianbaker@utah.gov>
2023-05-31 14:17:00 -0600

    Added CaclulatePhaseTotals and CalculateVehicleArrivals

--- ATSPM/ApplicationCore/Analysis/ApproachDelay/ApproachDelayResult.cs
+++ ATSPM/ApplicationCore/Analysis/ApproachDelay/ApproachDelayResult.cs
@@ -1,34 +1,34 @@
 ﻿using ATSPM.Application.Analysis.Common;
+using ATSPM.Domain.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace ATSPM.Application.Analysis.ApproachDelay
 {
-    public class ApproachDelayResult
+    public class ApproachDelayResult : StartEndRange
     {
         //public string ChartName { get; }
-        public string SignalId
-        {
-            get;
-            set;
-        }
+        public string SignalId { get; set; }
         //public string SignalLocation { get; }
         public int Phase { get; set; }
         //public string PhaseDescription { get; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
-        public double AverageDelay => Vehicles.Average(a => a.Delay);
-        public double TotalDelay => Vehicles.Sum(s => s.Delay) / 3600;
+        public double AverageDelay { get; set; }
+        public double TotalDelay { get; set; }
+
+        //public double AverageDelay => Vehicles.Average(a => a.Delay);
+        //public double TotalDelay => Vehicles.Sum(s => s.Delay) / 3600;
         //public List<ApproachDelayPlan> Plans { get; }
         //public List<ApproachDelayDataPoint> ApproachDelayDataPoints { get; }
         //public List<ApproachDelayPerVehicleDataPoint> ApproachDelayPerVehicleDataPoints { get; }
 
-        public List<Vehicle> Vehicles { get; set; } = new();
+        //public List<Vehicle> Vehicles { get; set; } = new();
 
         public override string ToString()
         {
-            return $"Signal: {SignalId} Phase: {Phase} Start: {Start:yyyy-MM-dd'T'HH:mm:ss.f} End: {End:yyyy-MM-dd'T'HH:mm:ss.f} Avg: {AverageDelay:0.00} Total: {TotalDelay:0.0h} Vehicles: {Vehicles.Count}";
+            return $"Signal: {SignalId} Phase: {Phase} Start: {Start:yyyy-MM-dd'T'HH:mm:ss.f} End: {End:yyyy-MM-dd'T'HH:mm:ss.f} Avg: {AverageDelay:0.00} Total: {TotalDelay:0.0h}";
         }
     }
 }
