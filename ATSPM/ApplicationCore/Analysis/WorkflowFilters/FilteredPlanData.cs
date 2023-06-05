f0dd1862ef82b0a0df3d451e4ea9e8403c50e9e4
Christian Baker <christianbaker@utah.gov>
2023-06-05 08:29:39 -0600

    Added FilteredPlanData

---
+++ ATSPM/ApplicationCore/Analysis/WorkflowFilters/FilteredPlanData.cs
@@ -0,0 +1,18 @@
+﻿using ATSPM.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace ATSPM.Application.Analysis.WorkflowFilters
+{
+    public class FilteredPlanData : FilterStepBase
+    {
+        public FilteredPlanData(DataflowBlockOptions dataflowBlockOptions = default) : base(dataflowBlockOptions)
+        {
+            filteredList.Add((int)DataLoggerEnum.CoordPatternChange);
+        }
+    }
+}
