30bad17cf21034421409c22f2b647be1330b129c
Christian Baker <christianbaker@utah.gov>
2023-06-21 13:50:36 -0600

    Added controllers to configapi

---
+++ ConfigApi/Controllers/ApplicationSettingController.cs
@@ -0,0 +1,15 @@
+﻿using ATSPM.Application.Repositories;
+using ATSPM.Data.Models;
+
+namespace ATSPM.ConfigApi.Controllers
+{
+    public class ApplicationSettingController : AtspmConfigControllerBase<ApplicationSetting, int>
+    {
+        private readonly IApplicationSettingsRepository _repository;
+
+        public ApplicationSettingController(IApplicationSettingsRepository repository) : base(repository)
+        {
+            _repository = repository;
+        }
+    }
+}
---
+++ ConfigApi/Controllers/LaneTypeController.cs
@@ -0,0 +1,15 @@
+﻿using ATSPM.Application.Repositories;
+using ATSPM.Data.Models;
+
+namespace ATSPM.ConfigApi.Controllers
+{
+    public class LaneTypeController : AtspmConfigControllerBase<LaneType, int>
+    {
+        private readonly ILaneTypeRepository _repository;
+
+        public LaneTypeController(ILaneTypeRepository repository) : base(repository)
+        {
+            _repository = repository;
+        }
+    }
+}
