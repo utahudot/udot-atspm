bac5482f6c5ce2f511375dca7aa82c5d2fb629b0
Christian Baker <christianbaker@utah.gov>
2023-06-21 13:50:36 -0600

    Added controllers to configapi

---
+++ ConfigApi/Controllers/ActionLogController.cs
@@ -0,0 +1,15 @@
+﻿using ATSPM.Application.Repositories;
+using ATSPM.Data.Models;
+
+namespace ATSPM.ConfigApi.Controllers
+{
+    public class ActionLogController : AtspmConfigControllerBase<ActionLog, int>
+    {
+        private readonly IActionLogRepository _repository;
+
+        public ActionLogController(IActionLogRepository repository) : base(repository)
+        {
+            _repository = repository;
+        }
+    }
+}
---
+++ ConfigApi/Controllers/DirectionTypeController.cs
@@ -0,0 +1,15 @@
+﻿using ATSPM.Application.Repositories;
+using ATSPM.Data.Models;
+
+namespace ATSPM.ConfigApi.Controllers
+{
+    public class DirectionTypeController : AtspmConfigControllerBase<DirectionType, int>
+    {
+        private readonly IDirectionTypeRepository _repository;
+
+        public DirectionTypeController(IDirectionTypeRepository repository) : base(repository)
+        {
+            _repository = repository;
+        }
+    }
+}
