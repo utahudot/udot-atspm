066c55e4f7895decb5e6964512128de34fd1f35f
Christian Baker <christianbaker@utah.gov>
2023-06-21 07:41:52 -0600

    Added empty workflow tests

---
+++ ATSPM/ApplicationCoreTests/Analysis/Workflows/ArrivalOnRedWorkflowTests.cs
@@ -0,0 +1,35 @@
+﻿using ATSPM.Application.Analysis.Common;
+using ATSPM.Application.Analysis.WorkflowSteps;
+using ATSPM.Application.Enums;
+using ATSPM.Data.Enums;
+using ATSPM.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ApplicationCoreTests.Analysis.Workflows
+{
+    public class ArrivalOnRedWorkflowTests : IDisposable
+    {
+        private readonly ITestOutputHelper _output;
+
+        public ArrivalOnRedWorkflowTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        //[Trait(nameof(AssignCyclesToVehicles), "Arrival on Yellow")]
+        public async void NotCompleted()
+        {
+            Assert.False(true);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
