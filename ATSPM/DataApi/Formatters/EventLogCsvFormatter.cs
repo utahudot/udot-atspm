f23aec717ea8a830ea867babf57d921dcb3cf955
Christian Baker <christianbaker@utah.gov>
2023-06-26 14:59:13 -0600

    added custom X-Timestamp-Format header for timestamp format

--- ATSPM/DataApi/Formatters/EventLogCsvFormatter.cs
+++ ATSPM/DataApi/Formatters/EventLogCsvFormatter.cs
@@ -1,5 +1,8 @@
 ﻿using ATSPM.Data.Models;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Primitives;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Text;
 
 namespace ATSPM.DataApi.Formatters
@@ -17,7 +20,10 @@ namespace ATSPM.DataApi.Formatters
         {
             if (context.Object is IEnumerable<ControllerEventLog> result)
             {
-                var csv = result.Select(x => $"{x.SignalId},{x.Timestamp:yyyy-MM-dd'T'HH:mm:ss.f},{x.EventCode},{x.EventParam}").ToList();
+                context.HttpContext.Request.Headers.TryGetValue("X-Timestamp-Format", out StringValues timestampFormat);
+                timestampFormat = string.IsNullOrEmpty(timestampFormat) ? "yyyy-MM-dd'T'HH:mm:ss.f" : timestampFormat;
+
+                var csv = result.Select(x => $"{x.SignalId},{x.Timestamp.ToString(timestampFormat)},{x.EventCode},{x.EventParam}").ToList();
                 csv.Insert(0, "SignalID,Timestamp,EventCode,EventParam");
 
                 return context.HttpContext.Response.WriteAsync(string.Join("\n", csv), selectedEncoding);
@@ -30,4 +36,26 @@ namespace ATSPM.DataApi.Formatters
             return typeof(IEnumerable<ControllerEventLog>).IsAssignableFrom(type);
         }
     }
+}
+
+public class CustomHeaderSwaggerAttribute : IOperationFilter
+{
+
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        if (operation.Parameters == null)
+            operation.Parameters = new List<OpenApiParameter>();
+
+        operation.Parameters.Add(new OpenApiParameter
+        {
+            Name = "X-Timestamp-Format",
+            In = ParameterLocation.Header,
+            Required = false,
+            Schema = new OpenApiSchema
+            {
+                Type = "string"
+            }
+        });
+    }
+
 }
