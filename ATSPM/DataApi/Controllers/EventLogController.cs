8b2018f3b93e8bb47b18380ee1b64ef40dd29f70
Christian Baker <christianbaker@utah.gov>
2023-06-28 11:17:28 -0600

    Cleaned up DataApi

--- ATSPM/DataApi/Controllers/EventLogController.cs
+++ ATSPM/DataApi/Controllers/EventLogController.cs
@@ -33,13 +33,20 @@ namespace ATSPM.DataApi.Controllers
         /// <param name="end">date/time of last event</param>
         /// <returns>List of ControllerEventLogs</returns>
         /// <response code="200">Call completed successfully</response>
+        /// <response code="400">Invalid request (start/end range)</response>
         /// <response code="404">Resource not found</response>
         [ApiVersion("1.0")]
         [HttpGet("{signalIdentifier}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<List<ControllerEventLog>> GetSignalEventsBetweenDates(string signalIdentifier, DateTime start, DateTime end)
         {
+            _log.LogDebug("signal: {signal} start: {start} end: {end}", signalIdentifier, start, end);
+
+            if (start == DateTime.MinValue || end == DateTime.MinValue)
+                return BadRequest("Invalid datetime range on start/end");
+            
             var result = _repository.GetSignalEventsBetweenDates(signalIdentifier, start, end);
 
             if (result == null)
@@ -57,13 +64,20 @@ namespace ATSPM.DataApi.Controllers
         /// <param name="end">date/time of last event</param>
         /// <returns>List of ControllerEventLogs</returns>
         /// <response code="200">Call completed successfully</response>
+        /// <response code="400">Invalid request (start/end range or event code)</response>
         /// <response code="404">Resource not found</response>
         [ApiVersion("1.0")]
         [HttpGet("{signalIdentifier}/{eventCode}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<List<ControllerEventLog>> GetEventLogs(string signalIdentifier, int eventCode, DateTime start, DateTime end)
+        public ActionResult<List<ControllerEventLog>> GetSignalEventsByEventCode(string signalIdentifier, int eventCode, DateTime start, DateTime end)
         {
+            _log.LogDebug("signal: {signal} event: {event} start: {start} end: {end}", signalIdentifier, eventCode, start, end);
+
+            if (start == DateTime.MinValue || end == DateTime.MinValue)
+                return BadRequest("Invalid datetime range on start/end");
+
             var result = _repository.GetSignalEventsByEventCode(signalIdentifier, start, end, eventCode);
 
             if (result == null)
