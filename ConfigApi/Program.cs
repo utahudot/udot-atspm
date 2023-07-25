0a154c2ffe78a75844dc3fc4a687e9d472d1d15d
Christian Baker <christianbaker@utah.gov>
2023-07-25 11:31:57 -0600

    don't even remember

--- ConfigApi/Program.cs
+++ ConfigApi/Program.cs
@@ -1,36 +1,78 @@
+using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using Asp.Versioning.Conventions;
 using ATSPM.Application.Repositories;
+using ATSPM.ConfigApi.Controllers;
 using ATSPM.ConfigApi.EntityDataModel;
 using ATSPM.Data;
 using ATSPM.Infrastructure.Extensions;
 using ATSPM.Infrastructure.Repositories;
 using Google.Api;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.OData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using OData.Swagger.Services;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+//using static Microsoft.AspNetCore.OData.Query.AllowedQueryOptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//https://github.com/dotnet/aspnet-api-versioning/wiki/OData-Versioned-Metadata
+
 builder.Services.AddControllers(options =>
 {
     options.ReturnHttpNotAcceptable = true;
-})
-.AddXmlDataContractSerializerFormatters()
+}).AddXmlDataContractSerializerFormatters()
+.AddOData(options =>
+{
+    options.Count().Select().OrderBy();
+    options.RouteOptions.EnableKeyInParenthesis = false;
+    options.RouteOptions.EnableNonParenthesisForEmptyParameterFunction = true;
+    options.RouteOptions.EnablePropertyNameCaseInsensitive = true;
+    options.RouteOptions.EnableQualifiedOperationCall = false;
+    options.RouteOptions.EnableUnqualifiedOperationCall = true;
+});
+builder.Services.AddProblemDetails();
+builder.Services.AddApiVersioning(options =>
+{
+    // reporting api versions will return the headers
+    // "api-supported-versions" and "api-deprecated-versions"
+options.ReportApiVersions = true;
 
-//https://github.com/microsoft/OpenAPI.NET.OData
-.AddOData(opt => opt.AddRouteComponents("config", new ConfigEdm().GetEntityDataModel())
-.Select()
-.Count()
-.Expand()
-.OrderBy()
-.Filter()
-.SkipToken()
-.SetMaxTop(5000));
+options.Policies.Sunset(0.9)
+    .Effective(DateTimeOffset.Now.AddDays(60))
+    .Link("policy.html")
+    .Title("Versioning Policy")
+    .Type("text/html");
+})
+    .AddOData(options => options.AddRouteComponents("api/v{version:apiVersion}"))
+    .AddODataApiExplorer(
+    options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+
+                    // configure query options (which cannot otherwise be configured by OData conventions)
+                    //options.QueryOptions.Controller<ActionsController>()
+                    //                    .Action(c => c.Get(default))
+                    //                        .Allow(Skip | Count)
+                    //                        .AllowTop(100);
+                    });
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Host.ConfigureServices((h, s) =>
 {
@@ -40,26 +82,221 @@ s.AddScoped<IAreaRepository, AreaEFRepository>();
 s.AddScoped<IControllerTypeRepository, ControllerTypeEFRepository>();
 s.AddScoped<ISignalRepository, SignalEFRepository>();
 });
-builder.Services.AddSwaggerGen();
+
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+builder.Services.AddSwaggerGen(
+    options =>
+    {
+        // add a custom operation filter which sets default values
+        options.OperationFilter<SwaggerDefaultValues>();
+
+        var fileName = typeof(Program).Assembly.GetName().Name + ".xml";
+        var filePath = Path.Combine(AppContext.BaseDirectory, fileName);
+
+        // integrate xml comments
+        //options.IncludeXmlComments(filePath);
+    });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("../swagger/v1/swagger.json", "ATSPM.Config v1"));
+    // navigate to ~/$odata to determine whether any endpoints did not match an odata route template
+    app.UseODataRouteDebug();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI(
+    options =>
+    {
+        var descriptions = app.DescribeApiVersions();
+
+        // build a swagger endpoint for each discovered API version
+        foreach (var description in descriptions)
+        {
+            var url = $"/swagger/{description.GroupName}/swagger.json";
+            var name = description.GroupName.ToUpperInvariant();
+            options.SwaggerEndpoint(url, name);
+        }
+    });
+
 app.UseHttpsRedirection();
-
-app.UseRouting();
-
 app.UseAuthorization();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
-
+app.MapControllers();
 app.Run();
+
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        /// <summary>
+                        /// Configures the Swagger generation options.
+                        /// </summary>
+                        /// <remarks>This allows API versioning to define a Swagger document per API version after the
+                        /// <see cref="IApiVersionDescriptionProvider"/> service has been resolved from the service container.</remarks>
+public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+{
+    private readonly IApiVersionDescriptionProvider provider;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ConfigureSwaggerOptions"/> class.
+    /// </summary>
+    /// <param name="provider">The <see cref="IApiVersionDescriptionProvider">provider</see> used to generate Swagger documents.</param>
+    public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+
+    /// <inheritdoc />
+    public void Configure(SwaggerGenOptions options)
+    {
+        // add a swagger document for each discovered API version
+        // note: you might choose to skip or document deprecated API versions differently
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+        }
+    }
+
+    private static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+    {
+        var text = new StringBuilder("An example application with OData, OpenAPI, Swashbuckle, and API versioning.");
+        var info = new OpenApiInfo()
+        {
+            Title = "Sample API",
+            Version = description.ApiVersion.ToString(),
+            Contact = new OpenApiContact() { Name = "Bill Mei", Email = "bill.mei@somewhere.com" },
+            License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+        };
+
+        if (description.IsDeprecated)
+        {
+            text.Append(" This API version has been deprecated.");
+        }
+
+        if (description.SunsetPolicy is SunsetPolicy policy)
+        {
+            if (policy.Date is DateTimeOffset when)
+            {
+                text.Append(" The API will be sunset on ")
+                    .Append(when.Date.ToShortDateString())
+                    .Append('.');
+            }
+
+            if (policy.HasLinks)
+            {
+                text.AppendLine();
+
+                for (var i = 0; i < policy.Links.Count; i++)
+                {
+                    var link = policy.Links[i];
+
+                    if (link.Type == "text/html")
+                    {
+                        text.AppendLine();
+
+                        if (link.Title.HasValue)
+                        {
+                            text.Append(link.Title.Value).Append(": ");
+                        }
+
+                        text.Append(link.LinkTarget.OriginalString);
+                    }
+                }
+            }
+        }
+
+        info.Description = text.ToString();
+
+        return info;
+    }
+}
+
+
+
+
+
+
+
+
+/// <summary>
+/// Represents the OpenAPI/Swashbuckle operation filter used to document the implicit API version parameter.
+/// </summary>
+/// <remarks>This <see cref="IOperationFilter"/> is only required due to bugs in the <see cref="SwaggerGenerator"/>.
+/// Once they are fixed and published, this class can be removed.</remarks>
+public class SwaggerDefaultValues : IOperationFilter
+{
+    /// <summary>
+    /// Applies the filter to the specified operation using the given context.
+    /// </summary>
+    /// <param name="operation">The operation to apply the filter to.</param>
+    /// <param name="context">The current operation filter context.</param>
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        var apiDescription = context.ApiDescription;
+
+        operation.Deprecated |= apiDescription.IsDeprecated();
+
+        // REF: https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/1752#issue-663991077
+        foreach (var responseType in context.ApiDescription.SupportedResponseTypes)
+        {
+            // REF: https://github.com/domaindrivendev/Swashbuckle.AspNetCore/blob/b7cf75e7905050305b115dd96640ddd6e74c7ac9/src/Swashbuckle.AspNetCore.SwaggerGen/SwaggerGenerator/SwaggerGenerator.cs#L383-L387
+            var responseKey = responseType.IsDefaultResponse ? "default" : responseType.StatusCode.ToString();
+            var response = operation.Responses[responseKey];
+
+            foreach (var contentType in response.Content.Keys)
+            {
+                if (!responseType.ApiResponseFormats.Any(x => x.MediaType == contentType))
+                {
+                    response.Content.Remove(contentType);
+                }
+            }
+        }
+
+        if (operation.Parameters == null)
+        {
+            return;
+        }
+
+        // REF: https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/412
+        // REF: https://github.com/domaindrivendev/Swashbuckle.AspNetCore/pull/413
+        foreach (var parameter in operation.Parameters)
+        {
+            var description = apiDescription.ParameterDescriptions.First(p => p.Name == parameter.Name);
+
+            parameter.Description ??= description.ModelMetadata?.Description;
+
+            if (parameter.Schema.Default == null && description.DefaultValue != null)
+            {
+                // REF: https://github.com/Microsoft/aspnet-api-versioning/issues/429#issuecomment-605402330
+                var json = JsonSerializer.Serialize(description.DefaultValue, description.ModelMetadata.ModelType);
+                parameter.Schema.Default = OpenApiAnyFactory.CreateFromJson(json);
+            }
+
+            parameter.Required |= description.IsRequired;
+        }
+    }
+}
