2cab44c4499426ef96d17c1770daff2c4091ece4
Christian Baker <christianbaker@utah.gov>
2023-07-25 11:31:57 -0600

    don't even remember

--- ConfigApi/Controllers/ActionsController.cs
+++ ConfigApi/Controllers/ActionsController.cs
@@ -1,7 +1,16 @@
-﻿using ATSPM.Application.Repositories;
+﻿using Asp.Versioning;
+using ATSPM.Application.Repositories;
+using ATSPM.Data;
+using ATSPM.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Deltas;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
 
 namespace ATSPM.ConfigApi.Controllers
 {
+    [ApiVersion(1.0)]
+    [ApiVersion(2.0)]
     public class ActionsController : AtspmConfigControllerBase<Data.Models.Action, int>
     {
         private readonly IActionRepository _repository;
@@ -9,6 +18,12 @@ namespace ATSPM.ConfigApi.Controllers
         public ActionsController(IActionRepository repository) : base(repository)
         {
             _repository = repository;
+        }
+
+        [MapToApiVersion(2.0)]
+        public override Task<IActionResult> Delete(int key)
+        {
+            return base.Delete(key);
         }
     }
 }
