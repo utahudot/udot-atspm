c92ea5ed52bdccaaea8cc41b93679f23b7740c00
Christian Baker <christianbaker@utah.gov>
2023-08-07 11:36:44 -0600

    Added ApiConventionProduces

--- ConfigApi/Controllers/SignalController.cs
+++ ConfigApi/Controllers/SignalController.cs
@@ -6,51 +6,47 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Formatter;
 using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Results;
+using Microsoft.OData;
 using static Microsoft.AspNetCore.Http.StatusCodes;
 using static Microsoft.AspNetCore.OData.Query.AllowedQueryOptions;
 
 namespace ATSPM.ConfigApi.Controllers
 {
     [ApiVersion(1.0)]
-    [ApiVersion(2.0)]
     public class SignalController : AtspmConfigControllerBase<Signal, int>
     {
         private readonly ISignalRepository _repository;
 
         public SignalController(ISignalRepository repository) : base(repository)
         {
-            _repository = repository;
-        }
+            _repository = repository; 
 
-        [MapToApiVersion(2.0)]
-        public override Task<IActionResult> Delete(int key)
-        {
-            return base.Delete(key);
-        }
-
-        [HttpPost]
-        [ProducesResponseType(Status204NoContent)]
-        [ProducesResponseType(Status400BadRequest)]
-        [ProducesResponseType(Status404NotFound)]
-        public IActionResult Promote(int key, [FromBody] ODataActionParameters parameters)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            var title = (string)parameters["title"];
-            return NoContent();
         }
 
         [HttpGet]
-        [Produces("application/json")]
+        [ProducesResponseType(Status400BadRequest)]
+        [ProducesResponseType(typeof(IEnumerable<Signal>), Status200OK)]
+        [EnableQuery]
+        public IActionResult GetLatestVersionOfAllSignals()
+        {
+            return Ok(_repository.GetLatestVersionOfAllSignals());
+        }
+
+        [HttpGet]
         [ProducesResponseType(Status400BadRequest)]
         [ProducesResponseType(typeof(Signal), Status200OK)]
-        public IActionResult Test(
-        int key,
-        ODataQueryOptions<Signal> options,
-        CancellationToken ct) =>
-        Ok(new Signal() { Id = key });
+        [ProducesResponseType(Status404NotFound)]
+        //[EnableQuery]
+        public SingleResult<Signal> GetLatestVersionOfSignal()
+        {
+            var i = _repository.GetLatestVersionOfSignal("1001");
+
+            //if (i == null)
+            //{
+            //    return NotFound();
+            //}
+
+            return SingleResult.Create(new[] { i }.AsQueryable());
+        }
     }
 }
