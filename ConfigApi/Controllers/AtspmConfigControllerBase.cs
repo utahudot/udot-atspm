b71affca30a8d941ed16675f412e1f9eeafefd19
Christian Baker <christianbaker@utah.gov>
2023-08-07 11:36:44 -0600

    Added ApiConventionProduces

--- ConfigApi/Controllers/AtspmConfigControllerBase.cs
+++ ConfigApi/Controllers/AtspmConfigControllerBase.cs
@@ -10,29 +10,52 @@ using Microsoft.AspNetCore.OData.Routing.Controllers;
 
 namespace ATSPM.ConfigApi.Controllers
 {
+    
+    /// <summary>
+    /// Base class for ATSPM Config oData Controllers
+    /// </summary>
+    /// <typeparam name="T">Type of ATSPM.Data.Model</typeparam>
+    /// <typeparam name="TKey">Model Key data type</typeparam>
     //[ApiController]
     //[Route("[controller]")]
+    [ApiConventionType(typeof(DefaultApiConventions))]
     public class AtspmConfigControllerBase<T, TKey> : ODataController where T : ATSPMModelBase
     {
-        //private readonly ConfigContext _configContext;
         private readonly IAsyncRepository<T> _repository;
 
+        /// <inheritdoc/>
         public AtspmConfigControllerBase(IAsyncRepository<T> repository)
         {
             //_configContext = configContext;
             _repository = repository;
         }
 
+        /// <summary>
+        /// Collection of type from oData query
+        /// </summary>
+        /// <returns>Action result of type</returns>
         // GET /Entity
         //[HttpGet()]
         [EnableQuery]
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Get))]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<IQueryable<T>> Get()
         {
             return Ok(_repository.GetList());
         }
 
+        /// <summary>
+        /// Key type from oData query
+        /// </summary>
+        /// <param name="key">Key value of object to get</param>
+        /// <returns>Action result of type</returns>
         // GET /Entity(1)
         //[HttpGet("{key}")]
+        [EnableQuery]
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Get))]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public virtual async Task<ActionResult<T?>> Get(TKey key)
         {
             var i = await _repository.LookupAsync(key);
@@ -45,8 +68,16 @@ namespace ATSPM.ConfigApi.Controllers
             return Ok(i);
         }
 
+        /// <summary>
+        /// Add new object of specified type
+        /// </summary>
+        /// <param name="item">Properties of object to add</param>
+        /// <returns>Action result with created object</returns>
         // POST /Entity 
         //[HttpPost("{key}")]
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Post))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public virtual async Task<IActionResult> Post([FromBody] T item)
         {
             if (!ModelState.IsValid)
@@ -61,6 +92,10 @@ namespace ATSPM.ConfigApi.Controllers
 
 
         // PUT /Entity(1)
+        //[ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Put))]
+        //[ProducesResponseType(StatusCodes.Status204NoContent)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(StatusCodes.Status400BadRequest)]
         //public virtual async Task<IActionResult> Put(TKey key, [FromBody] T item)
         //{
         //    if (!ModelState.IsValid)
@@ -82,8 +117,18 @@ namespace ATSPM.ConfigApi.Controllers
         //    return NoContent();
         //}
 
+        /// <summary>
+        /// Update object of specified type
+        /// </summary>
+        /// <param name="key">Key value of object to update</param>
+        /// <param name="item">Properites to update</param>
+        /// <returns>Action result confirming update</returns>
         // PATCH /Entity(1)
         //[HttpPatch("{key}")]
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Put))]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public virtual async Task<IActionResult> Patch(TKey key, [FromBody] Delta<T> item)
         {
             if (!ModelState.IsValid)
@@ -105,8 +150,17 @@ namespace ATSPM.ConfigApi.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Delete object of specified type
+        /// </summary>
+        /// <param name="key">Key value of object to delete</param>
+        /// <returns></returns>
         // DELETE /Entity(1)
         //[HttpDelete("{key}")]
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Delete))]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public virtual async Task<IActionResult> Delete(TKey key)
         {
             var i = await _repository.LookupAsync(key);
