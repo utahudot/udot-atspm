13f9fd8fe6d0db4b8134ef9a2c43c7b7df0fcb86
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2023-09-08 11:51:42 -0600

    Jira AVE-182 Preempt Service REquest (plus Preempt Service cleanup) - CA

--- ATSPM/ReportsTests/Controllers/PreemptServiceControllerTests.cs
+++ ATSPM/ReportsTests/Controllers/PreemptServiceControllerTests.cs
@@ -22,58 +22,12 @@ namespace ATSPM.Application.Reports.Controllers.Tests
             // Arrange
             PlanService planService = new PlanService();
             PreemptServiceService preemptServiceService = new PreemptServiceService(planService);
-            
-            //CycleService cycleService = new CycleService();
-            //ILoggerFactory loggerFactory = new LoggerFactory();
-            //ILogger<SignalPhaseService> logger = loggerFactory.CreateLogger<SignalPhaseService>();
-
-            //SignalPhaseService signalPhaseService = new SignalPhaseService(planService, cycleService, logger);
 
             System.DateTime start = new System.DateTime(2023, 4, 17, 12, 0, 0);
             System.DateTime end = new System.DateTime(2023, 4, 17, 14, 0, 0);
             List<ControllerEventLog> events = LoadDetectorEventsFromCsv(@"ControllerEvents-Preempt.csv"); // Sampleevents
             List<ControllerEventLog> planEvents = events.Where(e => new List<int> { 131 }.Contains(e.EventCode)).ToList(); // Load plan events from CSV
             List<ControllerEventLog> preemptEvents = events.Where(e => new List<int> { 105 }.Contains(e.EventCode)).ToList(); // Load preempt events from CSV
-
-
-            // Create the mock Approach object
-            //var approach = new Mock<Approach>();
-
-            //// Set the properties of the mock Approach object
-            //approach.Object.Id = 2880; // Updated Id
-            //approach.Object.SignalId = 1680; // Updated SignalId
-            //approach.Object.DirectionTypeId = DirectionTypes.NB;
-            //approach.Object.Description = "NBT Ph2";
-            //approach.Object.Mph = 45;
-            //approach.Object.ProtectedPhaseNumber = 2;
-            //approach.Object.IsProtectedPhaseOverlap = false;
-            //approach.Object.PermissivePhaseNumber = null;
-            //approach.Object.IsPermissivePhaseOverlap = false;
-            //approach.Object.PedestrianPhaseNumber = null;
-            //approach.Object.IsPedestrianPhaseOverlap = false;
-            //approach.Object.PedestrianDetectors = null;
-
-            //var mockSignal = new Mock<Signal>();
-
-            //// Set the properties of the mock Signal object
-            //mockSignal.Object.Id = 1680; // Updated Id
-            //mockSignal.Object.SignalId = "7115"; // Updated SignalId
-            //mockSignal.Object.Latitude = "40.62398502";
-            //mockSignal.Object.Longitude = "-111.9387819";
-            //mockSignal.Object.PrimaryName = "Redwood Road";
-            //mockSignal.Object.SecondaryName = "7000 South";
-            //mockSignal.Object.Ipaddress = IPAddress.Parse("10.210.14.39");
-            //mockSignal.Object.RegionId = 2;
-            //mockSignal.Object.ControllerTypeId = 2; // Updated ControllerTypeId
-            //mockSignal.Object.Enabled = true;
-            //mockSignal.Object.VersionActionId = SignaVersionActions.Initial;
-            //mockSignal.Object.Note = "10";
-            //mockSignal.Object.Start = new System.DateTime(2011, 1, 1);
-            //mockSignal.Object.JurisdictionId = 35;
-            //mockSignal.Object.Pedsare1to1 = true;
-
-            //// Create the mock Approach object and set its Signal property to the mock Signal object
-            //approach.Setup(a => a.Signal).Returns(mockSignal.Object);
 
             var options = new PreemptServiceMetricOptions() { SignalId = "7573", Start = start, End = end};
 
