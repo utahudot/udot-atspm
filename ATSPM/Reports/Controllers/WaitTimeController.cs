2d2b5bff70010977c12bb5d436df93f95e7709c4
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2023-09-11 10:12:31 -0600

    Uncommenting Wait Time

--- ATSPM/Reports/Controllers/WaitTimeController.cs
+++ ATSPM/Reports/Controllers/WaitTimeController.cs
@@ -1,90 +1,94 @@
-﻿//using ATSPM.Application.Extensions;
-//using ATSPM.Application.Reports.Business.Common;
-//using ATSPM.Application.Reports.Business.WaitTime;
-//using ATSPM.Application.Repositories;
-//using AutoFixture;
+﻿using ATSPM.Application.Extensions;
+using ATSPM.Application.Reports.Business.Common;
+using ATSPM.Application.Reports.Business.WaitTime;
+using ATSPM.Application.Repositories;
+using AutoFixture;
 //using Legacy.Common.Business;
-//using Microsoft.AspNetCore.Mvc;
-//using System.Collections.Generic;
-//using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
-//// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-//namespace ATSPM.Application.Reports.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class WaitTimeController : ControllerBase
-//    {
-//        private readonly AnalysisPhaseCollectionService analysisPhaseCollectionService;
-//        private readonly IApproachRepository approachRepository;
-//        private readonly WaitTimeService waitTimeService;
-//        private readonly IControllerEventLogRepository controllerEventLogRepository;
+namespace ATSPM.Application.Reports.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaitTimeController : ControllerBase
+    {
+        private readonly AnalysisPhaseCollectionService analysisPhaseCollectionService;
+        private readonly IApproachRepository approachRepository;
+        private readonly WaitTimeService waitTimeService;
+        private readonly IControllerEventLogRepository controllerEventLogRepository;
 
-//        public WaitTimeController(
-//            AnalysisPhaseCollectionService analysisPhaseCollectionService,
-//            IApproachRepository approachRepository,
-//            WaitTimeService waitTimeService,
-//            IControllerEventLogRepository controllerEventLogRepository
-//            )
-//        {
-//            this.analysisPhaseCollectionService = analysisPhaseCollectionService;
-//            this.approachRepository = approachRepository;
-//            this.waitTimeService = waitTimeService;
-//            this.controllerEventLogRepository = controllerEventLogRepository;
-//        }
+        public WaitTimeController(
+            AnalysisPhaseCollectionService analysisPhaseCollectionService,
+            IApproachRepository approachRepository,
+            WaitTimeService waitTimeService,
+            IControllerEventLogRepository controllerEventLogRepository
+            )
+        {
+            this.analysisPhaseCollectionService = analysisPhaseCollectionService;
+            this.approachRepository = approachRepository;
+            this.waitTimeService = waitTimeService;
+            this.controllerEventLogRepository = controllerEventLogRepository;
+        }
 
-//        // GET: api/<ApproachVolumeController>
-//        [HttpGet("test")]
-//        public WaitTimeResult Test()
-//        {
-//            Fixture fixture = new();
-//            WaitTimeResult viewModel = fixture.Create<WaitTimeResult>();
-//            return viewModel;
-//        }
+        // GET: api/<ApproachVolumeController>
+        [HttpGet("test")]
+        public WaitTimeResult Test()
+        {
+            Fixture fixture = new();
+            WaitTimeResult viewModel = fixture.Create<WaitTimeResult>();
+            return viewModel;
+        }
 
-//        [HttpPost("getChartData")]
-//        public WaitTimeResult GetChartData([FromBody] WaitTimeOptions options)
-//        {
-//            var approach = approachRepository.Lookup(options.ApproachId);
-//            var detectorChannels = approach.GetDetectorsForMetricType(32).Select(d => d.DetChannel).ToList();
-//            var planEvents = controllerEventLogRepository.GetPlanEvents(
-//                approach.SignalId,
-//                options.Start,
-//                options.End);
-//            var phaseEvents = controllerEventLogRepository.GetSignalEventsByEventCodes(
-//                approach.SignalId,
-//                options.Start,
-//                options.End,
-//                new List<int>() { 
-//                    82,
-//                    WaitTimeOptions.PHASE_BEGIN_GREEN,
-//                    WaitTimeOptions.PHASE_CALL_DROPPED,
-//                    WaitTimeOptions.PHASE_END_RED_CLEARANCE,
-//                    WaitTimeOptions.PHASE_CALL_REGISTERED}
-//                );
-//            var volume = new VolumeCollection(
-//                options.Start,
-//                options.End,
-//                phaseEvents.Where(e => e.EventCode == 82).ToList(),
-//                options.BinSize);
-//            var analysisPhaseDataCollection = analysisPhaseCollectionService.GetAnalysisPhaseCollectionData(
-//                approach.SignalId,
-//                options.Start,
-//                options.End,
-//                planEvents,
-//                phaseEvents,
-//                approach.Signal);
-//            var analysisPhaseData = analysisPhaseDataCollection.AnalysisPhases.Where(a => a.PhaseNumber == approach.ProtectedPhaseNumber).FirstOrDefault();
-//            return waitTimeService.GetChartData(
-//                options,
-//                approach,
-//                phaseEvents,
-//                analysisPhaseDataCollection.AnalysisPhases.Where(a => a.PhaseNumber == approach.ProtectedPhaseNumber).First(),
-//                analysisPhaseDataCollection.Plans,
-//                volume
-//                );
+        [HttpPost("getChartData")]
+        public WaitTimeResult GetChartData([FromBody] WaitTimeOptions options)
+        {
+            var approach = approachRepository.Lookup(options.ApproachId);
+            var detectorChannels = approach.GetDetectorsForMetricType(32).Select(d => d.DetChannel).ToList();
+            var planEvents = controllerEventLogRepository.GetPlanEvents(
+                approach.SignalId.ToString(),
+                options.Start,
+                options.End);
+            var phaseEvents = controllerEventLogRepository.GetSignalEventsByEventCodes(
+                approach.SignalId.ToString(),
+                options.Start,
+                options.End,
+                new List<int>() {
+                    82,
+                    WaitTimeOptions.PHASE_BEGIN_GREEN,
+                    WaitTimeOptions.PHASE_CALL_DROPPED,
+                    WaitTimeOptions.PHASE_END_RED_CLEARANCE,
+                    WaitTimeOptions.PHASE_CALL_REGISTERED}
+                );
+            var volume = new VolumeCollection(
+                options.Start,
+                options.End,
+                phaseEvents.Where(e => e.EventCode == 82).ToList(),
+                options.BinSize);
+            var analysisPhaseDataCollection = analysisPhaseCollectionService.GetAnalysisPhaseCollectionData(
+                approach.SignalId.ToString(),
+                options.Start,
+                options.End,
+                planEvents,
+                phaseEvents,
+                null,
+                null,
+                null,
+                approach.Signal,
+                1);
+            var analysisPhaseData = analysisPhaseDataCollection.AnalysisPhases.Where(a => a.PhaseNumber == approach.ProtectedPhaseNumber).FirstOrDefault();
+            return waitTimeService.GetChartData(
+                options,
+                approach,
+                phaseEvents,
+                analysisPhaseDataCollection.AnalysisPhases.Where(a => a.PhaseNumber == approach.ProtectedPhaseNumber).First(),
+                analysisPhaseDataCollection.Plans,
+                volume
+                );
 
-//        }
-//    }
-//}
+        }
+    }
+}
