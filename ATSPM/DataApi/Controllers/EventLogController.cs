58484dab777297bae4e2a66b199d903e27c9745a
Christian Baker <christianbaker@utah.gov>
2023-09-19 10:48:15 -0600

    ConfigApi changes to Signals

--- ATSPM/DataApi/Controllers/EventLogController.cs
+++ ATSPM/DataApi/Controllers/EventLogController.cs
@@ -63,7 +63,6 @@ namespace ATSPM.DataApi.Controllers
         /// Get signal events between dates with event code
         /// </summary>
         /// <param name="signalIdentifier">Signal identifier</param>
-        /// <param name="eventCode">Event code number</param>
         /// <param name="start">date/time of first event</param>
         /// <param name="end">date/time of last event</param>
         /// <returns>List of ControllerEventLogs</returns>
@@ -71,19 +70,23 @@ namespace ATSPM.DataApi.Controllers
         /// <response code="400">Invalid request (start/end range or event code)</response>
         /// <response code="404">Resource not found</response>
         //[ApiVersion("1.0")]
-        [HttpGet("{signalIdentifier}/{eventCode}")]
+        [HttpGet("[Action]/{signalIdentifier}")]
         [Produces("application/json", "application/xml", "text/csv")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<List<ControllerEventLog>> GetSignalEventsByEventCode(string signalIdentifier, int eventCode, DateTime start, DateTime end)
+        public ActionResult<List<ControllerEventLog>> GetSignalEventsByEventCode(string signalIdentifier, DateTime start, DateTime end, [FromQuery] IEnumerable<int> eventCode)
         {
+            //var eventCode = new List<int>();
+            
             _log.LogDebug("signal: {signal} event: {event} start: {start} end: {end}", signalIdentifier, eventCode, start, end);
+
+            Console.WriteLine($"events: {eventCode.Count()}");
 
             if (start == DateTime.MinValue || end == DateTime.MinValue)
                 return BadRequest("Invalid datetime range on start/end");
 
-            var result = _repository.GetSignalEventsByEventCode(signalIdentifier, start, end, eventCode);
+            var result = _repository.GetSignalEventsByEventCode(signalIdentifier, start, end, eventCode.First());
 
             if (result == null)
                 return NotFound();
