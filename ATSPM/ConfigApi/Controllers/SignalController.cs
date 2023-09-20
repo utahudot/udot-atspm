2ce1106440d193ff1ce9350f88843954a6352eb2
Christian Baker <christianbaker@utah.gov>
2023-09-20 11:31:33 -0600

    Added required actions to ConfigApi/Signals from the repo mothods

--- ATSPM/ConfigApi/Controllers/SignalController.cs
+++ ATSPM/ConfigApi/Controllers/SignalController.cs
@@ -1,17 +1,23 @@
 ﻿using Asp.Versioning;
 using Asp.Versioning.OData;
+using ATSPM.Application.Extensions;
 using ATSPM.Application.Repositories;
 using ATSPM.Data.Models;
+using Google.Apis.Util;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Extensions;
 using Microsoft.AspNetCore.OData.Formatter;
 using Microsoft.AspNetCore.OData.Formatter.Serialization;
 using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Query.Validator;
 using Microsoft.AspNetCore.OData.Results;
 using Microsoft.OData;
 using Microsoft.OData.Edm;
 using Microsoft.OData.ModelBuilder;
 using Microsoft.OData.UriParser;
+using NetTopologySuite.Densify;
 using System;
+using System.Linq;
 using System.Net;
 using System.Reflection.Metadata;
 using static Microsoft.AspNetCore.Http.StatusCodes;
@@ -19,6 +25,7 @@ using static Microsoft.AspNetCore.OData.Query.AllowedQueryOptions;
 
 namespace ATSPM.ConfigApi.Controllers
 {
+    [AutoExpand]
     [ApiVersion(1.0)]
     public class SignalController : AtspmConfigControllerBase<Signal, int>
     {
@@ -30,22 +37,57 @@ namespace ATSPM.ConfigApi.Controllers
 
         }
 
+        /// <summary>
+        /// Get all active <see cref="Signal"/> that match <paramref name="identifier"/>
+        /// </summary>
+        /// <param name="identifier">Signal controller identifier</param>
+        /// <param name="options">oData query options</param>
+        /// <returns>List of <see cref="Signal"/> in decescing order of start date</returns>
         [HttpGet]
-        [ProducesResponseType(Status400BadRequest)]
         [ProducesResponseType(typeof(IEnumerable<Signal>), Status200OK)]
+        [ProducesResponseType(Status400BadRequest)]
+        public IActionResult GetAllVersionsOfSignal(string identifier, ODataQueryOptions<Signal> options)
+        {
+            return Ok(options.ApplyTo(_repository.GetAllVersionsOfSignal(identifier).AsQueryable()));
+        }
+
+        /// <summary>
+        /// Get latest version of all <see cref="Signal"/>
+        /// </summary>
+        /// <param name="options">oData query options</param>
+        /// <returns>List of <see cref="Signal"/> with newest start date</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Signal>), Status200OK)]
+        [ProducesResponseType(Status400BadRequest)]
         [EnableQuery]
         public IActionResult GetLatestVersionOfAllSignals()
         {
+            //return Ok(options.ApplyTo(_repository.GetLatestVersionOfAllSignals().AsQueryable()));
+
             return Ok(_repository.GetLatestVersionOfAllSignals());
         }
 
+        /// <summary>
+        /// Get latest version of <see cref="Signal"/> and related entities that match <paramref name="identifier"/>
+        /// </summary>
+        /// <param name="identifier">Signal controller identifier</param>
+        /// <param name="options">oData query options</param>
+        /// <returns>Lastest <see cref="Signal"/> version</returns>
         [HttpGet]
-        //[ProducesResponseType(Status400BadRequest)]
         [ProducesResponseType(typeof(Signal), Status200OK)]
         [ProducesResponseType(Status404NotFound)]
-        [EnableQuery]
         public IActionResult GetLatestVersionOfSignal(string identifier, ODataQueryOptions<Signal> options)
         {
+            options.Request.ODataFeature().SelectExpandClause = new SelectExpandQueryOption(null, "Approaches", options.Context, new ODataQueryOptionParser(
+                model: options.Context.Model,
+                targetEdmType: options.Context.NavigationSource.EntityType(),
+                targetNavigationSource: options.Context.NavigationSource,
+                queryOptions: new Dictionary<string, string>
+                {
+                    { "$expand", "Approaches($expand=Detectors), Jurisdiction, ControllerType, Region, VersionAction, Areas" }
+                },
+                container: options.Context.RequestContainer)).SelectExpandClause;
+
             var i = _repository.GetLatestVersionOfSignal(identifier);
 
             if (i == null)
@@ -54,6 +96,48 @@ namespace ATSPM.ConfigApi.Controllers
             }
 
             return Ok(i);
+        }
+
+        /// <summary>
+        /// Copies <see cref="Signal"/> and associated <see cref="Approach"/> to new version
+        /// </summary>
+        /// <param name="key">Signal version to copy</param>
+        /// <returns>New version of copied <see cref="Signal"/></returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(Signal), Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> CopySignalToNewVersion(int key)
+        {
+            try
+            {
+                return Ok(await _repository.CopySignalToNewVersion(key));
+            }
+            catch (ArgumentException e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Marks <see cref="Signal"/> to deleted
+        /// </summary>
+        /// <param name="key">Key of <see cref="Signal"/> to mark as deleted</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> SetSignalToDeleted(int key)
+        {
+            try
+            {
+                await _repository.SetSignalToDeleted(key);
+            }
+            catch (ArgumentException e)
+            {
+                return NotFound(e.Message);
+            }
+            
+            return Ok();
         }
     }
 }
