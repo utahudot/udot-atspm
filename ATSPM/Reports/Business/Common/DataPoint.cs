b918b8d0d31fe7f40d3cd7107911e850e929178a
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2023-09-29 15:08:08 -0600

    AVE-316 Split Monitor Bugs

---
+++ ATSPM/Reports/Business/Common/DataPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Reports.Business.Common
+{
+    public class DataPoint
+    {
+        public DataPoint(DateTime startTime, double seconds)
+        {
+            StartTime = startTime;
+            Seconds = seconds;
+        }
+
+        public DateTime StartTime { get; set; }
+        public double Seconds { get; set; }
+    }
+}
