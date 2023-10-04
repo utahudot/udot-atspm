d42ed298e02d2669c9f0d5cac2dfb1de09c5a807
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2023-10-04 13:36:46 -0600

    Adding ignored file.

---
+++ ATSPM/Reports/Business/PhaseTermination/PurduePhaseTerminationOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATSPM.Application.Reports.Business.PhaseTermination
+{
+    public class PurduePhaseTerminationOptions
+    {
+        public int SelectedConsecutiveCount { get; set; }
+        public string SignalIdentifier { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
