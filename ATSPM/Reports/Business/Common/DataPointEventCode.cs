83f2458601387ed8f9cec96de71b916cebda9889
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2023-10-04 13:31:20 -0600

    Adding excluded files

---
+++ ATSPM/Reports/Business/Common/DataPointEventCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Reports.Business.Common
+{
+    public class DataPointEventCode
+    {
+        public DataPointEventCode(DateTime startTime, int eventCode)
+        {
+            StartTime = startTime;
+            EventCode = eventCode;
+        }
+
+        public DateTime StartTime { get; set; }
+        public int EventCode { get; set; }
+    }
+}
---
+++ ATSPM/Reports/Business/Common/DataPointSeconds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Reports.Business.Common
+{
+    public class DataPointSeconds
+    {
+        public DataPointSeconds(DateTime startTime, double seconds)
+        {
+            StartTime = startTime;
+            Seconds = seconds;
+        }
+
+        public DateTime StartTime { get; set; }
+        public double Seconds { get; set; }
+    }
+}
