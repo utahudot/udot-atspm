77a8e79e9e06ef22343249341bfa2d7e714d5494
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2023-10-05 16:26:20 -0600

    Adding ignored file

---
+++ ATSPM/Reports/Business/Common/DataPointBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Reports.Business.Common
+{
+    public class DataPointBase
+    {
+        private DateTime _timeStamp;
+        public DataPointBase(DateTime timeStamp)
+        {
+            _timeStamp = DateTime.SpecifyKind(timeStamp, DateTimeKind.Unspecified);
+        }
+        public DateTime TimeStamp
+        {
+            get
+            {
+                return _timeStamp;
+            }
+            set
+            {
+                _timeStamp = DateTime.SpecifyKind(value, DateTimeKind.Unspecified);
+            }
+        }
+    }
+}
