2fd1ce892a92a081946e79d1e1e4f78e1a455c1e
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2023-10-23 14:58:07 -0600

    Adding Watchdog Enums

---
+++ ATSPM/Data/Enums/WatchDogIssueType.cs
@@ -0,0 +1,20 @@
+﻿namespace ATSPM.Data.Enums
+{
+    public enum WatchDogIssueType
+    {
+        RecordCount = 1,
+        LowDetectorHits = 2,
+        StuckPed = 3,
+        ForceOffThreshold = 4,
+        MaxOutThreshold = 5,
+
+    }
+
+    [System.Flags]
+    public enum WatchDogComponentType
+    {
+        Signal,
+        Approach,
+        Detector
+    }
+}
