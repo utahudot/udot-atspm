f4de75c55da569c72e5940b17607e4bda4b348b6
Christian Baker <christianbaker@utah.gov>
2023-10-30 11:20:33 -0600

    Major configcotext changes

--- ATSPM/Infrastructure/Migrations/Config/20231030171950_EFCore6Upgrade.Designer.cs
+++ ATSPM/Infrastructure/Migrations/Config/20231030171950_EFCore6Upgrade.Designer.cs
@@ -4,6 +4,7 @@ using ATSPM.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ATSPM.Infrastructure.Migrations
 {
     [DbContext(typeof(ConfigContext))]
-    partial class ConfigContextModelSnapshot : ModelSnapshot
+    [Migration("20231030171950_EFCore6Upgrade")]
+    partial class EFCore6Upgrade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
