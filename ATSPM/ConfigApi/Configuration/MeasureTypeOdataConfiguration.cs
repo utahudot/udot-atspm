3d75bbcd757197b463b8c7a38a3a69d62ae0583d
Christian Baker <christianbaker@utah.gov>
2023-11-01 15:12:44 -0600

    Updated all oData configs and controllers on configapi

--- ATSPM/ConfigApi/Configuration/MeasureTypeOdataConfiguration.cs
+++ ATSPM/ConfigApi/Configuration/MeasureTypeOdataConfiguration.cs
@@ -5,21 +5,23 @@ using Microsoft.OData.ModelBuilder;
 
 namespace ATSPM.ConfigApi.Configuration
 {
-    public class RegionModelConfiguration : IModelConfiguration
+    /// <summary>
+    /// Measure type oData configuration
+    /// </summary>
+    public class MeasureTypeOdataConfiguration : IModelConfiguration
     {
         ///<inheritdoc/>
         public void Apply(ODataModelBuilder builder, ApiVersion apiVersion, string routePrefix)
         {
-            var model = builder.EntitySet<Region>("Region")
-                .EntityType
-                .Page(default, default)
-                .Expand(1, SelectExpandType.Allowed, new string[] { "signals" });
+            var model = builder.EntitySet<MeasureType>("MeasureType").EntityType;
+            model.Page(default, default);
 
             switch (apiVersion.MajorVersion)
             {
                 case 1:
                     {
-                        model.Property(p => p.Description).MaxLength = 50;
+                        model.Property(p => p.Abbreviation).MaxLength = 8;
+                        model.Property(p => p.Name).MaxLength = 50;
 
                         break;
                     }
---
+++ ATSPM/ConfigApi/Controllers/MeasureTypeController.cs
@@ -0,0 +1,65 @@
+﻿using Asp.Versioning;
+using ATSPM.Application.Repositories;
+using ATSPM.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using static Microsoft.AspNetCore.Http.StatusCodes;
+using static Microsoft.AspNetCore.OData.Query.AllowedQueryOptions;
+
+namespace ATSPM.ConfigApi.Controllers
+{
+    /// <summary>
+    /// Measure type controller
+    /// </summary>
+    [ApiVersion(1.0)]
+    public class MeasureTypeController : AtspmConfigControllerBase<MeasureType, int>
+    {
+        private readonly IMeasureTypeRepository _repository;
+
+        /// <inheritdoc/>
+        public MeasureTypeController(IMeasureTypeRepository repository) : base(repository)
+        {
+            _repository = repository;
+        }
+
+        #region NavigationProperties
+
+        /// <summary>
+        /// <see cref="DetectionType"/> navigation property action
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        [EnableQuery(AllowedQueryOptions = Count | Expand | Filter | Select | OrderBy | Top | Skip)]
+        [ProducesResponseType(Status200OK)]
+        [ProducesResponseType(Status404NotFound)]
+        [ProducesResponseType(Status400BadRequest)]
+        public ActionResult<IEnumerable<DetectionType>> GetDetectionTypes([FromRoute] int key)
+        {
+            return GetNavigationProperty<IEnumerable<DetectionType>>(key);
+        }
+
+        /// <summary>
+        /// <see cref="MeasureComment"/> navigation property action
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        [EnableQuery(AllowedQueryOptions = Count | Expand | Filter | Select | OrderBy | Top | Skip)]
+        [ProducesResponseType(Status200OK)]
+        [ProducesResponseType(Status404NotFound)]
+        [ProducesResponseType(Status400BadRequest)]
+        public ActionResult<IEnumerable<MeasureComment>> GetMeasureComments([FromRoute] int key)
+        {
+            return GetNavigationProperty<IEnumerable<MeasureComment>>(key);
+        }
+
+        #endregion
+
+        #region Actions
+
+        #endregion
+
+        #region Functions
+
+        #endregion
+    }
+}
