e61dbf3177881a82be5f130d89c1de2dc0fc6fff
Christian Baker <christianbaker@utah.gov>
2023-11-06 14:51:44 -0700

    Still refactoring

---
+++ ATSPM/ReportApi/TempModels/PreemptCycleResult.cs
@@ -0,0 +1,21 @@
+﻿namespace ATSPM.ReportApi.TempModels
+{
+    public class PreemptCycleResult
+    {
+        public DateTime InputOff { get; set; }
+
+        public DateTime InputOn { get; set; }
+
+        public DateTime GateDown { get; set; }
+
+        public double CallMaxOut { get; set; }
+
+        public double Delay { get; set; }
+
+        public double TimeToService { get; set; }
+
+        public double DwellTime { get; set; }
+
+        public double TrackClear { get; set; }
+    }
+}
