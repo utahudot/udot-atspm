3d7f50a14e678cab3afaf0bb116c4931c0fff5ea
Christian Baker <christianbaker@utah.gov>
2023-11-15 13:34:29 -0700

    worked on CreateVehicleTests

---
+++ ATSPM/ApplicationCore/Analysis/Common/IDetectorEvent.cs
@@ -0,0 +1,16 @@
+﻿using ATSPM.Data.Interfaces;
+using System;
+
+namespace ATSPM.Application.Analysis.Common
+{
+    /// <summary>
+    /// Detector event
+    /// </summary>
+    public interface IDetectorEvent : ISignalDetector
+    {
+        /// <summary>
+        /// Coreected timestamp of event
+        /// </summary>
+        DateTime CorrectedTimeStamp { get; set; }
+    }
+}
