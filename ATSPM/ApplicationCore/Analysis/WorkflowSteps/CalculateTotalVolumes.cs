7fb079c921510f2835c5a6c4aae7b5b36329f488
Christian Baker <christianbaker@utah.gov>
2023-11-16 12:54:00 -0700

    CalculatePhaseVolume

--- ATSPM/ApplicationCore/Analysis/WorkflowSteps/CalculateTotalVolumes.cs
+++ ATSPM/ApplicationCore/Analysis/WorkflowSteps/CalculateTotalVolumes.cs
@@ -1,9 +1,11 @@
 ﻿using ATSPM.Application.Analysis.Common;
 using ATSPM.Application.Analysis.Workflows;
 using ATSPM.Application.Common;
+using ATSPM.Data.Models;
 using ATSPM.Domain.Common;
 using ATSPM.Domain.Workflows;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,61 +15,117 @@ using System.Threading.Tasks.Dataflow;
 
 namespace ATSPM.Application.Analysis.WorkflowSteps
 {
-    public class CalculateTotalVolumes : TransformManyProcessStepBase<IEnumerable<CorrectedDetectorEvent>, TotalVolumes>
+    public class CalculatePhaseVolume : TransformProcessStepBase<IReadOnlyList<Tuple<Detector, IEnumerable<CorrectedDetectorEvent>>>, Volumes>
     {
-        private readonly TimelineOptions _options;
+        public CalculatePhaseVolume(ExecutionDataflowBlockOptions dataflowBlockOptions = default) : base(dataflowBlockOptions) { }
 
-        public CalculateTotalVolumes(TimelineOptions options, ExecutionDataflowBlockOptions dataflowBlockOptions = default) : base(dataflowBlockOptions)
+        protected override Task<Volumes> Process(IReadOnlyList<Tuple<Detector, IEnumerable<CorrectedDetectorEvent>>> input, CancellationToken cancelToken = default)
         {
-            _options = options;
+            var result = new Volumes(new TimelineOptions()
+            {
+                //Start = input.SelectMany(m => m.Item2).Min(m => m.CorrectedTimeStamp),
+                //End = input.SelectMany(m => m.Item2).Max(m => m.CorrectedTimeStamp),
+                Start = DateTime.Parse("4/17/2023 8:00:00"),
+                End = DateTime.Parse("4/17/2023 10:00:00"),
+                Type = TimelineType.Minutes,
+                Size = 15
+            });
+
+            result.ForEach(f =>
+            {
+                f.Phase = input.Select(m => m.Item1.Approach.ProtectedPhaseNumber).Distinct().First();
+                f.Direction = input.Select(m => m.Item1.Approach.DirectionTypeId).Distinct().First();
+                f.DetectorCount = input.SelectMany(m => m.Item2).Where(w => f.InRange(w.CorrectedTimeStamp)).Count();
+
+            });
+
+            return Task.FromResult(result);
         }
+    }
 
-        protected override Task<IEnumerable<TotalVolumes>> Process(IEnumerable<CorrectedDetectorEvent> input, CancellationToken cancelToken = default)
+
+    public class CalculateTotalVolumes : TransformProcessStepBase<Tuple<Approach, IEnumerable<Vehicle>>, TotalVolumes>
+    {
+        public CalculateTotalVolumes(ExecutionDataflowBlockOptions dataflowBlockOptions = default) : base(dataflowBlockOptions) { }
+
+        protected override Task<TotalVolumes> Process(Tuple<Approach, IEnumerable<Vehicle>> input, CancellationToken cancelToken = default)
         {
-            var result = new List<TotalVolumes>();
+            //var primary = input.Item1;
+            //var opposing = primary.Signal.Approaches.Where(w => w.DirectionTypeId == new OpposingDirection(primary.DirectionTypeId));
 
-            //this is the original i just removed it so it would compile
-            //var test = input.GroupBy(g => g.Detector.Approach);
+            var result = new TotalVolumes(new TimelineOptions()
+            {
+                Start = input.Item2.Min(m => m.CorrectedTimeStamp),
+                End = input.Item2.Max(m => m.CorrectedTimeStamp),
+                Type = TimelineType.Minutes,
+                Size = 15
+            });
 
-            //foreach (var t in test)
+            //var o = input.Item1.Detectors.Where(w => w.Item1.Approach.DirectionTypeId == new OpposingDirection(t.Item1.Approach.DirectionTypeId)).FirstOrDefault();
+
+            //result.ForEach(f =>
             //{
-            //    var total = new TotalVolumes(_options);
+            //    f.Primary = new Volume()
+            //    {
+            //        Start = f.Start,
+            //        End = f.End,
+            //        Direction = t.Item1.Approach.DirectionTypeId,
+            //        //TODO: get real phase number
+            //        Phase = t.Item1.Approach.ProtectedPhaseNumber,
+            //        DetectorCount = t.Item2.Where(w => f.InRange(w.CorrectedTimeStamp)).Count()
+            //    };
 
-            //    var c = new OpposingDirection(t.Key.DirectionTypeId);
+            //    f.Opposing = new Volume()
+            //    {
+            //        Start = f.Start,
+            //        End = f.End,
+            //        Direction = o.Item1.Approach.DirectionTypeId,
+            //        //TODO: get real phase number
+            //        Phase = o.Item1.Approach.ProtectedPhaseNumber,
+            //        DetectorCount = o.Item2.Where(w => f.InRange(w.CorrectedTimeStamp)).Count()
+            //    };
+            //});
 
-            //    var o = test.Where(w => w.Key.DirectionTypeId == c).FirstOrDefault();
 
-            //    total.ForEach(f =>
+
+
+            //var result = new TotalVolumes(new TimelineOptions()
+            //{
+            //    Start = input.SelectMany(m => m.Item2).Min(m => m.CorrectedTimeStamp),
+            //    End = input.SelectMany(m => m.Item2).Max(m => m.CorrectedTimeStamp),
+            //    Type = TimelineType.Minutes,
+            //    Size = 15
+            //});
+
+            //foreach (var t in input)
+            //{
+            //    var o = input.Where(w => w.Item1.Approach.DirectionTypeId == new OpposingDirection(t.Item1.Approach.DirectionTypeId)).FirstOrDefault();
+
+            //    result.ForEach(f =>
             //    {
             //        f.Primary = new Volume()
             //        {
             //            Start = f.Start,
             //            End = f.End,
-            //            Direction = t.Key.DirectionTypeId,
-            //            Phase = t.Key.ProtectedPhaseNumber,
-            //            DetectorCount = t.Where(w => f.InRange(w.CorrectedTimeStamp)).Count()
+            //            Direction = t.Item1.Approach.DirectionTypeId,
+            //            //TODO: get real phase number
+            //            Phase = t.Item1.Approach.ProtectedPhaseNumber,
+            //            DetectorCount = t.Item2.Where(w => f.InRange(w.CorrectedTimeStamp)).Count()
             //        };
 
             //        f.Opposing = new Volume()
             //        {
             //            Start = f.Start,
             //            End = f.End,
-            //            Direction = o.Key.DirectionTypeId,
-            //            Phase = o.Key.ProtectedPhaseNumber,
-            //            DetectorCount = o.Where(w => f.InRange(w.CorrectedTimeStamp)).Count()
+            //            Direction = o.Item1.Approach.DirectionTypeId,
+            //            //TODO: get real phase number
+            //            Phase = o.Item1.Approach.ProtectedPhaseNumber,
+            //            DetectorCount = o.Item2.Where(w => f.InRange(w.CorrectedTimeStamp)).Count()
             //        };
             //    });
-
-            //    result.Add(total);
-
-            //    foreach (var a in total)
-            //    {
-            //        Console.WriteLine($"p: {a.Primary}");
-            //        Console.WriteLine($"o: {a.Opposing}");
-            //    }
             //}
 
-            return Task.FromResult<IEnumerable<TotalVolumes>>(result);
+            return Task.FromResult<TotalVolumes>(result);
         }
     }
 }
--- ATSPM/ApplicationCoreTests/Analysis/TestingTesting.cs
+++ ATSPM/ApplicationCoreTests/Analysis/TestingTesting.cs
@@ -1,7 +1,10 @@
 ﻿using ApplicationCoreTests.Analysis.TestObjects;
+using ATSPM.Application;
 using ATSPM.Application.Analysis.Common;
 using ATSPM.Application.Analysis.WorkflowFilters;
 using ATSPM.Application.Analysis.WorkflowSteps;
+using ATSPM.Application.Common;
+using ATSPM.Application.Extensions;
 using ATSPM.Application.ValueObjects;
 using ATSPM.Data.Enums;
 using ATSPM.Data.Models;
@@ -21,8 +24,22 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace ApplicationCoreTests.Analysis
 {
-    
-    
+    public class TempPhaseDirections
+    {
+        private readonly IEnumerable<ControllerEventLog> _logs;
+
+        public TempPhaseDirections(IEnumerable<ControllerEventLog> logs)
+        {
+            _logs = logs;
+        }
+
+        public Approach Primary { get; set; }
+        public Approach Opposing { get; set; }
+
+        public IReadOnlyList<ControllerEventLog> PrimaryLogs => _logs.Where(w => w.SignalIdentifier == Primary.Signal.SignalIdentifier && Primary.Detectors.Select(s => s.DetectorChannel).Contains(w.EventParam)).ToList();
+        public IReadOnlyList<ControllerEventLog> OpposingLogs => _logs.Where(w => w.SignalIdentifier == Opposing.Signal.SignalIdentifier && Opposing.Detectors.Select(s => s.DetectorChannel).Contains(w.EventParam)).ToList();
+    }
+
     public class TestingTesting
     {
         private readonly ITestOutputHelper _output;
@@ -35,53 +52,122 @@ namespace ApplicationCoreTests.Analysis
         [Fact]
         public async void TestingStuff()
         {
-            var file1 = new FileInfo(@"C:\Users\christianbaker\source\repos\udot-atspm\ATSPM\ApplicationCoreTests\Analysis\TestData\CycleEvents.csv");
+            //var file1 = new FileInfo(@"C:\Users\christianbaker\source\repos\udot-atspm\ATSPM\ApplicationCoreTests\Analysis\TestData\ApproachVolumeTestLogs.csv");
 
-            var logs = File.ReadAllLines(file1.FullName)
-                   .Skip(1)
-                   .Select(x => x.Split(','))
-                   .Select(x => new ControllerEventLog
-                   {
-                       SignalIdentifier = x[0],
-                       Timestamp = DateTime.Parse(x[1]),
-                       EventCode = int.Parse(x[2]),
-                       EventParam = int.Parse(x[3])
-                   }).ToList();
-
-            //var file2 = new FileInfo(@"C:\Users\christianbaker\source\repos\udot-atspm\ATSPM\ApplicationCoreTests\Analysis\TestData\Correction.csv");
-
-            //var events = File.ReadAllLines(file2.FullName)
+            //var logs = File.ReadAllLines(file1.FullName)
             //       .Skip(1)
             //       .Select(x => x.Split(','))
-            //       .Select(x => new CorrectedDetectorEvent
+            //       .Select(x => new ControllerEventLog
             //       {
             //           SignalIdentifier = x[0],
-            //           CorrectedTimeStamp = DateTime.Parse(x[1]),
-            //           //EventCode = int.Parse(x[2]),
-            //           DetectorChannel = int.Parse(x[3])
+            //           Timestamp = DateTime.Parse(x[1]),
+            //           EventCode = int.Parse(x[2]),
+            //           EventParam = int.Parse(x[3])
             //       }).ToList();
 
-            //_output.WriteLine($"log count: {logs.Count}");
-            //_output.WriteLine($"event count: {events.Count} - {events.Any(a => a.DetectorChannel != 2)}");
+            ////var testdata = new RedToRedCyclesTestData()
+            ////{
+            ////    EventLogs = logs,
+            ////    //RedCycles = events
+            ////};
 
-            var testdata = new RedToRedCyclesTestData()
+            //var json = JsonConvert.SerializeObject(logs);
+
+            //File.WriteAllText(@"C:\Users\christianbaker\source\repos\udot-atspm\ATSPM\ApplicationCoreTests\Analysis\TestData\ApproachVolumeTestLogs.json", json);
+
+
+            var json = File.ReadAllText(new FileInfo(@"C:\Users\christianbaker\source\repos\udot-atspm\ATSPM\ApplicationCoreTests\Analysis\TestData\Signal7115.json").FullName);
+            var signal = JsonConvert.DeserializeObject<Signal>(json);
+
+            var json1 = File.ReadAllText(new FileInfo(@"C:\Users\christianbaker\source\repos\udot-atspm\ATSPM\ApplicationCoreTests\Analysis\TestData\ApproachVolumeTestLogs.json").FullName);
+            var logs = JsonConvert.DeserializeObject<IEnumerable<ControllerEventLog>>(json1);
+
+
+            _output.WriteLine($"signal: {signal}");
+
+
+            //var primaryChain = new FilteredDetectorData();
+            //var opposingChain = new FilteredDetectorData();
+
+            var primaryChain = new IdentifyandAdjustVehicleActivations();
+            var opposingChain = new IdentifyandAdjustVehicleActivations();
+            var VolumeP = new CalculatePhaseVolume();
+            var VolumeO = new CalculatePhaseVolume();
+
+            var testResults = new ActionBlock<Volumes>(a =>
             {
-                EventLogs = logs,
-                //RedCycles = events
-            };
+                _output.WriteLine($"a: {a.DetectorCount}");
 
-            var json = JsonConvert.SerializeObject(testdata);
+                foreach (var v in a)
+                {
+                    _output.WriteLine($"v: {v}");
+                }
+            });
 
-            File.WriteAllText(@"C:\Users\christianbaker\source\repos\udot-atspm\ATSPM\ApplicationCoreTests\Analysis\TestData\RedToRedCyclesTestData.json", json);
+            primaryChain.LinkTo(VolumeP, new DataflowLinkOptions() { PropagateCompletion = true });
+            opposingChain.LinkTo(VolumeO, new DataflowLinkOptions() { PropagateCompletion = true });
+
+            VolumeP.LinkTo(testResults, new DataflowLinkOptions() { PropagateCompletion = true });
+            VolumeO.LinkTo(testResults, new DataflowLinkOptions() { PropagateCompletion = true });
+
+            //var combineChains = new JoinBlock<IReadOnlyList<Tuple<Detector, IEnumerable<CorrectedDetectorEvent>>>, IReadOnlyList<Tuple<Detector, IEnumerable<CorrectedDetectorEvent>>>>();
+
+
+            var stuff = signal.Approaches.SelectMany(m => m.Detectors).GroupJoin(logs, d => d.DetectorChannel, l => l.EventParam, (o, i) => Tuple.Create(signal.Approaches.First(f => f.Id == o.Approach.Id), i)).Where(w => w.Item2.Any());
+
+            foreach (var app in stuff)
+            {
+                //if (s.Item2.Count() > 0)
+                //    _output.WriteLine($"stuff: {s.Item1} - {s.Item2.Count()}");
+                //else
+                //    _output.WriteLine($"fail: {s.Item1} - {s.Item2.Count()}");
+
+
+                var o = stuff.FirstOrDefault(w => w.Item1.DirectionTypeId == new OpposingDirection(app.Item1.DirectionTypeId));
+                _output.WriteLine($"primary: {app.Item1}:{app.Item2.Count()} opposing: {o}");
+
+
+                primaryChain.Post(app);
+                opposingChain.Post(app);
+
+                primaryChain.Complete();
+                opposingChain.Complete();
+
+                await testResults.Completion;
+
+
+
+
+            }
+
+
+
+            //foreach (var a in signal.Approaches)
+            //{
+            //    var o = signal.Approaches.FirstOrDefault(w => w.DirectionTypeId == new OpposingDirection(a.DirectionTypeId));
+
+            //    _output.WriteLine($"primary: {a} opposing: {o}");
+
+            //    if (o != null )
+            //    {
+            //        var boo = new TempPhaseDirections(logs) { Primary = a, Opposing = o };
+
+            //        _output.WriteLine($"primary: {boo.Primary} logs: {boo.PrimaryLogs.Count}");
+            //        _output.WriteLine($"opposing: {boo.Opposing} logs: {boo.OpposingLogs.Count}");
+            //    }
 
 
 
 
 
+            //   //foreach (var s in stuff)
+            //   // {
+            //   //     if (s.Item2.Count() > 0)
+            //   //         _output.WriteLine($"stuff: {s.Item1.DetectorChannel} logs: {s.Item2.Count()} - {s.Item2.All(a => a.EventParam == s.Item1.DetectorChannel)}");
+            //   // }
 
 
-
-
+            //}
 
 
 
@@ -126,5 +212,5 @@ namespace ApplicationCoreTests.Analysis
         public IReadOnlyList<ControllerEventLog> Logs { get; set; }
     }
 
-    
+
 }
