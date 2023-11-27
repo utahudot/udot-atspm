71c297c2d734af209058b1cce5e5c5c84863e390
Christian Baker <christianbaker@utah.gov>
2023-11-27 11:02:11 -0700

    working on CalculateTotalVolumesTests

--- ATSPM/ApplicationCore/Analysis/WorkflowSteps/CalculateTotalVolumes.cs
+++ ATSPM/ApplicationCore/Analysis/WorkflowSteps/CalculateTotalVolumes.cs
@@ -16,31 +16,32 @@ namespace ATSPM.Application.Analysis.WorkflowSteps
     {
         protected override Task<Tuple<Approach, TotalVolumes>> Process(Tuple<Tuple<Approach, Volumes>, Tuple<Approach, Volumes>> input, CancellationToken cancelToken = default)
         {
-            //var p = input.Item1.Item1;
-            //var o = input.Item2.Item1;
-            
-            //if (p.DirectionTypeId == new OpposingDirection(o.DirectionTypeId) && p.Signal.SignalIdentifier == o.Signal.SignalIdentifier)
-            //{
-            //    var start1 = input.Item1.Item2.Start;
-            //    var start2 = input.Item2.Item2.Start;
+            var p = input.Item1.Item1;
+            var o = input.Item2.Item1;
+
+            if (p.DirectionTypeId == new OpposingDirection(o.DirectionTypeId) && p.Signal.SignalIdentifier == o.Signal.SignalIdentifier)
+            {
+                var start1 = input.Item1.Item2.Start;
+                var start2 = input.Item2.Item2.Start;
 
 
 
-            //    var volumes = input.Item1.Item2.Union(input.Item2.Item2);
+                var volumes = input.Item1.Item2.Segments.Union(input.Item2.Item2.Segments);
 
-            //    var result = new TotalVolumes(volumes.CreateTimeline<TotalVolume>(TimelineType.Minutes, 15))
-            //    {
-            //        SignalIdentifier = p.Signal.SignalIdentifier,
-            //    };
+                var result = new TotalVolumes(volumes, TimeSpan.FromMinutes(15))
+                {
+                    SignalIdentifier = p.Signal.SignalIdentifier,
+                };
 
-            //    result.ForEach(f =>
-            //    {
-            //        f.Primary = input.Item1.Item2.FirstOrDefault(d => f.InRange(d));
-            //        f.Opposing = input.Item2.Item2.FirstOrDefault(d => f.InRange(d));
-            //    });
+                result.Segments.ToList().ForEach(f =>
+                {
+                    f.SignalIdentifier = p.Signal.SignalIdentifier;
+                    f.Primary = input.Item1.Item2.Segments.FirstOrDefault(d => f.InRange(d));
+                    f.Opposing = input.Item2.Item2.Segments.FirstOrDefault(d => f.InRange(d));
+                });
 
-            //    return Task.FromResult(Tuple.Create(p, result));
-            //}
+                return Task.FromResult(Tuple.Create(p, result));
+            }
 
             return Task.FromResult(Tuple.Create<Approach, TotalVolumes>(null, null));
         }
