6e77c9718fcb735722ef0cd461cd1f2b1abd4dee
Christian Baker <christianbaker@utah.gov>
2023-11-27 13:38:41 -0700

    Updated CreateVehicle

--- ATSPM/ApplicationCore/Analysis/WorkflowSteps/CreateVehicle.cs
+++ ATSPM/ApplicationCore/Analysis/WorkflowSteps/CreateVehicle.cs
@@ -10,29 +10,20 @@ using System.Threading.Tasks.Dataflow;
 
 namespace ATSPM.Application.Analysis.WorkflowSteps
 {
-    public class CreateVehicle : TransformProcessStepBase<Tuple<IEnumerable<Tuple<Detector, IEnumerable<CorrectedDetectorEvent>>>, Tuple<Approach, IEnumerable<RedToRedCycle>>>, Tuple<Approach, IEnumerable<Vehicle>>>
+    public class CreateVehicle : TransformProcessStepBase<Tuple<Tuple<Approach, IEnumerable<CorrectedDetectorEvent>>, Tuple<Approach, IEnumerable<RedToRedCycle>>>, Tuple<Approach, IEnumerable<Vehicle>>>
     {
         public CreateVehicle(ExecutionDataflowBlockOptions dataflowBlockOptions = default) : base(dataflowBlockOptions) { }
 
-        protected override Task<Tuple<Approach, IEnumerable<Vehicle>>> Process(Tuple<IEnumerable<Tuple<Detector, IEnumerable<CorrectedDetectorEvent>>>, Tuple<Approach, IEnumerable<RedToRedCycle>>> input, CancellationToken cancelToken = default)
+        protected override Task<Tuple<Approach, IEnumerable<Vehicle>>> Process(Tuple<Tuple<Approach, IEnumerable<CorrectedDetectorEvent>>, Tuple<Approach, IEnumerable<RedToRedCycle>>> input, CancellationToken cancelToken = default)
         {
-            var result = Tuple.Create(input.Item2?.Item1,
-                input.Item2?.Item2?.Select(s =>
-                input.Item1.Where(w => w.Item1.ApproachId == input.Item2.Item1.Id)
-                .SelectMany(m => m.Item2?.Where(w => w.SignalIdentifier == input.Item2?.Item1?.Signal.SignalIdentifier && s.InRange(w.Timestamp)))
-                .Select(v => new Vehicle()
-                {
-                    SignalIdentifier = v.SignalIdentifier,
-                    Timestamp = v.Timestamp,
-                    DetectorChannel = v.DetectorChannel,
-                    PhaseNumber = s.PhaseNumber,
-                    Start = s.Start,
-                    End = s.End,
-                    YellowEvent = s.YellowEvent,
-                    GreenEvent = s.GreenEvent,
-                })).SelectMany(m => m));
+            var result = input.Item2.Item2?.GroupJoin(input.Item1.Item2, 
+                o => new { o.SignalIdentifier, o.PhaseNumber }, 
+                i => new { i.SignalIdentifier, i.PhaseNumber },
+                (o, i) => i.Where(w => o.InRange(w.Timestamp))
+                .Select(s => new Vehicle(s, o)))
+                .SelectMany(m => m);
 
-            return Task.FromResult(result);
+            return Task.FromResult(Tuple.Create(input.Item1.Item1, result));
         }
     }
 }
