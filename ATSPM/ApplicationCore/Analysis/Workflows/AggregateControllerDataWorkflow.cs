574b61c84822935c232ef0fd63f67f75c4bc5705
Christian Baker <christianbaker@utah.gov>
2023-12-04 14:19:29 -0700

    Working on IdentifyTerminationTypesAndTimes

---
+++ ATSPM/ApplicationCore/Analysis/Workflows/AggregateControllerDataWorkflow.cs
@@ -0,0 +1,51 @@
+﻿using ATSPM.Application.Analysis.ApproachVolume;
+using ATSPM.Application.Analysis.Common;
+using ATSPM.Application.Analysis.WorkflowFilters;
+using ATSPM.Application.Analysis.WorkflowSteps;
+using ATSPM.Data.Models;
+using ATSPM.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks.Dataflow;
+
+namespace ATSPM.Application.Analysis.Workflows
+{
+    public class AggregateControllerDataWorkflow : WorkflowBase<>
+    {
+        public GroupSignalsByApproaches GroupSignalsByApproaches { get; private set; }
+        public FilteredDetectorData FilteredDetectorData { get; private set; }
+        public GroupDetectorsByDetectorEvent GroupDetectorsByDetectorEvent { get; private set; }
+        public AggregateDetectorEvents AggregateDetectorEvents { get; private set; }
+
+        /// <inheritdoc/>
+        public override void InstantiateSteps()
+        {
+            GroupSignalsByApproaches = new();
+            FilteredDetectorData = new();
+            GroupDetectorsByDetectorEvent = new();
+            AggregateDetectorEvents = new();
+        }
+
+        /// <inheritdoc/>
+        public override void AddStepsToTracker()
+        {
+            Steps.Add(GroupSignalsByApproaches);
+            Steps.Add(FilteredDetectorData);
+            Steps.Add(GroupDetectorsByDetectorEvent);
+            Steps.Add(AggregateDetectorEvents);
+        }
+
+        /// <inheritdoc/>
+        public override void LinkSteps()
+        {
+            Input.LinkTo(GroupSignalsByApproaches, new DataflowLinkOptions() { PropagateCompletion = true });
+
+            GroupSignalsByApproaches.LinkTo(filteredDetectorData, new DataflowLinkOptions() { PropagateCompletion = true });
+            filteredDetectorData.LinkTo(FilteredDetectorData, new DataflowLinkOptions() { PropagateCompletion = true });
+            FilteredDetectorData.LinkTo(GroupDetectorsByDetectorEvent, new DataflowLinkOptions() { PropagateCompletion = true });
+            GroupDetectorsByDetectorEvent.LinkTo(AggregateDetectorEvents, new DataflowLinkOptions() { PropagateCompletion = true });
+
+            AggregateDetectorEvents.LinkTo(Output, new DataflowLinkOptions() { PropagateCompletion = true });
+        }
+    }
+}
