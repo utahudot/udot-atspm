6f720f5bfa5470d073c4d776e88d6f821258a9ec
Christian Baker <christianbaker@utah.gov>
2023-12-05 09:54:26 -0700

    Completed AggregatePhaseTerminationEvents

--- ATSPM/ApplicationCore/Analysis/WorkflowSteps/AggregateSignalPlans.cs
+++ ATSPM/ApplicationCore/Analysis/WorkflowSteps/AggregateSignalPlans.cs
@@ -10,20 +10,20 @@ using System.Threading.Tasks.Dataflow;
 
 namespace ATSPM.Application.Analysis.WorkflowSteps
 {
-    public class AggregateSignalPlans : TransformProcessStepBase<Tuple<Signal, int, IEnumerable<Plan>>, Tuple<Signal, int, IEnumerable<SignalPlanAggregation>>>
+    public class AggregateSignalPlans : TransformProcessStepBase<Tuple<Signal, int, IEnumerable<Plan>>, IEnumerable<SignalPlanAggregation>>
     {
         public AggregateSignalPlans(ExecutionDataflowBlockOptions dataflowBlockOptions = default) : base(dataflowBlockOptions) { }
 
-        protected override Task<Tuple<Signal, int, IEnumerable<SignalPlanAggregation>>> Process(Tuple<Signal, int, IEnumerable<Plan>> input, CancellationToken cancelToken = default)
+        protected override Task<IEnumerable<SignalPlanAggregation>> Process(Tuple<Signal, int, IEnumerable<Plan>> input, CancellationToken cancelToken = default)
         {
-            var result = Tuple.Create(input.Item1, input.Item2, input.Item3
+            var result = input.Item3
                 .Select(s => new SignalPlanAggregation()
                 {
                     SignalIdentifier = s.SignalIdentifier,
                     PlanNumber = s.PlanNumber,
                     Start = s.Start,
                     End = s.End,
-                }));
+                });
 
             return Task.FromResult(result);
         }
