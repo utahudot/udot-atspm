e09052f2dca7310f2f3ed2ca7c367a02d51457ba
Christian Baker <christianbaker@utah.gov>
2023-12-05 09:54:26 -0700

    Completed AggregatePhaseTerminationEvents

--- ATSPM/ApplicationCore/Analysis/WorkflowFilters/FilteredDetectorData.cs
+++ ATSPM/ApplicationCore/Analysis/WorkflowFilters/FilteredDetectorData.cs
@@ -15,29 +15,13 @@ namespace ATSPM.Application.Analysis.WorkflowFilters
     /// <item><see cref="DataLoggerEnum.DetectorOn"/></item>
     /// </list>
     /// </summary>
-    public class FilteredDetectorData : ProcessStepBase<Tuple<Approach, IEnumerable<ControllerEventLog>>, Tuple<Approach, IEnumerable<ControllerEventLog>>>
+    public class FilteredDetectorData : FilterEventCodeSignalBase
     {
-        /// <summary>
-        /// List of filtered event codes
-        /// </summary>
-        protected List<int> filteredList = new();
-
         /// <inheritdoc/>
         public FilteredDetectorData(DataflowBlockOptions dataflowBlockOptions = default) : base(dataflowBlockOptions)
         {
             filteredList.Add((int)DataLoggerEnum.DetectorOff);
             filteredList.Add((int)DataLoggerEnum.DetectorOn);
-
-            workflowProcess = new BroadcastBlock<Tuple<Approach, IEnumerable<ControllerEventLog>>>(f =>
-            {
-                var dc = f.Item1.Detectors.Select(s => s.DetectorChannel).ToList();
-
-                var result = Tuple.Create(f.Item1, f.Item2.Where(w => w.SignalIdentifier == f.Item1.Signal.SignalIdentifier && filteredList.Contains(w.EventCode) && dc.Contains(w.EventParam)));
-
-                return result;
-
-            }, options);
-            workflowProcess.Completion.ContinueWith(t => Console.WriteLine($"!!!Task {options.NameFormat} is complete!!!"));
         }
     }
 }
