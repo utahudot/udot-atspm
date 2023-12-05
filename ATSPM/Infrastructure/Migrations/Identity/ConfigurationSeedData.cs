dc0a525e52210e5b5887c7bba53b07c42629c294
Jaithra <jbhatia@avenueconsultants.com>
2023-12-05 14:56:19 -0700

    finish adding all files for identity

---
+++ ATSPM/Infrastructure/Migrations/Identity/ConfigurationSeedData.cs
@@ -0,0 +1,222 @@
+﻿using Duende.IdentityServer.EntityFramework.Mappers;
+using Duende.IdentityServer.Models;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using static IdentityModel.OidcConstants;
+
+namespace ATSPM.Infrastructure.Migrations.Identity
+{
+    public static class ConfigurationSeedData
+    {
+        // Methods to define seed data
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+        {
+            new ApiResource("reportsApi", "Reports API")
+            {
+                Scopes = { "reports.public", "reports.private" }
+            },
+
+            new ApiResource("dataApi", "Data API")
+            {
+                Scopes = { "data.access" }
+            },
+
+            new ApiResource("configApi", "Configuration API")
+            {
+                Scopes = { "config.public", "config.admin" }
+            },
+
+            new ApiResource("identityApi", "Identity API")
+            {
+                Scopes = { "config.public", "config.admin" }
+            },
+
+            // If the Admin Utility interacts with an API, it could also be represented as an ApiResource.
+            // If it's just an executable without a backend API, then it may not need an ApiResource representation.
+            new ApiResource("adminUtilityApi", "Admin Utility API")
+            {
+                Scopes = { "admin.utility" }
+            }
+        };
+        }
+
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>
+        {
+            new ApiScope("reports.private", "Access to private reports"),
+            new ApiScope("reports.public", "Access to public reports"),
+            new ApiScope("data.access", "Access to data API"),
+            new ApiScope("config.admin", "Access to admin configuration"),
+            new ApiScope("admin.utility", "Access to admin utility"),
+            new ApiScope("config.public", "Basic user access")
+        };
+        }
+
+        // ... other parts of ConfigurationSeedData ...
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+    {
+        //new IdentityResources.OpenId(),
+        //new IdentityResources.Profile(),
+        // ... other identity resources ...
+    };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+    {
+         new Client
+        {
+            ClientId = "ATSPMWeb",
+             ClientSecrets =
+            {
+                new Secret("ATSPMWebTest".ToSha256())
+            },
+            AllowedScopes = {
+                "reports.public",
+                "reports.private",
+                "data.access",
+                "config.admin"
+                // ... other scopes ...
+            }
+        },
+        new Client
+        {
+            ClientId = "EventLogUtility",
+            ClientSecrets =
+            {
+                new Secret("ATSPMEventLogUtilityTest".ToSha256())
+            },
+            AllowedScopes = { "admin.utility" }
+        },
+        new Client
+        {
+            ClientId = "PostmanTest",
+            ClientSecrets =
+            {
+                new Secret("PostmanTest".ToSha256())
+            },
+            RedirectUris = { "https://127.0.0.1:44357" },
+            AllowedGrantTypes = new List<string> {  GrantTypes.ClientCredentials },
+            AllowedScopes = { "reports.public","reports.private","config.admin","config.public","admin.utility" },
+            RequireConsent = false,
+            RequirePkce = false,
+        },
+            new Client
+        {
+            ClientId = "Identity",
+                ClientSecrets =
+            {
+                new Secret("IdentityTest".ToSha256())
+            },
+            AllowedScopes = { "config.admin", "config.public" }
+        },
+    };
+        }
+
+        public static void Seed(IdentityConfigurationContext context)
+        {
+            // ... seeding logic for ApiResources and ApiScopes ...
+
+            // Seed IdentityResources
+            if (!context.IdentityResources.Any())
+            {
+                foreach (var resource in GetIdentityResources())
+                {
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            // Seed Clients
+            if (!context.Clients.Any())
+            {
+                foreach (var client in GetClients())
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            // Seed ApiResources
+            if (!context.ApiResources.Any())
+            {
+                foreach (var resource in GetApiResources())
+                {
+                    context.ApiResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            // Seed ApiScopes
+            if (!context.ApiScopes.Any())
+            {
+                foreach (var scope in GetApiScopes())
+                {
+                    context.ApiScopes.Add(scope.ToEntity());
+                }
+                context.SaveChanges();
+            }
+
+            // ... similar logic for seeding clients, identity resources, etc. ...
+        }
+
+        private static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            // Define your roles
+            string[] roles = new string[]
+            {
+        "Admin",
+        "User",
+                // Add other roles as needed
+            };
+
+            foreach (var roleName in roles)
+            {
+                // Check if the role already exists
+                if (!roleManager.RoleExistsAsync(roleName).Result)
+                {
+                    // Create the role
+                    var role = new IdentityRole(roleName);
+                    var result = roleManager.CreateAsync(role).Result;
+
+                    if (result.Succeeded)
+                    {
+                        // Add claims to the role
+                        AddClaimsToRole(roleManager, roleName);
+                    }
+                    else
+                    {
+                        // Handle role creation failure
+                    }
+                }
+            }
+        }
+
+        private static void AddClaimsToRole(RoleManager<IdentityRole> roleManager, string roleName)
+        {
+            var claims = CustomClaims.GetAllClaimsFromCustomClaims();
+
+            var role = roleManager.FindByNameAsync(roleName).Result;
+            if (role != null)
+            {
+                foreach (var claim in claims)
+                {
+                    roleManager.AddClaimAsync(role, new Claim(ClaimTypes.Role, claim)).Wait();
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ Identity/Controllers/AccountController.cs
@@ -0,0 +1,189 @@
+﻿using Identity.Business.Accounts;
+using Identity.Models.Account;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        private readonly IAccountService _accountService;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IConfiguration configuration,
+            IAccountService accountService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+            _accountService = accountService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                Agency = model.Agency,
+                FirstName = model.FirstName,
+                LastName = model.LastName
+            };
+
+            AccountResult result = await _accountService.CreateUser(user, model.Password);
+            if (result.Code == StatusCodes.Status400BadRequest)
+            {
+                return BadRequest(result);
+            }
+            if (result.Code == StatusCodes.Status500InternalServerError)
+            {
+                return new ObjectResult(result) { StatusCode = result.Code };
+            }
+            //check agencyExists
+            // if no, then create user with admin roles
+            // if yes, then just create user with no roles
+            // then log in the user as well and send back the credentials for logged in
+            //if error in any processes above can just send back the result since identity service also handles the errors or success message
+
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _accountService.Login(model.Email, model.Password, model.RememberMe);
+
+            if (result.Code == StatusCodes.Status200OK)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+
+        //[HttpPost("forgot-password")]
+        //public async Task<IActionResult> ForgotPassword(LoginViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    var result = await _accountService.Login(model.Email, model.Password, model.RememberMe);
+
+        //    if (result.Code == StatusCodes.Status200OK)
+        //    {
+        //        return Ok(result);
+        //    }
+
+        //    return BadRequest(result);
+        //}
+
+        [HttpPost("external-login")]
+        public IActionResult ExternalLogin(LoginViewModel model)
+        {
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties("Google", "http://localhost:44357/api/account/google-callback");
+            return Challenge(properties, "Google");
+
+        }
+
+        [HttpGet("google-callback")]
+        public async Task<IActionResult> GoogleCallback()
+        {
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+            if (info == null)
+            {
+                // Handle external login failure
+                return BadRequest();
+            }
+
+            // Sign in the user using external login information
+            var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false);
+
+            if (result.Succeeded)
+            {
+                // Redirect to the desired URL after successful login
+                return Redirect("http://localhost:3000/callback");
+            }
+            else
+            {
+                // Handle the case where the user does not have an account yet
+                // You may want to redirect the user to a registration page
+                return BadRequest("User does not have an account");
+            }
+        }
+
+        [Authorize]
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok(new { Message = "Successfully logged out." });
+        }
+
+        [HttpPost("changepassword")]
+        [Authorize]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+
+            var result = await _userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+
+            if (result != null && result.Succeeded)
+            {
+                return Ok();
+            }
+
+            return BadRequest(result?.Errors);
+        }
+
+        [HttpPost("forgotpassword")]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        {
+            if (model.Email == null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+            {
+                // To prevent user enumeration attacks, return a generic error message
+                // instead of providing information whether the user exists or the email is confirmed.
+                return Ok("An email will be sent with the reset instructions.");
+            }
+
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            // Send the password reset token to the user's email for further steps.
+
+            return Ok("An email will be sent with the reset instructions");
+        }
+    }
+
+}
