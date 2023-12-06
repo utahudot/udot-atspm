5c81504105413b94dd8d873cf8c8ad6ce57e028a
Christian Baker <christianbaker@utah.gov>
2023-12-06 12:36:15 -0700

    Created AggregateDetectorEventsTests

--- ATSPM/DomainCore/Extensions/DateTimeExtensions.cs
+++ ATSPM/DomainCore/Extensions/DateTimeExtensions.cs
@@ -11,7 +11,7 @@ namespace ATSPM.Domain.Extensions
         public static DateTime RoundUp(this DateTime dt, TimeSpan d)
         {
             var modTicks = dt.Ticks % d.Ticks;
-            var delta = modTicks != 0 ? d.Ticks - modTicks : 0;
+            var delta = modTicks != 0 ? d.Ticks - modTicks : d.Ticks;
             return new DateTime(dt.Ticks + delta, dt.Kind);
         }
 
