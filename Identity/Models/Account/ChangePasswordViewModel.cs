624d2833260d3f5659549478de5983f12c7b795f
avetraffic <96312362+avetraffic@users.noreply.github.com>
2023-12-06 14:53:08 -0700

    Ave 422 identity (#26)
    * add identity context
    
    * add files for different identity context
    
    * finish adding all files for identity
    
    * finish adding identity test and fully pass them
    
    * Add watchdog and its tests
    
    ---------
    
    Co-authored-by: Jaithra <jbhatia@avenueconsultants.com>

---
+++ Identity/Models/Account/ChangePasswordViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models.Account
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Compare("NewPassword")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
