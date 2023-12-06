59bfd9f6e26adf6b3580f540ff68244fa73b6cbb
Christian Baker <christianbaker@utah.gov>
2023-12-06 09:19:35 -0700

    Created GroupSignalsByApproachesTests

--- ATSPM/ApplicationCore/Analysis/WorkflowSteps/GroupSignalsByApproaches.cs
+++ ATSPM/ApplicationCore/Analysis/WorkflowSteps/GroupSignalsByApproaches.cs
@@ -11,13 +11,23 @@ using System.Threading.Tasks.Dataflow;
 
 namespace ATSPM.Application.Analysis.WorkflowSteps
 {
+    /// <summary>
+    /// Breaks out <see cref="Approach"/> from <see cref="Signal"/>
+    /// and returns separate Tuples of <see cref="Approach"/>/<see cref="ControllerEventLog"/> pairs.
+    /// Sorted by <see cref="ControllerEventLog.Timestamp"/>.
+    /// </summary>
     public class GroupSignalsByApproaches : TransformManyProcessStepBase<Tuple<Signal, IEnumerable<ControllerEventLog>>, Tuple<Approach, IEnumerable<ControllerEventLog>>>
     {
+        /// <inheritdoc/>
         public GroupSignalsByApproaches(ExecutionDataflowBlockOptions dataflowBlockOptions = default) : base(dataflowBlockOptions) { }
 
+        /// <inheritdoc/>
         protected override Task<IEnumerable<Tuple<Approach, IEnumerable<ControllerEventLog>>>> Process(Tuple<Signal, IEnumerable<ControllerEventLog>> input, CancellationToken cancelToken = default)
         {
-            var result = input.Item1.Approaches.Select(s => Tuple.Create(s, input.Item2.FromSpecification(new ControllerLogSignalFilterSpecification(input.Item1))));
+            var signal = input.Item1;
+            var logs = input.Item2;
+            
+            var result = signal.Approaches.Select(s => Tuple.Create(s, logs.FromSpecification(new ControllerLogSignalFilterSpecification(signal))));
 
             return Task.FromResult(result);
         }
