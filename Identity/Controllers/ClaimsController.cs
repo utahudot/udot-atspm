1035640a8fbeb0d9723c4c8c295d0453e4f0a167
avetraffic <96312362+avetraffic@users.noreply.github.com>
2023-12-06 14:53:08 -0700

    Ave 422 identity (#26)
    * add identity context
    
    * add files for different identity context
    
    * finish adding all files for identity
    
    * finish adding identity test and fully pass them
    
    * Add watchdog and its tests
    
    ---------
    
    Co-authored-by: Jaithra <jbhatia@avenueconsultants.com>

---
+++ Identity/Controllers/ClaimsController.cs
@@ -0,0 +1,46 @@
+﻿
+using ATSPM.Identity.Business.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ClaimsController : ControllerBase
+{
+    private readonly ClaimsService _claimsService;
+
+    public ClaimsController(ClaimsService claimsService)
+    {
+        _claimsService = claimsService;
+    }
+
+    // GET: api/claims/roleName
+    [HttpGet("{roleName}")]
+    public async Task<IActionResult> GetClaimsForRole(string roleName)
+    {
+        return Ok(await _claimsService.GetAllClaimsForRole(roleName));
+    }
+
+    // POST: api/claims/roleName
+    [HttpPost("{roleName}")]
+    public async Task<IActionResult> AddClaimToRole(string roleName, [FromBody] ClaimModel claim)
+    {
+        if (await _claimsService.AddClaimToRole(roleName, claim.Type, claim.Value))
+            return Ok();
+        return BadRequest("Could not add claim to role.");
+    }
+
+    // DELETE: api/claims/roleName
+    [HttpDelete("{roleName}")]
+    public async Task<IActionResult> RemoveClaimFromRole(string roleName, [FromBody] ClaimModel claim)
+    {
+        if (await _claimsService.RemoveClaimFromRole(roleName, claim.Type, claim.Value))
+            return Ok();
+        return BadRequest("Could not remove claim from role.");
+    }
+}
+
+public class ClaimModel
+{
+    public string Type { get; set; }
+    public string Value { get; set; }
+}
