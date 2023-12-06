64a57b79a8fc94c38b28fbf935c8d0b97923527d
avetraffic <96312362+avetraffic@users.noreply.github.com>
2023-12-06 14:53:08 -0700

    Ave 422 identity (#26)
    * add identity context
    
    * add files for different identity context
    
    * finish adding all files for identity
    
    * finish adding identity test and fully pass them
    
    * Add watchdog and its tests
    
    ---------
    
    Co-authored-by: Jaithra <jbhatia@avenueconsultants.com>

---
+++ ATSPM/Data/Configuration/UserAreaConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using ATSPM.Data.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ATSPM.Data.Configuration
+{
+    public class UserAreaConfiguration : IEntityTypeConfiguration<UserArea>
+    {
+        public void Configure(EntityTypeBuilder<UserArea> builder)
+        {
+            builder
+            .HasKey(ur => new { ur.UserId, ur.AreaId });
+
+            //builder
+            //    .HasOne(ur => ur.User)
+            //    .WithMany(u => u.UserAreas)
+            //    .HasForeignKey(ur => ur.UserId);
+
+            builder
+                .HasOne(ur => ur.Area)
+                .WithMany(r => r.UserAreas)
+                .HasForeignKey(ur => ur.AreaId);
+        }
+    }
+}
