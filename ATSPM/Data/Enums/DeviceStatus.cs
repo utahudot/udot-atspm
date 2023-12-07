5416ce5418b53f44d3e19f3ee667142d53965eca
Christian Baker <christianbaker@utah.gov>
2023-12-07 13:22:10 -0700

    Added products and devices to config database

---
+++ ATSPM/Data/Enums/DeviceStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace ATSPM.Data.Enums
+{
+    public enum DeviceStatus
+    {
+        Unknown,
+        Decommissioned,
+        Inactive,
+        Active,
+        Testing,
+        Staging
+    }
+}
