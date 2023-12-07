b69cfdbac84fcfec10bae3d5be2a73997e75212d
Christian Baker <christianbaker@utah.gov>
2023-12-07 10:35:23 -0700

    Added VersionHistoryController

--- ATSPM/Infrastructure/Extensions/ServiceExtensions.cs
+++ ATSPM/Infrastructure/Extensions/ServiceExtensions.cs
@@ -65,6 +65,7 @@ namespace ATSPM.Infrastructure.Extensions
             services.AddScoped<IRouteSignalsRepository, RouteSignalEFRepository>();
             services.AddScoped<ISettingsRepository, SettingsEFRepository>();
             services.AddScoped<ISignalRepository, SignalEFRepository>();
+            services.AddScoped<IVersionHistoryRepository, VersionHistoryEFRepository>();
 
             return services;
         }
