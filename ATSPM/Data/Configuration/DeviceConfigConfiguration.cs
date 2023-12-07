302b1c981773e656dd7d290abf6586329136c5fe
Christian Baker <christianbaker@utah.gov>
2023-12-07 13:22:10 -0700

    Added products and devices to config database

---
+++ ATSPM/Data/Configuration/DeviceConfigConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using ATSPM.Data.Enums;
+using ATSPM.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ATSPM.Data.Configuration
+{
+    /// <summary>
+    /// Device configuration configuration
+    /// </summary>
+    public class DeviceConfigConfiguration : IEntityTypeConfiguration<DeviceConfiguration>
+    {
+        /// <inheritdoc/>
+        public void Configure(EntityTypeBuilder<DeviceConfiguration> builder)
+        {
+            builder.HasComment("DeviceConfiguration");
+
+            builder.Property(e => e.Firmware)
+                .IsRequired()
+                .HasMaxLength(16);
+
+            builder.Property(e => e.Notes)
+                .HasMaxLength(512);
+
+            builder.Property(e => e.Protocol)
+                .HasMaxLength(12)
+                .HasDefaultValue(TransportProtocols.Unknown);
+
+            builder.Property(e => e.Port)
+                .HasDefaultValueSql("((0))");
+
+            builder.Property(e => e.Directory)
+                .IsRequired(false)
+                .HasMaxLength(1024);
+
+            builder.Property(e => e.SearchTerm)
+                .IsRequired(false)
+                .HasMaxLength(128);
+
+            builder.Property(e => e.UserName)
+                .IsRequired(false)
+                .HasMaxLength(50);
+
+            builder.Property(e => e.Password)
+                .IsRequired(false)
+                .HasMaxLength(50);
+        }
+    }
+}
---
+++ ATSPM/Data/Relationships/IRelatedDevices.cs
@@ -0,0 +1,15 @@
+﻿using ATSPM.Data.Models;
+
+namespace ATSPM.Data.Relationships
+{
+    /// <summary>
+    /// Related devices
+    /// </summary>
+    public interface IRelatedDevices
+    {
+        /// <summary>
+        /// Collection of devices
+        /// </summary>
+        ICollection<Device> Devices { get; set; }
+    }
+}
