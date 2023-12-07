75f29319e7a54cc42f408754b6d2ec7a2c8eec96
Christian Baker <christianbaker@utah.gov>
2023-12-07 10:35:23 -0700

    Added VersionHistoryController

---
+++ ATSPM/ConfigApi/Controllers/VersionHistoryController.cs
@@ -0,0 +1,33 @@
+﻿using Asp.Versioning;
+using ATSPM.Application.Repositories;
+using ATSPM.Data.Models;
+
+namespace ATSPM.ConfigApi.Controllers
+{
+    /// <summary>
+    /// Version history controller
+    /// </summary>
+    [ApiVersion(1.0)]
+    public class VersionHistoryController : AtspmConfigControllerBase<VersionHistory, int>
+    {
+        private readonly IVersionHistoryRepository _repository;
+
+        /// <inheritdoc/>
+        public VersionHistoryController(IVersionHistoryRepository repository) : base(repository)
+        {
+            _repository = repository;
+        }
+
+        #region NavigationProperties
+
+        #endregion
+
+        #region Actions
+
+        #endregion
+
+        #region Functions
+
+        #endregion
+    }
+}
