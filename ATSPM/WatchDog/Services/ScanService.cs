0fc38086dc3a6f441a1defd57213e0a0cf74ab49
dloweAvenueConsultants <92349445+dloweAvenueConsultants@users.noreply.github.com>
2023-12-18 12:33:05 -0700

    Ave 446 split monitor bugs (#32)
    * Removed folder from tracking
    
    * Removed folder from tracking
    
    * Removed folder from tracking
    
    * updated gitignore
    
    * Removed folder from tracking
    
    * Removed folder from tracking
    
    * Removed folder from tracking
    
    * bug fix for split monitor
    
    * Enable timestamp behavior
    
    * Add hosting to watchdog.
    
    * Removed folder from tracking
    
    * Removed folder from tracking
    
    * Removed folder from tracking
    
    * Removed folder from tracking
    
    * git ignore update
    
    * Removed folder from tracking
    
    * Removed folder from tracking
    
    * Removed folder from tracking
    
    * Merge with GCP branch

--- ATSPM/WatchDog/Services/ScanService.cs
+++ ATSPM/WatchDog/Services/ScanService.cs
@@ -157,26 +157,8 @@ namespace WatchDog.Services
             return usersWithWatchDogClaim;
         }
 
-
-
-
-
-
-
     }
 
-    //public class EventsContainer
-    //{
-    //    public ConcurrentBag<WatchDogLogEvent> ForceOffErrors = new ConcurrentBag<WatchDogLogEvent>();
-    //    public ConcurrentBag<WatchDogLogEvent> LowHitCountErrors = new ConcurrentBag<WatchDogLogEvent>();
-    //    public ConcurrentBag<WatchDogLogEvent> MaxOutErrors = new ConcurrentBag<WatchDogLogEvent>();
-    //    public ConcurrentBag<WatchDogLogEvent> MissingRecords = new ConcurrentBag<WatchDogLogEvent>();
-    //    public ConcurrentBag<WatchDogLogEvent> CannotFtpFiles = new ConcurrentBag<WatchDogLogEvent>();
-    //    public List<WatchDogLogEvent> RecordsFromTheDayBefore = new List<WatchDogLogEvent>();
-    //    public ConcurrentBag<Location> LocationsNoRecords = new ConcurrentBag<Location>();
-    //    public ConcurrentBag<Location> LocationsWithRecords = new ConcurrentBag<Location>();
-    //    public ConcurrentBag<WatchDogLogEvent> StuckPedErrors = new ConcurrentBag<WatchDogLogEvent>();
-    //}
 
 
 }
