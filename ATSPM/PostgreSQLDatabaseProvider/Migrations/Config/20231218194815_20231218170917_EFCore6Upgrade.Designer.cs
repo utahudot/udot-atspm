37081fe93b39e5e6eb6d1e58c7d4c4225171d1b7
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2023-12-19 16:25:42 -0700

    Adding Migration for Postgres

--- ATSPM/PostgreSQLDatabaseProvider/Migrations/Config/20231218194815_20231218170917_EFCore6Upgrade.Designer.cs
+++ ATSPM/PostgreSQLDatabaseProvider/Migrations/Config/20231218194815_20231218170917_EFCore6Upgrade.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ATSPM.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ATSPM.Infrastructure.PostgreSQLDatabaseProvider.Migrations.Config
 {
     [DbContext(typeof(ConfigContext))]
-    partial class ConfigContextModelSnapshot : ModelSnapshot
+    [Migration("20231218194815_20231218170917_EFCore6Upgrade")]
+    partial class _20231218170917_EFCore6Upgrade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
