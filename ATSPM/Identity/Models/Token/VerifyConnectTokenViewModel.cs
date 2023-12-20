389c0fee0cc707d72d7f2a73aac8943bc5f073a4
Jaithra <jbhatia@avenueconsultants.com>
2023-12-20 13:30:45 -0700

    update code to work with the controllers for identity

---
+++ ATSPM/Identity/Models/Token/VerifyConnectTokenViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models.Token
+{
+    public class VerifyConnectTokenViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+    }
+}
