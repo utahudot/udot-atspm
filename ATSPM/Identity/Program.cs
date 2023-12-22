4ba54dc60e5bcbd4f510c247dd94066070e733b7
Jaithra <jbhatia@avenueconsultants.com>
2023-12-22 10:36:59 -0700

    trying to update code to handle token creation on api call

--- ATSPM/Identity/Program.cs
+++ ATSPM/Identity/Program.cs
@@ -102,6 +102,8 @@ builder.Host.ConfigureServices((host, services) =>
         options.ClientSecret = "***REMOVED***";
     });
 
+    services.AddClientCredentialsTokenManagement();
+
     //This is for the production certificate
     //var certificate = new X509Store(StoreName.My, StoreLocation.LocalMachine)
     //.Certificates
