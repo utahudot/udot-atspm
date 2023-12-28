2f1f616363a6c7daa642c58426d37792c762d3fb
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2023-12-28 14:38:01 -0700

    Fixes for Watchdog, Unkown Terminations, Wait time

---
+++ ATSPM/ReportAPI/Business/Common/WaitTimeCycle.cs
@@ -0,0 +1,21 @@
+﻿using ATSPM.Data.Models;
+
+namespace ATSPM.ReportApi.Business.Common
+{
+    /// <summary>
+    ///     Data that represents a red to red cycle for a Location phase
+    /// </summary>
+    public class WaitTimeCycle
+    {
+        public WaitTimeCycle(DateTime redEvent, DateTime greenEvent)
+        {
+            PhaseRegisterDroppedCalls = new List<ControllerEventLog>();
+            RedEvent = redEvent;
+            GreenEvent = greenEvent;
+        }
+
+        public List<ControllerEventLog> PhaseRegisterDroppedCalls { get; set; }
+        public DateTime RedEvent { get; }
+        public DateTime GreenEvent { get; }
+    }
+}
