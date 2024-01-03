58f2888b67aeba056c79967f42fa50b389b0ecea
Jaithra <jbhatia@avenueconsultants.com>
2024-01-03 08:13:42 -0700

    finish cleaning up api calls

--- ATSPM/Identity/Business/Claims/ClaimsService.cs
+++ ATSPM/Identity/Business/Claims/ClaimsService.cs
@@ -1,21 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Data;
+using System.Security.Claims;
 
 namespace ATSPM.Identity.Business.Claims
 {
     public class ClaimsService
     {
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IdentityContext _context;
 
-        public ClaimsService(RoleManager<IdentityRole> roleManager)
+        public ClaimsService(RoleManager<IdentityRole> roleManager, IdentityContext context)
         {
             _roleManager = roleManager;
+            _context = context;
         }
 
         public async Task<IList<string>> GetAllClaimsForRole(string roleName)
         {
             var role = await _roleManager.FindByNameAsync(roleName);
             var claims = await _roleManager.GetClaimsAsync(role);
-            return claims.Select(c => c.Type).ToList();
+            return claims.Select(c => c.Value).ToList();
         }
 
         public async Task<bool> AddClaimToRole(string roleName, string claimType, string claimValue)
@@ -30,6 +34,32 @@ namespace ATSPM.Identity.Business.Claims
             var role = await _roleManager.FindByNameAsync(roleName);
             var result = await _roleManager.RemoveClaimAsync(role, new System.Security.Claims.Claim(claimType, claimValue));
             return result.Succeeded;
+        }
+
+        public async Task AddClaimsToRole(string roleName, List<string> claims)
+        {
+            var existingRole = await _roleManager.FindByNameAsync(roleName);
+            var existingClaims = _roleManager.GetClaimsAsync(existingRole).Result.Select(claim => claim.Value);
+
+            var i1 = claims.Except(existingClaims).ToList();
+            var i2 = existingClaims.Except(claims).ToList();
+
+            var u = i1.Union(i2).ToList();
+            foreach (var claimName in u)
+            {
+                var roleHasClaim = existingClaims.Any(c => c == claimName);
+                var existingClaim = new Claim(ClaimTypes.Role, claimName);
+
+                if (!roleHasClaim)
+                {
+                    // Add the existing claim to the role
+                    await _roleManager.AddClaimAsync(existingRole, existingClaim);
+                } else
+                {
+                    await _roleManager.RemoveClaimAsync(existingRole, existingClaim);
+                }
+                
+            }
         }
     }
 }
--- ATSPM/Identity/Controllers/ClaimsController.cs
+++ ATSPM/Identity/Controllers/ClaimsController.cs
@@ -1,16 +1,35 @@
 ﻿
 using ATSPM.Identity.Business.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
+[Authorize()]
 [Route("api/[controller]")]
 [ApiController]
 public class ClaimsController : ControllerBase
 {
     private readonly ClaimsService _claimsService;
+    private readonly RoleManager<IdentityRole> _roleManager;
 
-    public ClaimsController(ClaimsService claimsService)
+    public ClaimsController(ClaimsService claimsService, RoleManager<IdentityRole> roleManager)
     {
         _claimsService = claimsService;
+        _roleManager = roleManager;
+    }
+
+    // GET: api/claims
+    [HttpGet]
+    public async Task<IActionResult> GetClaims()
+    {
+        var roles = _roleManager.Roles.ToList();
+        var claims = new List<string>();
+        foreach (var role in roles)
+        {
+            var roleClaims = await _claimsService.GetAllClaimsForRole(role.Name);
+            claims = claims.Concat(roleClaims).ToList();
+        }
+        return Ok(new ClaimsModel() { Claims = new HashSet<string>(claims).ToList()});
     }
 
     // GET: api/claims/roleName
@@ -29,6 +48,23 @@ public class ClaimsController : ControllerBase
         return BadRequest("Could not add claim to role.");
     }
 
+    // POST: api/claims/addClaims/roleName
+    [HttpPost("addClaims/{roleName}")]
+    public async Task<IActionResult> AddClaimsToRole(string roleName, [FromBody] ClaimsModel model)
+    {
+        try
+        {
+            await _claimsService.AddClaimsToRole(roleName, model.Claims);
+            return Ok();
+
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        //return BadRequest("Could not add claim to role.");
+    }
+
     // DELETE: api/claims/roleName
     [HttpDelete("{roleName}")]
     public async Task<IActionResult> RemoveClaimFromRole(string roleName, [FromBody] ClaimModel claim)
@@ -43,4 +79,10 @@ public class ClaimModel
 {
     public string Type { get; set; }
     public string Value { get; set; }
+}
+
+
+public class ClaimsModel
+{
+    public List<string> Claims { get; set; }
 }
