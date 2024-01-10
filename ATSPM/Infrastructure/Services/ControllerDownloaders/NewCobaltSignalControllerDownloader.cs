6cc2c5db070dce24803d41ade2144463ec195a31
Christian Baker <christianbaker@utah.gov>
2024-01-10 10:59:44 -0700

    Added TransformManyProcessStepBaseAsync

--- ATSPM/Infrastructure/Services/ControllerDownloaders/NewCobaltSignalControllerDownloader.cs
+++ ATSPM/Infrastructure/Services/ControllerDownloaders/NewCobaltSignalControllerDownloader.cs
@@ -8,13 +8,5 @@ namespace ATSPM.Infrastructure.Services.ControllerDownloaders
     public class NewCobaltLocationControllerDownloader : ControllerDownloaderBase
     {
         public NewCobaltLocationControllerDownloader(ISFTPDownloaderClient client, ILogger<NewCobaltLocationControllerDownloader> log, IOptionsSnapshot<SignalControllerDownloaderConfiguration> options) : base(client, log, options) { }
-
-        #region Properties
-
-        public override int ControllerType => 10;
-
-        public override string[] FileFilters { get; set; } = new string[] { "dat", "datZ" };
-
-        #endregion
     }
 }
