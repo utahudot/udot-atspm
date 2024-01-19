48aeba4ec01819aeb258ec9796da8a20219a6535
avetraffic <96312362+avetraffic@users.noreply.github.com>
2024-01-19 11:56:43 -0700

    Ave 468 time space controller (#36)
    * Finish off historic time space results
    
    * Add to program file
    
    * use cycle service to get cycles
    
    * dont print data for routes with no distances
    
    * Code cleanup
    
    * rename timespaceoptions
    
    ---------
    
    Co-authored-by: Jaithra <jbhatia@avenueconsultants.com>

--- ATSPM/ReportApi/Business/TimingAndActuation/TimingAndActuationsOptions.cs
+++ ATSPM/ReportApi/Business/TimingAndActuation/TimingAndActuationsOptions.cs
@@ -2,16 +2,14 @@ using ATSPM.ReportApi.Business.Common;
 
 namespace ATSPM.ReportApi.Business.TimingAndActuation
 {
-    public class TimingAndActuationsOptions : OptionsBase
+    public class TimingAndActuationsOptions : BasePhaseOptions
     {
         public bool ShowPedestrianIntervals { get; set; }
         public bool ShowPedestrianActuation { get; set; }
-        public double ExtendStartStopSearch { get; set; }
         public bool ShowStopBarPresence { get; set; }
         public bool ShowLaneByLaneCount { get; set; }
         public bool ShowAdvancedDilemmaZone { get; set; }
         public bool ShowAdvancedCount { get; set; }
-        public bool ShowAllLanesInfo { get; set; }
         public List<int>? GlobalEventCodesList { get; set; }
         public List<int>? GlobalEventParamsList { get; set; }
         public List<int>? PhaseEventCodesList { get; set; }
