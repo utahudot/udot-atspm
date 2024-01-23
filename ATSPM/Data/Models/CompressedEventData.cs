d57e3f562d1f6613d1e886251eecedb5a2ca3209
Christian Baker <christianbaker@utah.gov>
2024-01-23 10:58:45 -0700

    Playing with table heirarchies in EF

--- ATSPM/Data/Models/CompressedEventData.cs
+++ ATSPM/Data/Models/CompressedEventData.cs
@@ -1,5 +1,7 @@
 ﻿using ATSPM.Data.EventModels;
 using ATSPM.Data.Interfaces;
+using ATSPM.Domain.Extensions;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 
@@ -11,25 +13,84 @@ namespace ATSPM.Data.Models
     {
         ///<inheritdoc/>
         public string LocationIdentifier { get; set; }
+
+        /// <summary>
+        /// Id of the device the logs came from
+        /// </summary>
+        public int DeviceId { get; set; }
         
         /// <summary>
         /// Day the data is archived for
         /// </summary>
         public DateOnly ArchiveDate { get; set; }
 
+        /// <summary>
+        /// Compressed log data
+        /// </summary>
         public IEnumerable<EventModelBase> LogData { get; set; }
 
-        //public IReadOnlyList<T> GetData<T>() where T : EventModelBase
-        //{
-        //    if (LogData is IEnumerable<T> a)
-        //        return a.ToList();
-        //    else 
-        //        return new List<T>();
-        //}
-
+        ///<inheritdoc/>
         public override string ToString()
         {
-            return $"{LocationIdentifier}-{ArchiveDate:dd/MM/yyyy}";
+            return $"{LocationIdentifier}-{ArchiveDate:dd/MM/yyyy} - {LogData.Count()}";
         }
+    }
+
+    public abstract class CompressedData : ILocationLayer
+    {
+        ///<inheritdoc/>
+        public string LocationIdentifier { get; set; }
+
+        public Type DataType { get; set; }
+
+        /// <summary>
+        /// Day the data is archived for
+        /// </summary>
+        public DateOnly ArchiveDate { get; set; }
+
+        /// <summary>
+        /// Compressed data
+        /// </summary>
+        public byte[] Data { get; set; }
+    }
+
+    public abstract class EventsBase : CompressedData
+    {
+        /// <summary>
+        /// Id of the device the logs came from
+        /// </summary>
+        public int DeviceId { get; set; }
+
+        //[JsonIgnore]
+        //public List<T> Events
+        //{
+        //    get
+        //    {
+        //        return JsonConvert.DeserializeObject<List<T>>(Data.GZipDecompressToString(), new JsonSerializerSettings()
+        //        {
+        //            TypeNameHandling = TypeNameHandling.Arrays
+        //        });
+        //    }
+        //    set
+        //    {
+        //        Data = JsonConvert.SerializeObject(value, new JsonSerializerSettings()
+        //        {
+        //            TypeNameHandling = TypeNameHandling.Arrays
+        //        }).GZipCompressToByte();
+        //    }
+        //}
+    }
+
+    public abstract class EventsTypeBase<T> : EventsBase where T : EventModelBase
+    {
+
+    }
+
+    public class CompressedIndiannaEvents : EventsTypeBase<IndiannaEvent>
+    {
+    }
+
+    public class CompressedPedestrianCounter : EventsTypeBase<PedestrianCounter>
+    {
     }
 }
