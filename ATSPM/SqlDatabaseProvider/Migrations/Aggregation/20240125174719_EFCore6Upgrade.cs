5d0957d32eee108b376f4174507c7ce78d069920
Christian Baker <christianbaker@utah.gov>
2024-01-25 11:31:53 -0700

    Updated AggregationContext to be compressed

---
+++ ATSPM/SqlDatabaseProvider/Migrations/Aggregation/20240125174719_EFCore6Upgrade.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ATSPM.Infrastructure.SqlDatabaseProvider.Migrations.Aggregation
+{
+    /// <inheritdoc />
+    public partial class EFCore6Upgrade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CompressedAggregations",
+                columns: table => new
+                {
+                    LocationIdentifier = table.Column<string>(type: "varchar(10)", unicode: false, maxLength: 10, nullable: false),
+                    ArchiveDate = table.Column<DateTime>(type: "Date", nullable: false),
+                    Data = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    DataType = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompressedAggregations", x => new { x.LocationIdentifier, x.ArchiveDate });
+                },
+                comment: "Compressed aggregations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompressedAggregations");
+        }
+    }
+}
