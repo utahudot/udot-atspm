e360149f3ee5a413d3d7dd64b5adfe90202f77c3
Jaithra <jbhatia@avenueconsultants.com>
2024-01-29 11:16:06 -0700

    checking in changes

--- ATSPM/Identity/Business/Accounts/AccountService.cs
+++ ATSPM/Identity/Business/Accounts/AccountService.cs
@@ -9,22 +9,18 @@ namespace Identity.Business.Accounts
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly IAgencyService _agencyService;
 
         public AccountService(
             UserManager<ApplicationUser> userManager,
-            IAgencyService agencyService,
             SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
-            _agencyService = agencyService;
             _signInManager = signInManager;
         }
 
         public async Task<AccountResult> CreateUser(ApplicationUser user, string password)
         {
-            //var tokenService = new TokenService("your_long_and_secure_key_here", "yourIssuer", "yourAudience");
-            //var agencyExists = await _agencyService.AgencyExistsAsync(user.Agency);
+            var tokenService = new TokenService("your_long_and_secure_key_here");
             var createUser = await _userManager.CreateAsync(user, password);
 
             // Just add new people as users
@@ -34,41 +30,43 @@ namespace Identity.Business.Accounts
             {
                 await _signInManager.SignInAsync(user, isPersistent: false);
                 var info = await _signInManager.UserManager.FindByEmailAsync(user.Email);
-                //var roles = await _signInManager.UserManager.GetRolesAsync(user);
-                //var token = tokenService.GenerateToken(user.Id, roles?.ToArray() ?? Array.Empty<string>(), user.Agency);
 
                 if (info != null)
                 {
-                    return new AccountResult(user.UserName, StatusCodes.Status200OK, "");
+
+                    var roles = await _signInManager.UserManager.GetRolesAsync(user);
+                    var token = tokenService.GenerateToken(user.Id, roles?.ToArray() ?? Array.Empty<string>());
+                    return new AccountResult(user.UserName, token, StatusCodes.Status200OK, "");
                 }
                 else
                 {
-                    return new AccountResult("", StatusCodes.Status500InternalServerError, "Server Error");
+                    return new AccountResult("", "", StatusCodes.Status500InternalServerError, "Server Error");
                 }
             }
 
-            return new AccountResult("", StatusCodes.Status400BadRequest, createUser.Errors.First().Description);
+            return new AccountResult("", "", StatusCodes.Status400BadRequest, createUser.Errors.First().Description);
         }
 
         public async Task<AccountResult> Login(string email, string password, bool rememberMe = false)
         {
-            //var tokenService = new TokenService("your_long_and_secure_key_here", "yourIssuer", "yourAudience");
+            var tokenService = new TokenService("your_long_and_secure_key_here");
             var result = await _signInManager.PasswordSignInAsync(email, password, rememberMe, lockoutOnFailure: false);
 
             if (result != null && result.Succeeded)
             {
                 var user = await _signInManager.UserManager.FindByEmailAsync(email);
-                //var roles = await _signInManager.UserManager.GetRolesAsync(user);
-                //var token = tokenService.GenerateToken(user.Id, roles?.ToArray() ?? Array.Empty<string>(), user.Agency);
                 if (user == null)
                 {
-                    return new AccountResult("", StatusCodes.Status400BadRequest, "User not found");
+                    return new AccountResult("", "", StatusCodes.Status400BadRequest, "User not found");
                 }
 
-                return new AccountResult(user.UserName, StatusCodes.Status200OK, "");
+                var roles = await _signInManager.UserManager.GetRolesAsync(user);
+                var token = tokenService.GenerateToken(user.Id, roles?.ToArray() ?? Array.Empty<string>());
+
+                return new AccountResult(user.UserName, token, StatusCodes.Status200OK, "");
             }
 
-            return new AccountResult("", StatusCodes.Status400BadRequest, "Incorrect username or password");
+            return new AccountResult("", "", StatusCodes.Status400BadRequest, "Incorrect username or password");
         }
     }
 }
