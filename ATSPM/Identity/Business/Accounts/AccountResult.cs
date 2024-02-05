7277e574126a12e838e6f43505c8fbcd512ce8fc
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-02-05 12:43:04 -0700

    Initial Identity changes

--- ATSPM/Identity/Business/Accounts/AccountResult.cs
+++ ATSPM/Identity/Business/Accounts/AccountResult.cs
@@ -2,15 +2,16 @@
 {
     public class AccountResult
     {
-        public AccountResult(string username, int code, string? error)
+        public AccountResult(string username, int code, string token, string? error)
         {
             Username = username;
             Code = code;
+            Token = token;
             Error = error;
         }
         public string Username { get; set; }
         //public List<string> Roles { get; set; }
-        //public string Token { get; set; }
+        public string Token { get; set; }
         public int Code { get; }
         public string? Error { get; }
     }
