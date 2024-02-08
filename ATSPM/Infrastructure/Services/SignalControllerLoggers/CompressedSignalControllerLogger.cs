60d14379b128aa8bc782fa006c8c61d0a6da54f2
Christian Baker <christianbaker@utah.gov>
2024-02-08 08:39:21 -0700

    Testing logging to new compressed events table

--- ATSPM/Infrastructure/Services/SignalControllerLoggers/CompressedSignalControllerLogger.cs
+++ ATSPM/Infrastructure/Services/SignalControllerLoggers/CompressedSignalControllerLogger.cs
@@ -94,7 +94,7 @@ namespace ATSPM.Infrastructure.Services.LocationControllerLoggers
             using (var scope = _serviceProvider.CreateScope())
             {
                 var decoder = scope.ServiceProvider.GetServices<ILocationControllerDecoder>().First(c => c.CanExecute(file));
-                logList = await decoder.ExecuteAsync(file, cancellationToken);
+                //logList = await decoder.ExecuteAsync(file, cancellationToken);
             }
 
             return logList;
