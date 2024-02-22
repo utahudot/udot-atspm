0c1ba9ac6bfad446642c2e95af97c903e877f9dc
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-02-21 17:51:57 -0700

    Moving the business folders

---
+++ ATSPM/ApplicationCore/Business/Watchdog/WatchDogResult.cs
@@ -0,0 +1,11 @@
+﻿using ATSPM.ReportApi.Business.Common;
+using System.Collections.Generic;
+
+namespace ATSPM.ReportApi.Business.Watchdog
+{
+    public class WatchDogResult : BaseResult
+    {
+        public List<WatchDogLogEventDTO> LogEvents { get; set; }
+
+    }
+}
