4e3f09da51070014b3f5d95f715a9895edbc2e7d
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-02-22 13:22:12 -0700

    merge fix

--- ATSPM/Infrastructure/Extensions/ServiceExtensions.cs
+++ ATSPM/Infrastructure/Extensions/ServiceExtensions.cs
@@ -1,4 +1,23 @@
-﻿using System.Security.Claims;
+﻿using ATSPM.Application.Configuration;
+using ATSPM.Application.Repositories;
+using ATSPM.Data;
+using ATSPM.Infrastructure.MySqlDatabaseProvider;
+using ATSPM.Infrastructure.OracleDatabaseProvider;
+using ATSPM.Infrastructure.PostgreSQLDatabaseProvider;
+using ATSPM.Infrastructure.Repositories;
+using ATSPM.Infrastructure.Services.ControllerDecoders;
+using ATSPM.Infrastructure.Services.ControllerDownloaders;
+using ATSPM.Infrastructure.SqlDatabaseProvider;
+using ATSPM.Infrastructure.SqlLiteDatabaseProvider;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
 
 namespace ATSPM.Infrastructure.Extensions
 {
@@ -70,64 +89,169 @@ namespace ATSPM.Infrastructure.Extensions
         {
             services.AddDbContext<IdentityContext>(db => db.GetDbProviderInfo<IdentityContext>(host).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking).EnableSensitiveDataLogging(host.HostingEnvironment.IsDevelopment()));
 
-            options.AddPolicy("CanViewWatchDog", policy =>
-               policy.RequireAssertion(context =>
-                   context.User.HasClaim(c =>
-                       (c.Type == ClaimTypes.Role && c.Value == "Watchdog:View") ||
-                       (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
-        });
+            return services;
+        }
+        public static IServiceCollection AddAtspmAuthentication(this IServiceCollection services, HostBuilderContext host, WebApplicationBuilder builder)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+           .AddJwtBearer(options =>
+           {
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateLifetime = true,
+                   ValidateIssuerSigningKey = true,
+                   ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                   ValidAudience = builder.Configuration["Jwt:Audience"],
+                   IssuerSigningKey = new SymmetricSecurityKey(
+                       Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+               };
+           });
+            return services;
+        }
+        public static IServiceCollection AddAtspmAuthorization(this IServiceCollection services, HostBuilderContext host)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("CanViewUsers", policy =>
+                    policy.RequireAssertion(context =>
+                        context.User.HasClaim(c =>
+                            (c.Type == ClaimTypes.Role && c.Value == "User:View") ||
+                            (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+                options.AddPolicy("CanEditUsers", policy =>
+                    policy.RequireAssertion(context =>
+                        context.User.HasClaim(c =>
+                            (c.Type == ClaimTypes.Role && c.Value == "User:Edit") ||
+                            (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+                options.AddPolicy("CanDeleteUsers", policy =>
+                    policy.RequireAssertion(context =>
+                        context.User.HasClaim(c =>
+                            (c.Type == ClaimTypes.Role && c.Value == "User:Delete") ||
+                            (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+
+
+                options.AddPolicy("CanViewRoles", policy =>
+                   policy.RequireAssertion(context =>
+                       context.User.HasClaim(c =>
+                           (c.Type == ClaimTypes.Role && c.Value == "Role:View") ||
+                           (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+                options.AddPolicy("CanEditRoles", policy =>
+                   policy.RequireAssertion(context =>
+                       context.User.HasClaim(c =>
+                           (c.Type == ClaimTypes.Role && c.Value == "Role:Edit") ||
+                           (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+                options.AddPolicy("CanDeleteRoles", policy =>
+                   policy.RequireAssertion(context =>
+                       context.User.HasClaim(c =>
+                           (c.Type == ClaimTypes.Role && c.Value == "Role:Delete") ||
+                           (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+
+
+                options.AddPolicy("CanViewLocationConfigurations", policy =>
+                   policy.RequireAssertion(context =>
+                       context.User.HasClaim(c =>
+                           (c.Type == ClaimTypes.Role && c.Value == "LocationConfiguration:View") ||
+                           (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+                options.AddPolicy("CanEditLocationConfigurations", policy =>
+                   policy.RequireAssertion(context =>
+                       context.User.HasClaim(c =>
+                           (c.Type == ClaimTypes.Role && c.Value == "LocationConfiguration:Edit") ||
+                           (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+                options.AddPolicy("CanDeleteLocationConfigurations", policy =>
+                   policy.RequireAssertion(context =>
+                       context.User.HasClaim(c =>
+                           (c.Type == ClaimTypes.Role && c.Value == "LocationConfiguration:Delete") ||
+                           (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+
+
+                options.AddPolicy("CanViewGeneralConfigurations", policy =>
+                   policy.RequireAssertion(context =>
+                       context.User.HasClaim(c =>
+                           (c.Type == ClaimTypes.Role && c.Value == "GeneralConfiguration:View") ||
+                           (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+                options.AddPolicy("CanEditGeneralConfigurations", policy =>
+                   policy.RequireAssertion(context =>
+                       context.User.HasClaim(c =>
+                           (c.Type == ClaimTypes.Role && c.Value == "GeneralConfiguration:Edit") ||
+                           (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+                options.AddPolicy("CanDeleteGeneralConfigurations", policy =>
+                   policy.RequireAssertion(context =>
+                       context.User.HasClaim(c =>
+                           (c.Type == ClaimTypes.Role && c.Value == "GeneralConfiguration:Delete") ||
+                           (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+
+
+                options.AddPolicy("CanViewData", policy =>
+                   policy.RequireAssertion(context =>
+                       context.User.HasClaim(c =>
+                           (c.Type == ClaimTypes.Role && c.Value == "Data:View") ||
+                           (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+                options.AddPolicy("CanEditData", policy =>
+                   policy.RequireAssertion(context =>
+                       context.User.HasClaim(c =>
+                           (c.Type == ClaimTypes.Role && c.Value == "Data:Edit") ||
+                           (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+
+
+                options.AddPolicy("CanViewWatchDog", policy =>
+                   policy.RequireAssertion(context =>
+                       context.User.HasClaim(c =>
+                           (c.Type == ClaimTypes.Role && c.Value == "Watchdog:View") ||
+                           (c.Type == ClaimTypes.Role && c.Value == "Admin"))));
+            });
             return services;
         }
 
-    public static IServiceCollection ConfigureLocationControllerDownloaders(this IServiceCollection services, HostBuilderContext host)
-    {
-        services.Configure<SignalControllerDownloaderConfiguration>(nameof(ASC3LocationControllerDownloader), host.Configuration.GetSection($"{nameof(SignalControllerDownloaderConfiguration)}:{nameof(ASC3LocationControllerDownloader)}"));
-        services.Configure<SignalControllerDownloaderConfiguration>(nameof(CobaltLocationControllerDownloader), host.Configuration.GetSection($"{nameof(SignalControllerDownloaderConfiguration)}:{nameof(CobaltLocationControllerDownloader)}"));
-        services.Configure<SignalControllerDownloaderConfiguration>(nameof(MaxTimeLocationControllerDownloader), host.Configuration.GetSection($"{nameof(SignalControllerDownloaderConfiguration)}:{nameof(MaxTimeLocationControllerDownloader)}"));
-        services.Configure<SignalControllerDownloaderConfiguration>(nameof(EOSLocationControllerDownloader), host.Configuration.GetSection($"{nameof(SignalControllerDownloaderConfiguration)}:{nameof(EOSLocationControllerDownloader)}"));
-        services.Configure<SignalControllerDownloaderConfiguration>(nameof(NewCobaltLocationControllerDownloader), host.Configuration.GetSection($"{nameof(SignalControllerDownloaderConfiguration)}:{nameof(NewCobaltLocationControllerDownloader)}"));
+        public static IServiceCollection ConfigureLocationControllerDownloaders(this IServiceCollection services, HostBuilderContext host)
+        {
+            services.Configure<SignalControllerDownloaderConfiguration>(nameof(ASC3LocationControllerDownloader), host.Configuration.GetSection($"{nameof(SignalControllerDownloaderConfiguration)}:{nameof(ASC3LocationControllerDownloader)}"));
+            services.Configure<SignalControllerDownloaderConfiguration>(nameof(CobaltLocationControllerDownloader), host.Configuration.GetSection($"{nameof(SignalControllerDownloaderConfiguration)}:{nameof(CobaltLocationControllerDownloader)}"));
+            services.Configure<SignalControllerDownloaderConfiguration>(nameof(MaxTimeLocationControllerDownloader), host.Configuration.GetSection($"{nameof(SignalControllerDownloaderConfiguration)}:{nameof(MaxTimeLocationControllerDownloader)}"));
+            services.Configure<SignalControllerDownloaderConfiguration>(nameof(EOSLocationControllerDownloader), host.Configuration.GetSection($"{nameof(SignalControllerDownloaderConfiguration)}:{nameof(EOSLocationControllerDownloader)}"));
+            services.Configure<SignalControllerDownloaderConfiguration>(nameof(NewCobaltLocationControllerDownloader), host.Configuration.GetSection($"{nameof(SignalControllerDownloaderConfiguration)}:{nameof(NewCobaltLocationControllerDownloader)}"));
 
-        return services;
+            return services;
+        }
+
+        public static IServiceCollection ConfigureLocationControllerDecoders(this IServiceCollection services, HostBuilderContext host)
+        {
+            services.Configure<SignalControllerDecoderConfiguration>(nameof(ASCLocationControllerDecoder), host.Configuration.GetSection($"{nameof(SignalControllerDecoderConfiguration)}:{nameof(ASCLocationControllerDecoder)}"));
+            services.Configure<SignalControllerDecoderConfiguration>(nameof(MaxTimeLocationControllerDecoder), host.Configuration.GetSection($"{nameof(SignalControllerDecoderConfiguration)}:{nameof(MaxTimeLocationControllerDecoder)}"));
+
+            return services;
+        }
+
+        public static IServiceCollection AddAtspmEFRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IApproachRepository, ApproachEFRepository>();
+            services.AddScoped<ISpeedEventRepository, SpeedEventEFRepository>();
+            services.AddScoped<IAreaRepository, AreaEFRepository>();
+            services.AddScoped<IDetectionTypeRepository, DetectionTypeEFRepository>();
+            services.AddScoped<IDetectorCommentRepository, DetectorCommentEFRepository>();
+            services.AddScoped<IDetectorRepository, DetectorEFRepository>();
+            services.AddScoped<IDeviceRepository, DeviceEFRepository>();
+            services.AddScoped<IDeviceConfigurationRepository, DeviceConfigurationEFRepository>();
+            services.AddScoped<IDirectionTypeRepository, DirectionTypeEFRepository>();
+            services.AddScoped<IExternalLinksRepository, ExternalLinsEFRepository>();
+            services.AddScoped<IFaqRepository, FaqEFRepository>();
+            services.AddScoped<IJurisdictionRepository, JurisdictionEFRepository>();
+            services.AddScoped<ILocationRepository, LocationEFRepository>();
+            services.AddScoped<ILocationTypeRepository, LocationTypeEFRepository>();
+            services.AddScoped<IMeasureCommentRepository, MeasureCommentEFRepository>();
+            services.AddScoped<IMeasureOptionsRepository, MeasureOptionsEFRepository>();
+            services.AddScoped<IMeasureTypeRepository, MeasureTypeEFRepository>();
+            services.AddScoped<IMenuItemReposiotry, MenuItemEFRepository>();
+            services.AddScoped<IProductRepository, ProductEFRepository>();
+            services.AddScoped<IRegionsRepository, RegionEFRepository>();
+            services.AddScoped<IRouteRepository, RouteEFRepository>();
+            services.AddScoped<IRouteDistanceRepository, RouteDistanceEFRepository>();
+            services.AddScoped<IRouteLocationsRepository, RouteLocationEFRepository>();
+            services.AddScoped<ISettingsRepository, SettingsEFRepository>();
+            services.AddScoped<IVersionHistoryRepository, VersionHistoryEFRepository>();
+            services.AddScoped<IWatchDogLogEventRepository, WatchDogLogEventEFRepository>();
+
+            return services;
+        }
     }
-
-    public static IServiceCollection ConfigureLocationControllerDecoders(this IServiceCollection services, HostBuilderContext host)
-    {
-        services.Configure<SignalControllerDecoderConfiguration>(nameof(ASCLocationControllerDecoder), host.Configuration.GetSection($"{nameof(SignalControllerDecoderConfiguration)}:{nameof(ASCLocationControllerDecoder)}"));
-        services.Configure<SignalControllerDecoderConfiguration>(nameof(MaxTimeLocationControllerDecoder), host.Configuration.GetSection($"{nameof(SignalControllerDecoderConfiguration)}:{nameof(MaxTimeLocationControllerDecoder)}"));
-
-        return services;
-    }
-
-    public static IServiceCollection AddAtspmEFRepositories(this IServiceCollection services)
-    {
-        services.AddScoped<IApproachRepository, ApproachEFRepository>();
-        services.AddScoped<ISpeedEventRepository, SpeedEventEFRepository>();
-        services.AddScoped<IAreaRepository, AreaEFRepository>();
-        services.AddScoped<IDetectionTypeRepository, DetectionTypeEFRepository>();
-        services.AddScoped<IDetectorCommentRepository, DetectorCommentEFRepository>();
-        services.AddScoped<IDetectorRepository, DetectorEFRepository>();
-        services.AddScoped<IDeviceRepository, DeviceEFRepository>();
-        services.AddScoped<IDeviceConfigurationRepository, DeviceConfigurationEFRepository>();
-        services.AddScoped<IDirectionTypeRepository, DirectionTypeEFRepository>();
-        services.AddScoped<IExternalLinksRepository, ExternalLinsEFRepository>();
-        services.AddScoped<IFaqRepository, FaqEFRepository>();
-        services.AddScoped<IJurisdictionRepository, JurisdictionEFRepository>();
-        services.AddScoped<ILocationRepository, LocationEFRepository>();
-        services.AddScoped<ILocationTypeRepository, LocationTypeEFRepository>();
-        services.AddScoped<IMeasureCommentRepository, MeasureCommentEFRepository>();
-        services.AddScoped<IMeasureOptionsRepository, MeasureOptionsEFRepository>();
-        services.AddScoped<IMeasureTypeRepository, MeasureTypeEFRepository>();
-        services.AddScoped<IMenuItemReposiotry, MenuItemEFRepository>();
-        services.AddScoped<IProductRepository, ProductEFRepository>();
-        services.AddScoped<IRegionsRepository, RegionEFRepository>();
-        services.AddScoped<IRouteRepository, RouteEFRepository>();
-        services.AddScoped<IRouteDistanceRepository, RouteDistanceEFRepository>();
-        services.AddScoped<IRouteLocationsRepository, RouteLocationEFRepository>();
-        services.AddScoped<ISettingsRepository, SettingsEFRepository>();
-        services.AddScoped<IVersionHistoryRepository, VersionHistoryEFRepository>();
-        services.AddScoped<IWatchDogLogEventRepository, WatchDogLogEventEFRepository>();
-
-        return services;
-    }
-}
 }
