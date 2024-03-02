da5eb7dee06c7cf215f9c07f72241369c9b6f431
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-03-01 23:07:55 -0700

    Fixes for Watchdog

---
+++ ATSPM/WatchDog/Models/IMailService.cs
@@ -0,0 +1,7 @@
+﻿namespace WatchDog.Models
+{
+    public interface IMailService
+    {
+        Task<bool> SendEmailAsync(string from, List<ApplicationUser> users, string subject, string body);
+    }
+}
