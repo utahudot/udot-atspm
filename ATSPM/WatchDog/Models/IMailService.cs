fbd6c1b1170cdca513e180cc162e0e912f8e9ff3
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-03-04 19:28:44 -0700

    AVE-668 Deploy Watchdog

--- ATSPM/WatchDog/Models/IMailService.cs
+++ ATSPM/WatchDog/Models/IMailService.cs
@@ -2,6 +2,7 @@
 {
     public interface IMailService
     {
+        void ConnectAndAuthenticate();
         Task<bool> SendEmailAsync(string from, List<ApplicationUser> users, string subject, string body);
     }
 }
