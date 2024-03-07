1e490a71f6fa71d62b438b8687dd7e356967eda9
Christian Baker <christianbaker@utah.gov>
2024-03-07 10:04:04 -0700

    Changed Device.Ipaddress to string

---
+++ ATSPM/ConfigApi/Utility/IpAddressJsonConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+#nullable disable
+
+namespace ATSPM.ConfigApi.Utility
+{
+    /// <summary>
+    /// Json converter for <see cref="IPAddress"/>
+    /// </summary>
+    public class IpAddressJsonConverter : JsonConverter<IPAddress>
+    {
+        /// <inheritdoc/>
+        public override IPAddress Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return IPAddress.Parse(reader.GetString());
+        }
+
+        /// <inheritdoc/>
+        public override void Write(Utf8JsonWriter writer, IPAddress value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
