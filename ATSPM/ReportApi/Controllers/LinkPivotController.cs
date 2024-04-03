be888dd3dec02152cbb616ddf27156278dd13789
avetraffic <96312362+avetraffic@users.noreply.github.com>
2024-04-03 14:18:04 -0600

    Ave 25 link pivot (#48)
    * Initial Import of Link Pivot  logic
    
    * Additional conversions from old site
    
    * Working on Link Pivot
    
    * Link Pivot Works for pcd link pivot
    
    * Fix up values for link pivot
    
    * Fixing Link Pivot calls
    
    * Final Fixes to link Pivot
    
    * Add link pivot to startup
    
    * Spelling error
    
    * Delete ATSPM/ReportAPI directory
    
    remove unwanted folder
    
    ---------
    
    Co-authored-by: dloweAvenueConsultants <dlowe@avenueconsultants.com>
    Co-authored-by: Jaithra <jbhatia@avenueconsultants.com>

---
+++ ATSPM/ReportApi/Controllers/LinkPivotController.cs
@@ -0,0 +1,41 @@
+﻿using Asp.Versioning;
+using ATSPM.Application.Business;
+using ATSPM.Application.Business.LinkPivot;
+using ATSPM.ReportApi.ReportServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ATSPM.ReportApi.Controllers
+{
+    /// <summary>
+    /// Left turn gap analysis report controller
+    /// </summary>
+    [ApiVersion(1.0)]
+    [ApiController]
+    [Route("v{version:apiVersion}/[controller]")]
+    public class LinkPivotController : ReportControllerBase<LinkPivotOptions, LinkPivotResult>
+    {
+        private readonly LinkPivotReportService linkPivotReportService;
+        /// <inheritdoc/>
+        public LinkPivotController(IReportService<LinkPivotOptions, LinkPivotResult> reportService, LinkPivotReportService linkPivotReportService) : base(reportService) {
+            this.linkPivotReportService = linkPivotReportService;
+        }
+
+        [HttpPost("getPcdData")]
+        //[Produces("application/json", "application/xml")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<LinkPivotPcdResult>> GetPcdData(LinkPivotPcdOptions options)
+        {
+            try
+            {
+                var result = await linkPivotReportService.GetPcdData(options);
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
