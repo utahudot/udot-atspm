e48dc3701901750f881939c7346e1fa2e302cf01
avetraffic <96312362+avetraffic@users.noreply.github.com>
2024-04-03 14:18:04 -0600

    Ave 25 link pivot (#48)
    * Initial Import of Link Pivot  logic
    
    * Additional conversions from old site
    
    * Working on Link Pivot
    
    * Link Pivot Works for pcd link pivot
    
    * Fix up values for link pivot
    
    * Fixing Link Pivot calls
    
    * Final Fixes to link Pivot
    
    * Add link pivot to startup
    
    * Spelling error
    
    * Delete ATSPM/ReportAPI directory
    
    remove unwanted folder
    
    ---------
    
    Co-authored-by: dloweAvenueConsultants <dlowe@avenueconsultants.com>
    Co-authored-by: Jaithra <jbhatia@avenueconsultants.com>

---
+++ ATSPM/ApplicationCore/Business/LinkPivot/LinkPivot.cs
@@ -0,0 +1,16 @@
+﻿using ATSPM.Application.Business.Common;
+using System;
+using System.Collections.Generic;
+
+namespace ATSPM.Application.Business.LinkPivot
+{
+    public class LinkPivot : BaseResult
+    {
+        public LinkPivot(DateTime start, DateTime end) : base(start, end) {
+        }
+        
+        public List<AdjustmentObject> Adjustments { get; set; }
+
+        public List<LinkPivotPair> PairedApproaches { get; set; } = new List<LinkPivotPair>();
+    }
+}
---
+++ ATSPM/ApplicationCore/Business/LinkPivot/LinkPivotPcdResult.cs
@@ -0,0 +1,22 @@
+﻿using ATSPM.Application.Business.PurdueCoordinationDiagram;
+using System.Collections.Generic;
+
+namespace ATSPM.Application.Business.LinkPivot
+{
+    public class LinkPivotPcdResult
+    {
+        public LinkPivotPcdResult()
+        {
+            pcdExisting = new List<PurdueCoordinationDiagramResult>();
+            pcdPredicted = new List<PurdueCoordinationDiagramResult>();
+        }
+        public double ExistingTotalAOG { get; set; }
+        public double ExistingTotalPAOG { get; set; } = 0;
+        public double PredictedTotalAOG { get; set; }
+        public double PredictedTotalPAOG { get; set; } = 0;
+        public double PredictedVolume { get; set; }
+        public double ExistingVolume { get; set; }
+        public List<PurdueCoordinationDiagramResult> pcdExisting { get; set; }
+        public List<PurdueCoordinationDiagramResult> pcdPredicted { get; set; }
+    }
+}
