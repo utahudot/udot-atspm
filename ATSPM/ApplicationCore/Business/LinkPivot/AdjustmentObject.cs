42f2fbd237bd99db653052a0bd89299c396f8281
avetraffic <96312362+avetraffic@users.noreply.github.com>
2024-04-03 14:18:04 -0600

    Ave 25 link pivot (#48)
    * Initial Import of Link Pivot  logic
    
    * Additional conversions from old site
    
    * Working on Link Pivot
    
    * Link Pivot Works for pcd link pivot
    
    * Fix up values for link pivot
    
    * Fixing Link Pivot calls
    
    * Final Fixes to link Pivot
    
    * Add link pivot to startup
    
    * Spelling error
    
    * Delete ATSPM/ReportAPI directory
    
    remove unwanted folder
    
    ---------
    
    Co-authored-by: dloweAvenueConsultants <dlowe@avenueconsultants.com>
    Co-authored-by: Jaithra <jbhatia@avenueconsultants.com>

---
+++ ATSPM/ApplicationCore/Business/LinkPivot/AdjustmentObject.cs
@@ -0,0 +1,35 @@
+﻿using ATSPM.Data.Enums;
+using ATSPM.Data.Models;
+
+namespace ATSPM.Application.Business.LinkPivot
+{
+    public class AdjustmentObject
+    {
+        public double DownstreamVolume { get; set; }
+        public double UpstreamVolume { get; set; }
+        public int LinkNumber { get; set; }
+        public double AogTotalBefore { get; set; }
+        public int PAogTotalBefore { get; set; }
+        public double AogTotalPredicted { get; set; }
+        public int PAogTotalPredicted { get; set; }
+        public string ResultChartLocation { get; set; }
+        public string DownstreamLocation { get; set; }
+        public string DownLocationIdentifier { get; set; }
+        public string DownstreamApproachDirection { get; set; }
+        public string UpstreamApproachDirection { get; set; }
+        public double AOGDownstreamPredicted { get; set; }
+        public double AOGUpstreamPredicted { get; set; }
+        public double AOGDownstreamBefore { get; set; }
+        public double AOGUpstreamBefore { get; set; }
+        public int PAOGDownstreamPredicted { get; set; }
+        public int PAOGUpstreamPredicted { get; set; }
+        public int PAOGDownstreamBefore { get; set; }
+        public int PAOGUpstreamBefore { get; set; }
+        public int Adjustment { get; set; }
+        public int Delta { get; set; }
+        public string LocationIdentifier { get; set; }
+        public string DownstreamLocationIdentifier { get; set; }
+        public string Location { get; set; }
+
+    }
+}
