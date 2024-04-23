b0a05182d2467aaa89df49dfe2c7d01eb0e791b4
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-04-23 09:06:48 -0600

    Adding Left Turn Gap Report main service

---
+++ ATSPM/ApplicationCore/Enums/PhaseType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace ATSPM.Application.Enums
+{
+    public enum PhaseType
+    {
+        [Description("Protected Only")]
+        ProtectedOnly,
+        [Description("Permissive Only")]
+        PermissiveOnly,
+        [Description("Protected/Permissive")]
+        ProtectedPermissive
+    }
+}
