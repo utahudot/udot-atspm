ef34abf4f3d4401dab4783abd3e6f92badc4e8df
Christian Baker <christianbaker@utah.gov>
2024-05-01 10:22:34 -0600

    Done testing UpdateCollections ovveride

---
+++ ATSPM/ApplicationCore/Common/EqualityComparers/ConfigEntityIdComparer.cs
@@ -0,0 +1,20 @@
+﻿using ATSPM.Data.Models.ConfigurationModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ATSPM.Application.Common.EqualityComparers
+{
+    public class ConfigEntityIdComparer<T, Tid> : EqualityComparer<T> where T : AtspmConfigModelBase<Tid>
+    {
+        public override bool Equals(T x, T y)
+        {
+            return x.Id.Equals(y.Id);
+        }
+
+        public override int GetHashCode([DisallowNull] T obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
