f2462b293270250f34bc62fc1fa89c9f4d3a244f
Christian Baker <christianbaker@utah.gov>
2024-05-08 13:24:23 -0600

    Created SmtpEmailService

--- ATSPM/DomainCore/Configuration/EmailConfiguration.cs
+++ ATSPM/DomainCore/Configuration/EmailConfiguration.cs
@@ -14,6 +14,5 @@ namespace ATSPM.Domain.Configuration
         public string UserName { get; set; }
         public string Password { get; set; }
         public bool EnableSsl { get; set; }
-        public string Key { get; set; }
     }
 }
