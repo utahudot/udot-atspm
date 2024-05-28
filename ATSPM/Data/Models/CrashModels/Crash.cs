be08e64f7053066af7e586d281046acfd26b7484
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-05-28 11:33:08 -0600

    Initial configuration

---
+++ ATSPM/Data/Models/CrashModels/Crash.cs
@@ -0,0 +1,36 @@
+﻿namespace ATSPM.Data.Models.CrashModels;
+
+public partial class Crash
+{
+    public int Id { get; set; }
+
+    public int Severity { get; set; }
+
+    public int PersonId { get; set; }
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+
+    public DateTime Timestamp { get; set; }
+
+    public int Speedinvolved { get; set; }
+
+    public int NonmotorActionId { get; set; }
+
+    public int NonmotorContribCirId { get; set; }
+
+    public int? ClearguideRouteId { get; set; }
+
+    public int? Avgspd15 { get; set; }
+
+    public int? Percentilespd8515 { get; set; }
+
+    public int? Avgspd30 { get; set; }
+
+    public int? Percentilespd8530 { get; set; }
+
+    public int? AvgspdHour { get; set; }
+
+    public int? Percentilespd85Hour { get; set; }
+}
---
+++ ATSPM/Data/Models/SpeedManagementConfigModels/Route.cs
@@ -0,0 +1,23 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace ATSPM.Data.Models.SpeedManagementConfigModels
+{
+    public class Route
+    {
+        public int Id { get; set; }
+        public int UdotRouteNumber { get; set; }
+        public double StartMilePoint { get; set; }
+        public double EndMilePoint { get; set; }
+        public string FunctionalType { get; set; }
+        public string Name { get; set; }
+        public string Direction { get; set; }
+        public int SpeedLimit { get; set; }
+        public string Region { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public Geometry Shape { get; set; }
+        public string? AlternateIdentifier { get; set; }
+        public virtual List<RouteEntity> RouteEntities { get; set; }
+
+    }
+}
