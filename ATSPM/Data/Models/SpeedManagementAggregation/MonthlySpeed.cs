71df18beb94af412407a5f8e8be1dbc6e918407c
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-05-28 11:33:08 -0600

    Initial configuration

---
+++ ATSPM/Data/Models/SpeedManagementAggregation/MonthlySpeed.cs
@@ -0,0 +1,18 @@
+﻿namespace ATSPM.Data.Models.SpeedConfigModels;
+
+public class MonthlySpeed
+{
+    public DateOnly Date { get; set; }
+    public TimeOnly BinStartTime { get; set; }
+    public int RouteId { get; set; }
+    public int SourceId { get; set; }
+    public int ConfidenceId { get; set; }
+    public int Average { get; set; }
+    public int? FifteenthSpeed { get; set; }
+    public int? EightyFifthSpeed { get; set; }
+    public int? NinetyFifthSpeed { get; set; }
+    public int? NinetyNinthSpeed { get; set; }
+    public int? Violation { get; set; }
+    public int? Flow { get; set; }
+
+}
---
+++ ATSPM/Data/Models/SpeedManagementConfigModels/RouteEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace ATSPM.Data.Models.SpeedManagementConfigModels
+{
+    public class RouteEntity
+    {
+        public int EntityId { get; set; }
+        public int RouteId { get; set; }
+        public Route Route { get; set; }
+    }
+}
+
