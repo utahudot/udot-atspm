940bbfaa0e0c6e42fcfecc9c35dd0ebe6cacaea1
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-06-01 20:34:10 -0600

    Base Repository big query

---
+++ ATSPM/ApplicationCore/Repositories/SpeedManagementAggregationRepositories/IMonthlySpeedRepository.cs
@@ -0,0 +1,18 @@
+﻿using ATSPM.Data.Models.SpeedConfigModels;
+using ATSPM.Domain.Services;
+
+namespace ATSPM.Application.Repositories.SpeedManagementAggregationRepositories
+{
+    public interface IMonthlySpeedRepository : IAsyncRepository<MonthlySpeed>
+    {
+        #region ExtensionMethods
+
+
+        #endregion
+
+        #region Obsolete
+
+
+        #endregion
+    }
+}
---
+++ ATSPM/Infrastructure/Repositories/SpeedManagementAggregationRepositories/HourlySpeedBQRepository.cs
@@ -0,0 +1,64 @@
+﻿using ATSPM.Application.Repositories.SpeedManagementAggregationRepositories;
+using ATSPM.Data;
+using ATSPM.Data.Models.SpeedManagementAggregation;
+using ATSPM.Domain.Extensions;
+using Google.Cloud.BigQuery.V2;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ATSPM.Infrastructure.Repositories.ConfigurationRepositories
+{
+    ///<inheritdoc cref="IHourlySpeedRepository"/>
+    public class HourlySpeedBQRepository : ATSPMRepositoryBQBase<HourlySpeed>, IHourlySpeedRepository
+    {
+        /// <inheritdoc/>
+        public HourlySpeedBQRepository(ConfigContext db, ILogger<HourlySpeedBQRepository> log) : base(db, log) { }
+
+        #region Overrides
+        protected override BigQueryInsertRow CreateRow(HourlySpeed item)
+        {
+            return new BigQueryInsertRow
+            {
+                { "Date", item.Date },
+                {"BinStartTime", item.BinStartTime },
+                {"RouteId", item.RouteId },
+                {"SourceId", item.SourceId },
+                {"ConfidenceId", item.ConfidenceId },
+                {"Average", item.Average },
+                {"FifteenthSpeed", item.FifteenthSpeed },
+                {"EightyFifthSpeed", item.EightyFifthSpeed },
+                {"NinetyFifthSpeed", item.NinetyFifthSpeed },
+                {"NinetyNinthSpeed", item.NinetyNinthSpeed },
+                {"Violation", item.Violation },
+                {"Flow", item.Flow }
+            };
+        }
+
+        protected override HourlySpeed MapRowToEntity(BigQueryRow row)
+        {
+            return new HourlySpeed
+            {
+                Date = DateOnly.FromDateTime(row.GetPropertyValue<DateTime>("Id")),
+                BinStartTime = TimeOnly.FromDateTime(row.GetPropertyValue<DateTime>("BinStartTime")),
+                RouteId = row.GetPropertyValue<int>("RouteId"),
+                SourceId = row.GetPropertyValue<int>("SourceId"),
+                ConfidenceId = row.GetPropertyValue<int>("ConfidenceId"),
+                Average = row.GetPropertyValue<int>("Average"),
+                FifteenthSpeed = row.GetPropertyValue<int?>("FifteenthSpeed"),
+                EightyFifthSpeed = row.GetPropertyValue<int?>("EightyFifthSpeed"),
+                NinetyFifthSpeed = row.GetPropertyValue<int?>("NinetyFifthSpeed"),
+                NinetyNinthSpeed = row.GetPropertyValue<int?>("NinetyNinthSpeed"),
+                Violation = row.GetPropertyValue<int?>("Violation"),
+                Flow = row.GetPropertyValue<int?>("Flow")
+            };
+        }
+
+
+
+        #endregion
+
+        #region IApproachRepository
+
+        #endregion
+    }
+}
