3ea1a81349cf7f160f3f1f5d5df30dba9de404d7
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-06-25 09:33:33 -0600

    SonarQube recommended changes for GreenTimeUtilization

--- ATSPM/ApplicationCore/Business/GreenTimeUtilization/GreenTimeUtilizationService.cs
+++ ATSPM/ApplicationCore/Business/GreenTimeUtilization/GreenTimeUtilizationService.cs
@@ -10,9 +10,6 @@ namespace ATSPM.Application.Business.GreenTimeUtilization
     public class GreenTimeUtilizationService
     {
         //define ECs
-        private const int PHASE_BEGIN_GREEN = 1;
-        private const int PHASE_BEGIN_YELLOW = 8;
-        private const int PHASE_END_RED_CLEAR = 11;
         private readonly PlanService planService;
 
         public GreenTimeUtilizationService(
@@ -30,50 +27,15 @@ namespace ATSPM.Application.Business.GreenTimeUtilization
             List<IndianaEvent> controllerEventLogs
             ) // the plans/splits input is still TBD
         {
-            //var Location = LocationRepository.GetLatestVersionOfLocation(options.LocationIdentifier, options.Start);
-            //var controllerEventLogs = controllerEventLogRepository.GetEventsBetweenDates(Location.LocationIdentifier, options.Start.AddHours(-12), options.End.AddHours(12)).ToList();
-
             var isPermissivePhase = phaseDetail.PhaseNumber != phaseDetail.Approach.ProtectedPhaseNumber;
-            //define properties
-            string phaseNumberSort;
-            if (phaseDetail.Approach.PermissivePhaseNumber.HasValue)
-            {
-                phaseNumberSort = phaseDetail.PhaseNumber == phaseDetail.Approach.PermissivePhaseNumber.Value ?
-                   phaseDetail.Approach.PermissivePhaseNumber.Value.ToString() + "-1" : phaseDetail.Approach.ProtectedPhaseNumber.ToString() + "-2";
-            }
-            else
-            {
-                phaseNumberSort = phaseDetail.Approach.ProtectedPhaseNumber.ToString() + "-2";
-            }
-
-            string phaseNumberDescription;
-            if (phaseDetail.UseOverlap)
-            {
-                phaseNumberDescription = "Overlap " + phaseDetail.PhaseNumber + ": " + phaseDetail.Approach.Description;
-            }
-            else
-            {
-                phaseNumberDescription = "Phase " + phaseDetail.PhaseNumber + ": " + phaseDetail.Approach.Description;
-            }
 
             //get a list of cycle events
             var phaseEventNumbers = new List<short> { 1, 8 };
-            //var phaseEvents = controllerEventLogs.GetCycleEventsWithTimeExtension(approach, options.UsePermissivePhase, options.Start, options.End)
             var checkAgainstEvents = new List<IndianaEvent>();
-            if (isPermissivePhase == true && phaseDetail.PhaseNumber != 0)   // if it's a permissive phase, it will need to be checked against the protected green/yellow events
+            if (isPermissivePhase && phaseDetail.PhaseNumber != 0)   // if it's a permissive phase, it will need to be checked against the protected timeStamp/yellow events
             {
                 checkAgainstEvents = controllerEventLogs.GetEventsByEventCodes(options.Start, options.End.AddMinutes(options.XAxisBinSize), phaseEventNumbers, phaseDetail.PhaseNumber).ToList();
             }
-
-            //get a list of detections for that phase
-            //var detectorsToUse = approach.GetAllDetectorsOfDetectionType(4);  //should this really be approach-based and not phase-based? - I think so because of getpermissivephase
-            //var allDetectionEvents = cel.GetLocationEventsByEventCode(options.LocationIdentifier, options.Start, options.End.AddMinutes(options.SelectedAggSize), DETECTOR_ON);
-            //var detectionEvents = new List<IndianaEvent>();
-            //foreach (var detector in detectorsToUse)
-            //{
-            //    detectionEvents.AddRange(allDetectionEvents.Where(x =>
-            //        x.EventCode == DETECTOR_ON && x.EventParam == detector.DetectorChannel));
-            //}
 
             var bins = new List<BarStack>();
             var averageSplits = new List<DataPointForDouble>();
@@ -86,17 +48,17 @@ namespace ATSPM.Application.Business.GreenTimeUtilization
                 List<int> BinValueList = new List<int>(new int[99]);
                 int cycleCount = 0;
 
-                //determine timestamps of the first green and last yellow
+                //determine timestamps of the first timeStamp and last yellow
                 var firstGreen = cycleEvents.Where(x => x.Timestamp > StartBinTime && x.EventCode == 1).OrderBy(x => x.Timestamp).FirstOrDefault();
                 if (firstGreen is null || firstGreen.Timestamp > endAggTime)
                 {
-                    continue; //skip this agg and go to the next if there is no green at all or if there isw no green in the agg period
+                    continue; //skip this agg and go to the next if there is no timeStamp at all or if there isw no timeStamp in the agg period
                 }
                 var lastGreen = cycleEvents.Where(x => x.Timestamp < endAggTime && x.EventCode == 1).OrderByDescending(x => x.Timestamp).FirstOrDefault();
                 var lastYellow = cycleEvents.Where(x => x.Timestamp > lastGreen.Timestamp && x.EventCode == 8).OrderBy(x => x.Timestamp).FirstOrDefault();
                 if (lastGreen is null || lastYellow is null)
                 {
-                    continue; //skip this agg and go to the next if there is no green at all or if there isw no green in the agg period
+                    continue; //skip this agg and go to the next if there is no timeStamp at all or if there isw no timeStamp in the agg period
                 }
 
                 //get the event lists for the agg bin
@@ -119,38 +81,38 @@ namespace ATSPM.Application.Business.GreenTimeUtilization
                 {
                     var yProtectedEvents = checkAgainstEvents.Where(x => x.EventCode == 8);
                     var gProtectedEvents = checkAgainstEvents.Where(x => x.EventCode == 1);
-                    greenList = (IOrderedEnumerable<IndianaEvent>)CheckProtectedGreens(greenList, yellowList, gProtectedEvents, yProtectedEvents); //redefine the greenList after editng the green values to start at the beginning of the protected yellow phases if there is overlapping green time between the protected nad permissive phases (only happens with doghouses)
+                    greenList = (IOrderedEnumerable<IndianaEvent>)CheckProtectedGreens(greenList, yellowList, gProtectedEvents, yProtectedEvents); //redefine the greenList after editng the timeStamp values to start at the beginning of the protected yellow phases if there is overlapping timeStamp time between the protected nad permissive phases (only happens with doghouses)
                 }
 
-                //pair each green with a yellow
-                foreach (var green in greenList)
+                //pair each timeStamp with a yellow
+                foreach (var timeStamp in greenList.Select(g => g.Timestamp))
                 {
                     //Find the corresponding yellow
-                    var yellow = yellowList.Where(x => x.Timestamp >= green.Timestamp).OrderBy(x => x.Timestamp)
+                    var yellow = yellowList.Where(x => x.Timestamp >= timeStamp).OrderBy(x => x.Timestamp)
                         .FirstOrDefault();
                     if (yellow == null)
                         continue;
 
-                    //get the green duration
-                    TimeSpan greenDuration = yellow.Timestamp - green.Timestamp;
+                    //get the timeStamp duration
+                    TimeSpan greenDuration = yellow.Timestamp - timeStamp;
                     greenDurationList.Add(greenDuration.TotalSeconds);  //if zero, maybe don't add it?  
 
                     //count the number of cycles
                     cycleCount++;
 
-                    //Find all events between the green and yellow
+                    //Find all events between the timeStamp and yellow
                     var greenDetectionsList = aggDetections
-                        .Where(x => x.Timestamp >= green.Timestamp && x.Timestamp <= yellow.Timestamp)
+                        .Where(x => x.Timestamp >= timeStamp && x.Timestamp <= yellow.Timestamp)
                         .OrderBy(x => x.Timestamp).ToList();
                     if (!greenDetectionsList.Any())
                         continue;
-                    //maybe add a catch here where you make sure the BinValue list has as many indexes as it would need to cover the whole green (and add more if not)
+                    //maybe add a catch here where you make sure the BinValue list has as many indexes as it would need to cover the whole timeStamp (and add more if not)
                     //****or maybe add a catch inside the foreach loop below that checks if the intended index exists - and add enough to make sure it does ****
 
-                    //add 1 to the bin value for each detection occuring during green
+                    //add 1 to the bin value for each detection occuring during timeStamp
                     foreach (var detection in greenDetectionsList)
                     {
-                        TimeSpan timeSinceGreenStart = detection.Timestamp - green.Timestamp;
+                        TimeSpan timeSinceGreenStart = detection.Timestamp - timeStamp;
                         var yAxisBinNumber = (int)(timeSinceGreenStart.TotalSeconds / options.YAxisBinSize);
                         if (BinValueList.Count < yAxisBinNumber)
                         {
@@ -175,7 +137,6 @@ namespace ATSPM.Application.Business.GreenTimeUtilization
             var programmedSplits = new List<ProgrammedSplit>();
             foreach (Plan analysisplan in plans)
             {
-                //GetProgrammedSplitTimesInAnalysisPeriod(approach.ProtectedPhaseNumber, analysisplan, options.End);
                 var splitTime = GetProgrammedSplitTime(phaseDetail.PhaseNumber, analysisplan.Start, analysisplan.End.AddMinutes(-1), controllerEventLogs);
                 programmedSplits.Add(new ProgrammedSplit(analysisplan, options.Start, splitTime, durYellowRed));
             }
@@ -198,7 +159,7 @@ namespace ATSPM.Application.Business.GreenTimeUtilization
             return result;
         }
 
-        private void createStack(List<BarStack> bins, List<int> binValueList, int cycleCount, int xAxisBinNumber)
+        private static void createStack(List<BarStack> bins, List<int> binValueList, int cycleCount, int xAxisBinNumber)
         {
             int maxI = 0;
             for (int i = 0; i < binValueList.Count; i++)
@@ -226,34 +187,13 @@ namespace ATSPM.Application.Business.GreenTimeUtilization
             if (eventCode == null)
                 return 0;
             return controllerEventLogs.GetEventsByEventCodes(startDate, endDate, new List<short> { eventCode.Value })
-                .OrderByDescending(e => e.Timestamp)
                 .Where(e => e.Timestamp <= startDate)
+                .OrderByDescending(e => e.Timestamp)
                 .Select(e => e.EventParam)
                 .FirstOrDefault();
         }
 
-
-
-        //void GetProgrammedSplitTimesInAnalysisPeriod(int phaseNumber, Plan analysisplan, DateTime analysisEnd)
-        //{
-        //    SPM db = new SPM();
-        //    var cel = ControllerEventLogRepositoryFactory.Create(db);
-        //    GetEventCodeForPhase(phaseNumber);
-        //    var tempSplitTimes = cel.GetLocationEventsByEventCode(locationId, analysisplan.StartTime, analysisEnd, splitLengthEventCode)
-        //        .OrderByDescending(e => e.Timestamp).ToList();
-        //    int i = 0;
-        //    for (i = 0; tempSplitTimes[i].Timestamp < analysisplan.StartTime; i++)
-        //    {
-        //        splitLength = tempSplitTimes[i].EventParam;
-        //        break;
-
-        //    }
-        //    i++;
-
-        //}
-
-
-        private short? GetEventCodeForPhase(int PhaseNumber)
+        private static short? GetEventCodeForPhase(int PhaseNumber)
         {
             switch (PhaseNumber)
             {
