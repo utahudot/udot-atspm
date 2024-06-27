cc0ea539f282ee1ff5ea0f90290aeca265cb377a
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-06-27 14:28:00 -0600

    Adding dto objects

---
+++ ATSPM/ConfigApi/Models/DetectorDto.cs
@@ -0,0 +1,23 @@
+﻿using ATSPM.Data.Enums;
+
+namespace ATSPM.ConfigApi.Models
+{
+    public class DetectorDto
+    {
+        public int Id { get; set; }
+        public string DectectorIdentifier { get; set; }
+        public int DetectorChannel { get; set; }
+        public int? DistanceFromStopBar { get; set; }
+        public int? MinSpeedFilter { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime? DateDisabled { get; set; }
+        public int? LaneNumber { get; set; }
+        public MovementTypes MovementType { get; set; }
+        public LaneTypes LaneType { get; set; }
+        public DetectionHardwareTypes DetectionHardware { get; set; }
+        public int? DecisionPoint { get; set; }
+        public int? MovementDelay { get; set; }
+        public double LatencyCorrection { get; set; }
+        public int ApproachId { get; set; }
+    }
+}
