fb91c0f1fe75b5fa58ea2aef83bb109ce6172295
dloweAvenueConsultants <92349445+dloweAvenueConsultants@users.noreply.github.com>
2024-06-27 15:29:12 -0600

    Ave 1425 route editor bug (#59)
    * initial
    
    * gitignore update
    
    * Adding route  create
    
    * add delete functionality
    
    * my changes to route bug
    
    * change the create to return the object.
    
    * Route Get conflict
    
    * Adding dto objects
    
    * add locationId field
    
    * merge with staging
    
    ---------
    
    Co-authored-by: Morgan Stanley <mstanley@avenueconsultants.com>
    Co-authored-by: Jaithra <jbhatia@avenueconsultants.com>

---
+++ ATSPM/ConfigApi/Services/IRouteService.cs
@@ -0,0 +1,11 @@
+﻿using ATSPM.ConfigApi.Models;
+
+namespace ATSPM.ConfigApi.Services
+{
+    public interface IRouteService
+    {
+        RouteDto UpsertRoute(RouteDto route);
+
+        RouteDto GetRouteWithExpandedLocations(int routeId, bool includeLocationDetail);
+    }
+}
