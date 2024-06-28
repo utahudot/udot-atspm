c08680a0befc26c21d8edf8c9cd9108a977698f9
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-06-28 12:58:46 -0600

    Adding interface

---
+++ ATSPM/ConfigApi/Services/IApproachService.cs
@@ -0,0 +1,9 @@
+﻿using ATSPM.ConfigApi.Models;
+
+namespace ATSPM.ConfigApi.Services
+{
+    public interface IApproachService
+    {
+        Task<ApproachDto> UpsertApproachAsync(ApproachDto dto);
+    }
+}
