3d3a6dc37d1527e11afc03ddce0d201749c9868a
kford <kford@avenueconsultants.com>
2024-07-03 15:51:23 -0600

    AVE-1522 - Fix Swagger for ConfigApi
    Adding in the dto

--- ATSPM/ConfigApi/Services/ApproachService.cs
+++ ATSPM/ConfigApi/Services/ApproachService.cs
@@ -82,21 +82,6 @@ namespace ATSPM.ConfigApi.Services
                         detector.DetectionTypes = detectorDto.DetectionTypes
                             .Select(dto => allDetectionTypes.FirstOrDefault(dt => dt.Id == dto.Id))
                             .Where(dt => dt != null).ToList();
-                            }
-                        }
-
-                        // Add new detection types
-                        foreach (var dtoDetectionType in dtoDetectionTypes)
-                        {
-                            if (!existingDetectionTypes.Contains(dtoDetectionType))
-                            {
-                                var detectionType = allDetectionTypes.FirstOrDefault(dt => dt.Id == dtoDetectionType.Id);
-                                if (detectionType != null)
-                                {
-                                    detector.DetectionTypes.Add(detectionType);
-                                }
-                            }
-                        }
                     }
                     else
                     {
@@ -240,9 +225,43 @@ namespace ATSPM.ConfigApi.Services
                     MovementDelay = d.MovementDelay,
                     LatencyCorrection = d.LatencyCorrection,
                     ApproachId = d.ApproachId,
-                    DetectionTypes = d.DetectionTypes.Select(dt => dt).ToList()
+                    DetectionTypes = ExtractDetectionTypesToDto(d.DetectionTypes.ToList())
                 }).ToList()
             };
+        }
+
+        private static List<DetectionTypeDto> ExtractDetectionTypesToDto(List<DetectionType> d)
+        {
+            return d.Select(dt => new DetectionTypeDto
+            {
+                Id = dt.Id,
+                Description = dt.Description,
+                Abbreviation = dt.Abbreviation,
+                DisplayOrder = dt.DisplayOrder,
+                MeasureTypes = dt.MeasureTypes.Select(mt => new MeasureTypeDto
+                {
+                    Id = mt.Id,
+                    Name = mt.Name,
+                    Abbreviation = mt.Abbreviation,
+                    ShowOnWebsite = mt.ShowOnWebsite,
+                    ShowOnAggregationSite = mt.ShowOnAggregationSite,
+                    DisplayOrder = mt.DisplayOrder,
+                    MeasureComments = mt.MeasureComments.Select(mc => new MeasureCommentsDto
+                    {
+                        Id = mc.Id,
+                        TimeStamp = mc.TimeStamp,
+                        Comment = mc.Comment,
+                        LocationIdentifier = mc.LocationIdentifier
+                    }).ToList(),
+                    MeasureOptions = mt.MeasureOptions.Select(mo => new MeasureOptionDto
+                    {
+                        Id = mo.Id,
+                        Option = mo.Option,
+                        Value = mo.Value,
+                        MeasureTypeId = mo.MeasureTypeId
+                    }).ToList()
+                }).ToList()
+            }).ToList();
         }
     }
 }
