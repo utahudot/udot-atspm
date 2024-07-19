2f0ed548520b5d2d93dd3db94da9cfb3799a22c2
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-07-19 09:12:09 -0600

    Method has 8 parameters, which is greater than the 7 authorized.

--- ATSPM/ApplicationCore/Business/ApproachSpeed/ApproachSpeedService.cs
+++ ATSPM/ApplicationCore/Business/ApproachSpeed/ApproachSpeedService.cs
@@ -3,7 +3,6 @@ using ATSPM.Data.Models;
 using ATSPM.Data.Models.EventLogModels;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -22,6 +21,7 @@ namespace ATSPM.Application.Business.ApproachSpeed
             this.cycleService = cycleService;
             this.planService = planService;
         }
+        const string DetectionNotFoundMessage = "Detection Type Not Found";
 
         public ApproachSpeedResult GetChartData(
             ApproachSpeedOptions options,
@@ -33,10 +33,8 @@ namespace ATSPM.Application.Business.ApproachSpeed
         {
             var speedEventsForDetector = speedEvents.Where(d => d.DetectorId == detector.DectectorIdentifier && d.Timestamp >= options.Start && d.Timestamp < options.End).ToList();
             var speedDetector = GetSpeedDetector(
+                options,
                 detector,
-                options.Start,
-                options.End,
-                options.BinSize,
                 planEvents,
                 cycleEvents,
                 speedEventsForDetector,
@@ -51,8 +49,8 @@ namespace ATSPM.Application.Business.ApproachSpeed
                    options.Start,
                    options.End,
                    detector.DetectionTypes.IsNullOrEmpty()
-                    ? "Detection Type Not Found"
-                    : detector.DetectionTypes.FirstOrDefault(d => d.MeasureTypes.Any(m => m.Id == options.MetricTypeId))?.Description ?? "Detection Type Not Found",
+                    ? DetectionNotFoundMessage
+                    : detector.DetectionTypes.FirstOrDefault(d => d.MeasureTypes.Any(m => m.Id == options.MetricTypeId))?.Description ?? DetectionNotFoundMessage,
                    detector.DistanceFromStopBar.Value,
                 detector.Approach.Mph.Value)
                 {
@@ -79,8 +77,8 @@ namespace ATSPM.Application.Business.ApproachSpeed
                     options.Start,
                     options.End,
                     detector.DetectionTypes.IsNullOrEmpty()
-                    ? "Detection Type Not Found"
-                    : detector.DetectionTypes.FirstOrDefault(d => d.MeasureTypes.Any(m => m.Id == options.MetricTypeId))?.Description ?? "Detection Type Not Found",
+                    ? DetectionNotFoundMessage
+                    : detector.DetectionTypes.FirstOrDefault(d => d.MeasureTypes.Any(m => m.Id == options.MetricTypeId))?.Description ?? DetectionNotFoundMessage,
                     detector.DistanceFromStopBar.Value,
                     detector.Approach.Mph.Value)
             {
@@ -92,29 +90,27 @@ namespace ATSPM.Application.Business.ApproachSpeed
         }
 
         public SpeedDetector GetSpeedDetector(
+            ApproachSpeedOptions options,
             Detector detector,
-            DateTime start,
-            DateTime end,
-            int binSize,
             List<IndianaEvent> planEvents,
             List<IndianaEvent> cycleEvents,
             List<SpeedEvent> speedEventsForDetector,
             ILogger logger)
         {
-            var cycles = cycleService.GetSpeedCycles(start, end, cycleEvents);
+            var cycles = cycleService.GetSpeedCycles(options.Start, options.End, cycleEvents);
             if (cycles.Any())
             {
                 foreach (var cycle in cycles)
                     cycle.FindSpeedEventsForCycle(speedEventsForDetector);
             }
-            var plans = planService.GetSpeedPlans(cycles, start, end, detector.Approach, planEvents);
+            var plans = planService.GetSpeedPlans(cycles, options.Start, options.End, detector.Approach, planEvents);
             if (speedEventsForDetector.IsNullOrEmpty())
             {
                 return new SpeedDetector(
                     plans,
                     0,
-                    start,
-                    end,
+                    options.Start,
+                    options.End,
                     cycles,
                     null,
                     null
@@ -125,12 +121,12 @@ namespace ATSPM.Application.Business.ApproachSpeed
             var movementDelay = 0;
             if (detector.MovementDelay != null)
                 movementDelay = detector.MovementDelay.Value;
-            var avgSpeedBucketCollection = new AvgSpeedBucketCollection(start, end, binSize, movementDelay, cycles, logger);
+            var avgSpeedBucketCollection = new AvgSpeedBucketCollection(options.Start, options.End, options.BinSize, movementDelay, cycles, logger);
             return new SpeedDetector(
                 plans,
                 totalDetectorHits,
-                start,
-                end,
+                options.Start,
+                options.End,
                 cycles,
                 speedEventsForDetector,
                 avgSpeedBucketCollection
