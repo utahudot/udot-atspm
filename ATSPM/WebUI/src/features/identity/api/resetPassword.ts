e38f0137c8ee6488082d731569fc3bb449d76c19
dkimball <dkimball@avenueconsultants.com>
2024-07-26 13:20:26 -0600

    Fixed password reset and profile page

--- ATSPM/WebUI/src/features/identity/api/resetPassword.ts
+++ ATSPM/WebUI/src/features/identity/api/resetPassword.ts
@@ -2,7 +2,7 @@ import { identityAxios } from '@/lib/axios'
 import { ExtractFnReturnType, QueryConfig } from '@/lib/react-query'
 import { useQuery } from 'react-query'
 
-type QueryFnType = typeof forgotPassword
+type QueryFnType = typeof resetPassword
 
 type UseLoginOptions = {
   config?: QueryConfig<QueryFnType>
@@ -11,17 +11,17 @@ type UseLoginOptions = {
 
 // const userToken = Cookies.get('token')
 
-const forgotPassword = async (email: string) => {
+const resetPassword = async (email: string) => {
   const response: any = await identityAxios.post('/Account/forgotpassword', {
     email,
   })
   return response
 }
 
-export const useForgotPassword = ({ config, email }: UseLoginOptions) => {
+export const useResetPassword = ({ config, email }: UseLoginOptions) => {
   return useQuery<ExtractFnReturnType<QueryFnType>>({
     ...config,
     enabled: false,
-    queryFn: () => forgotPassword(email),
+    queryFn: () => resetPassword(email),
   })
 }
