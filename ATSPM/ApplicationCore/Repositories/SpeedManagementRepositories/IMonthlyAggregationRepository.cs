e180ba37a30a1fbb4e03d9041b7182cc10f3a1c6
kford <kford@avenueconsultants.com>
2024-07-29 10:55:52 -0600

    AVE-1579 - Data rollup
    All the inputs and the structure.

---
+++ ATSPM/ApplicationCore/Repositories/SpeedManagementRepositories/IMonthlyAggregationRepository.cs
@@ -0,0 +1,18 @@
+﻿using ATSPM.Data.Models.SpeedManagementAggregation;
+using ATSPM.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ATSPM.Application.Repositories.SpeedManagementRepositories
+{
+    public interface IMonthlyAggregationRepository : IAsyncRepository<MonthlyAggregation>
+    {
+        Task<List<MonthlyAggregation>> SelectBinStartTimesInRange(DateTime startTime, DateTime endTime, MonthlyAggregation monthlyAggregation);
+        Task<List<MonthlyAggregation>> SelectBinStartTimesInRangeFromSource(DateTime startTime, DateTime endTime, MonthlyAggregation monthlyAggregation);
+        Task<MonthlyAggregation> SelectByBinTimeSegment(DateTime binStartTime, MonthlyAggregation monthlyAggregation);
+        Task<MonthlyAggregation> SelectByBinTimeSegmentAndSource(DateTime binStartTime, MonthlyAggregation monthlyAggregation);
+        Task<List<MonthlyAggregation>> SelectMonthlyAggregationBySegment(Guid SegmentId);
+        Task<MonthlyAggregation> UpsertMonthlyAggregationAsync(MonthlyAggregation item);
+    }
+}
