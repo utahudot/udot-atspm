3d14f5e2beccbd76d6644749fe8d79364696339b
Jaithra <jbhatia@avenueconsultants.com>
2024-07-29 13:22:09 -0600

    another fix

--- ATSPM/SpeedManagementApi/Controllers/CongestionTrackingController.cs
+++ ATSPM/SpeedManagementApi/Controllers/CongestionTrackingController.cs
@@ -1,13 +1,11 @@
-﻿using Asp.Versioning;
-using ATSPM.Application.Repositories.SpeedManagementRepositories;
+﻿using ATSPM.Application.Repositories.SpeedManagementRepositories;
 using ATSPM.Data.Models.SpeedManagement.CongestionTracking;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SpeedManagementApi.Controllers
 {
-    [ApiVersion(1.0)]
     [ApiController]
-    [Route("v{version:apiVersion}/[controller]")]
+    [Route("[controller]")]
     public class CongestionTrackingController : ControllerBase
     {
         private ICongestionTrackingService _congestionTrackingService;
@@ -24,16 +22,8 @@ namespace SpeedManagementApi.Controllers
                 return BadRequest(ModelState);
             }
 
-            try
-            {
-                var result = await _congestionTrackingService.GetReportData(options);
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                BadRequest(ex.Message);
-            }
-            return Unauthorized("I am here");
+            var result = await _congestionTrackingService.GetReportData(options);
+            return Ok(result);
         }
     }
 }
