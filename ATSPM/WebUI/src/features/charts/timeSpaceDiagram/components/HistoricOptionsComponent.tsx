660ee72b51f10acc5ab6a0fda5f89418fe7238c7
Morgan Stanley <mstanley@avenueconsultants.com>
2024-07-29 21:43:59 -0600

    update select datetime, fix time space request

--- ATSPM/WebUI/src/features/charts/timeSpaceDiagram/components/HistoricOptionsComponent.tsx
+++ ATSPM/WebUI/src/features/charts/timeSpaceDiagram/components/HistoricOptionsComponent.tsx
@@ -1,12 +1,12 @@
 import SelectTimeSpan from '@/components/selectTimeSpan'
 import {
+  Box,
   FormControl,
   InputLabel,
   MenuItem,
   Paper,
   Select,
   TextField,
-  useTheme,
 } from '@mui/material'
 import { ToolType } from '../../common/types'
 import { TSHistoricHandler } from './handlers/handlers'
@@ -16,55 +16,53 @@ interface Props {
 }
 
 export const HistoricOptionsComponent = ({ handler }: Props) => {
-  const theme = useTheme()
-
   return (
-    <Paper
-      sx={{
-        padding: theme.spacing(3),
-      }}
-    >
-      <FormControl fullWidth sx={{ mb: 2 }}>
-        <InputLabel htmlFor="route-input">Route</InputLabel>
-        <Select
-          label="Route"
+    <Box display="flex" gap={2}>
+      <Paper sx={{ p: 3, width: '350px' }}>
+        <FormControl fullWidth sx={{ mb: 2 }}>
+          <InputLabel htmlFor="route-input">Route</InputLabel>
+          <Select
+            label="Route"
+            variant="outlined"
+            fullWidth
+            sx={{ mb: 2 }}
+            onChange={(e) => handler.setRouteId(e.target.value)}
+            value={
+              handler.routes?.find(
+                (route) => route.id === Number.parseInt(handler.routeId)
+              )?.id || ''
+            }
+            inputProps={{ id: 'route-input' }}
+          >
+            {handler.routes.map(
+              (route: { name: string; id: number }, index: number) => {
+                return (
+                  <MenuItem key={index} value={route.id}>
+                    {route.name}
+                  </MenuItem>
+                )
+              }
+            )}
+          </Select>
+        </FormControl>
+        <TextField
+          label="Speed Limit (mph)"
           variant="outlined"
           fullWidth
           sx={{ mb: 2 }}
-          onChange={(e) => handler.setRouteId(e.target.value)}
-          value={
-            handler.routes?.find(
-              (route) => route.id === Number.parseInt(handler.routeId)
-            )?.id || ''
-          }
-          inputProps={{ id: 'route-input' }}
-        >
-          {handler.routes.map(
-            (route: { name: string; id: number }, index: number) => {
-              return (
-                <MenuItem key={index} value={route.id}>
-                  {route.name}
-                </MenuItem>
-              )
-            }
-          )}
-        </Select>
-      </FormControl>
-      <TextField
-        label="Speed Limit (mph)"
-        variant="outlined"
-        helperText="Optional"
-        fullWidth
-        sx={{ mb: 2 }}
-        onChange={(e) => handler.setSpeedLimit(parseInt(e.target.value))}
-      />
-      <SelectTimeSpan
-        startDateTime={handler.startDateTime}
-        endDateTime={handler.endDateTime}
-        changeStartDate={handler.changeStartDate}
-        changeEndDate={handler.changeEndDate}
-        chartType={ToolType.TimeSpaceHistoric}
-      />
-    </Paper>
+          onChange={(e) => handler.setSpeedLimit(parseInt(e.target.value))}
+        />
+      </Paper>
+      <Paper sx={{ p: 3 }}>
+        <SelectTimeSpan
+          startDateTime={handler.startDateTime}
+          endDateTime={handler.endDateTime}
+          changeStartDate={handler.changeStartDate}
+          changeEndDate={handler.changeEndDate}
+          chartType={ToolType.TimeSpaceHistoric}
+          calendarLocation="right"
+        />
+      </Paper>
+    </Box>
   )
 }
