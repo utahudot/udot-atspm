c4db7a63edad2d99bbaec6817226ad9353d32419
Morgan Stanley <mstanley@avenueconsultants.com>
2024-07-31 10:15:05 -0600

    remove next-runtime-config stuff

--- ATSPM/WebUI/next.config.js
+++ ATSPM/WebUI/next.config.js
@@ -1,6 +1,3 @@
-const { configureRuntimeEnv } = require('next-runtime-env/build/configure')
-configureRuntimeEnv()
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
