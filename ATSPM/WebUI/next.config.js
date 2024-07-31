fbfd946ddf252de38df87d84b13aa1e89656511c
Morgan Stanley <mstanley@avenueconsultants.com>
2024-07-30 22:08:39 -0600

    add next-runtime-env

--- ATSPM/WebUI/next.config.js
+++ ATSPM/WebUI/next.config.js
@@ -1,3 +1,6 @@
+const { configureRuntimeEnv } = require('next-runtime-env/build/configure')
+configureRuntimeEnv()
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
