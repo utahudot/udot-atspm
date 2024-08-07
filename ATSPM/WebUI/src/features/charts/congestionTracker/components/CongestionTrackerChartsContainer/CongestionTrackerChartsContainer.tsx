781cd2c90d7ce2cc8915c410eb30d504ab8eeb76
Morgan Stanley <mstanley@avenueconsultants.com>
2024-08-07 12:42:33 -0600

    update chart

---
+++ ATSPM/WebUI/src/features/charts/congestionTracker/components/CongestionTrackerChartsContainer/CongestionTrackerChartsContainer.tsx
@@ -0,0 +1,32 @@
+import ApacheEChart from '@/features/charts/components/apacheEChart'
+import { useCongestionTrackingChart } from '@/features/speedManagementTool/api/getCongestionTrackingData'
+import { Paper } from '@mui/material'
+
+const CongestionTrackingChartsContainer = ({
+  selectedRouteId,
+}: {
+  selectedRouteId: number
+}) => {
+  const { data: congestionTrackingData } = useCongestionTrackingChart({
+    options: {
+      segmentId: selectedRouteId.toString(),
+      startDate: '2022-01-01T00:00:00',
+      endDate: '2022-01-01T23:59:59',
+    },
+  })
+  if (!congestionTrackingData) return null
+
+  return (
+    // align in center
+    <Paper sx={{ display: 'flex', justifyContent: 'center' }}>
+      <ApacheEChart
+        id="congestion-chart"
+        option={congestionTrackingData}
+        style={{ width: '800px', height: '550px' }}
+        hideInteractionMessage
+      />
+    </Paper>
+  )
+}
+
+export default CongestionTrackingChartsContainer
