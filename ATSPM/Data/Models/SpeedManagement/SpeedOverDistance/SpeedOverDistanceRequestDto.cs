474a067ddeaac444bb1901394eef341895888958
kford <kford@avenueconsultants.com>
2024-08-09 13:55:56 -0600

    AVE-1577 - Backend - Endpoint
    Make and test the Speed over Distance endpoint.

---
+++ ATSPM/Data/Models/SpeedManagement/SpeedOverDistance/SpeedOverDistanceRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ATSPM.Data.Models.SpeedManagement.MonthlyAggregation;
+
+public class SpeedOverDistanceRequestDto
+{
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public List<Guid> SegmentIds { get; set; }
+}
