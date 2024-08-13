033f10078cfdc6dcfe61c676a28d68d0d763eb8b
Christian Baker <christianbaker@utah.gov>
2024-08-13 14:55:19 -0600

    Removed Data-ETL (for now)

--- ATSPM/Data-ETL/SpeedEventLog.cs
+++
@@ -1,7 +0,0 @@
-﻿//public partial class SpeedEvent
-//{
-//    public string DetectorId { get; set; }
-//    public int Mph { get; set; }
-//    public int Kph { get; set; }
-//    public DateTime TimeStamp { get; set; }
-//}
