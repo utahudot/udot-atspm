9b96cf551f4f9dece7e751b6735612811be8a4f1
Morgan Stanley <mstanley@avenueconsultants.com>
2024-08-14 13:50:30 -0600

    hail mary for env variables

--- ATSPM/WebUI/src/config/index.ts
+++ ATSPM/WebUI/src/config/index.ts
@@ -1,13 +1,18 @@
-export const MOCKING_ENABLED = process.env.NEXT_PUBLIC_API_MOCKING === 'true'
+'use client'
 
-export const CONFIG_URL = process.env.NEXT_PUBLIC_CONFIG_URL as string
-export const REPORTS_URL = process.env.NEXT_PUBLIC_REPORTS_URL as string
-export const IDENTITY_URL = process.env.NEXT_PUBLIC_IDENTITY_URL as string
-export const DATA_URL = process.env.NEXT_PUBLIC_DATA_URL as string
+import { env } from 'next-runtime-env'
+
+export const MOCKING_ENABLED = env('NEXT_PUBLIC_API_MOCKING') === 'true'
+
+export const CONFIG_URL = env('NEXT_PUBLIC_CONFIG_URL') as string
+export const REPORTS_URL = env('NEXT_PUBLIC_REPORTS_URL') as string
+export const IDENTITY_URL = env('NEXT_PUBLIC_IDENTITY_URL') as string
+export const DATA_URL = env('NEXT_PUBLIC_DATA_URL') as string
+export const SPEED_URL = env('NEXT_PUBLIC_SPEED_URL') as string
 
 export const MAP_DEFAULT_LATITUDE = parseFloat(
-  process.env.NEXT_PUBLIC_MAP_DEFAULT_LATITUDE as string
+  env('NEXT_PUBLIC_MAP_DEFAULT_LATITUDE') as string
 )
 export const MAP_DEFAULT_LONGITUDE = parseFloat(
-  process.env.NEXT_PUBLIC_MAP_DEFAULT_LONGITUDE as string
+  env('NEXT_PUBLIC_MAP_DEFAULT_LONGITUDE') as string
 )
