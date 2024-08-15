0ccee769cf3f9359ec3082bcac6ca61d0e8f763f
Morgan Stanley <mstanley@avenueconsultants.com>
2024-08-15 15:50:18 -0600

    remove next-runtime-env

--- ATSPM/WebUI/src/config/index.ts
+++ ATSPM/WebUI/src/config/index.ts
@@ -1,18 +1,6 @@
-'use client'
-
-import { env } from 'next-runtime-env'
-
-export const MOCKING_ENABLED = env('NEXT_PUBLIC_API_MOCKING') === 'true'
-
-export const CONFIG_URL = env('NEXT_PUBLIC_CONFIG_URL') as string
-export const REPORTS_URL = env('NEXT_PUBLIC_REPORTS_URL') as string
-export const IDENTITY_URL = env('NEXT_PUBLIC_IDENTITY_URL') as string
-export const DATA_URL = env('NEXT_PUBLIC_DATA_URL') as string
-export const SPEED_URL = env('NEXT_PUBLIC_SPEED_URL') as string
-
 export const MAP_DEFAULT_LATITUDE = parseFloat(
-  env('NEXT_PUBLIC_MAP_DEFAULT_LATITUDE') as string
+  process.env.NEXT_PUBLIC_MAP_DEFAULT_LATITUDE as string
 )
 export const MAP_DEFAULT_LONGITUDE = parseFloat(
-  env('NEXT_PUBLIC_MAP_DEFAULT_LONGITUDE') as string
+  process.env.NEXT_PUBLIC_MAP_DEFAULT_LONGITUDE as string
 )
