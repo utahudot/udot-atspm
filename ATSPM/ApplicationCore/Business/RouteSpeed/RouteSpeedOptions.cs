9314f1e6cc9c53e3f2e29519d9b5e40cf5040f7b
Jaithra <jbhatia@avenueconsultants.com>
2024-08-19 13:15:08 -0600

    allow more options for get route speeds

--- ATSPM/ApplicationCore/Business/RouteSpeed/RouteSpeedOptions.cs
+++ ATSPM/ApplicationCore/Business/RouteSpeed/RouteSpeedOptions.cs
@@ -7,11 +7,15 @@ namespace ATSPM.Application.Business.RouteSpeed
     {
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public DateTime? StartTime { get; set; }
-        public DateTime? EndTime { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
         public List<DayOfWeek> DaysOfWeek { get; set; }
         public int ViolationThreshold { get; set; }
         public int SourceId { get; set; }
-        public AnalysisPeriod AnalysisPeriod { get; set; }
+        public string? Region { get; set; }
+        public string? County { get; set; }
+        public string? City { get; set; }
+        public string? AccessCategory { get; set; }
+        public string? FunctionalType { get; set; }
     }
 }
