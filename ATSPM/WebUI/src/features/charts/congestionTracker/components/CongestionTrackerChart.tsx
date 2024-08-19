8802eaceff88a58f2de9b8b5e9f0f148007e1d0a
Morgan Stanley <mstanley@avenueconsultants.com>
2024-08-19 17:17:48 -0600

    save current changes

--- ATSPM/WebUI/src/features/charts/congestionTracker/components/CongestionTrackerChart.tsx
+++ ATSPM/WebUI/src/features/charts/congestionTracker/components/CongestionTrackerChart.tsx
@@ -1,20 +1,8 @@
 import { ChartType } from '@/features/charts/common/types'
 import { transformCongestionTrackerData } from '@/features/charts/congestionTracker/congestionTracker.transformer'
-import {
-  adjustPlanPositions,
-  handleGreenTimeUtilizationDataZoom,
-} from '@/features/charts/utils'
-import { useChartsStore } from '@/stores/charts'
 import { ToggleButton, ToggleButtonGroup } from '@mui/material'
-import type {
-  DataZoomComponentOption,
-  DatasetComponentOption,
-  ECharts,
-  EChartsOption,
-  SeriesOption,
-  SetOptionOpts,
-} from 'echarts'
-import { connect, init } from 'echarts'
+import type { ECharts, EChartsOption, SetOptionOpts } from 'echarts'
+import { init } from 'echarts'
 import type { CSSProperties } from 'react'
 import { useEffect, useRef, useState } from 'react'
 
@@ -37,17 +25,11 @@ export default function CongestionChart({
   settings,
   loading,
   theme,
-  hideInteractionMessage = false,
 }: CongestionChartProps) {
   const chartRef = useRef<HTMLDivElement>(null)
-  const { activeChart, setActiveChart } = useChartsStore()
-  const [isHovered, setIsHovered] = useState(false)
-  const [isScrolling, setIsScrolling] = useState(false)
   const chartInstance = useRef<ECharts | null>(null)
 
   const [currentView, setCurrentView] = useState<'week' | 'month'>('month')
-
-  const isActive = activeChart === id
 
   const initChart = () => {
     if (chartRef.current !== null) {
@@ -55,36 +37,7 @@ export default function CongestionChart({
         useDirtyRect: true,
       })
 
-      // Set initial options with zooming disabled
-      const disabledZoomOption: EChartsOption = {
-        ...option,
-        dataZoom: (option.dataZoom as DataZoomComponentOption[])?.map(
-          (zoom) => ({
-            ...zoom,
-            disabled: true,
-            zoomLock: true,
-          })
-        ),
-        series: (option.series as SeriesOption[])?.map((series) => ({
-          ...series,
-          silent: true,
-        })),
-      }
-
-      chartInstance.current.setOption(disabledZoomOption, settings)
-
-      if (chartType === ChartType.GreenTimeUtilization) {
-        chartInstance.current.on('datazoom', () =>
-          handleGreenTimeUtilizationDataZoom(chartInstance.current!)
-        )
-      } else if (chartType === ChartType.TimingAndActuation) {
-        chartInstance.current.group = 'group1'
-        connect('group1')
-      } else {
-        chartInstance.current.on('datazoom', () =>
-          adjustPlanPositions(chartInstance.current!)
-        )
-      }
+      chartInstance.current.setOption(option, settings)
     }
   }
 
@@ -104,87 +57,26 @@ export default function CongestionChart({
 
   useEffect(() => {
     if (chartInstance.current) {
-      const updatedOption: EChartsOption = {
-        ...option,
-        dataZoom: (option.dataZoom as DataZoomComponentOption[])?.map(
-          (zoom) => ({
-            ...zoom,
-            disabled: !isActive,
-            zoomLock: !isActive,
-          })
-        ),
-        series: (option.series as SeriesOption[])?.map((series) => ({
-          ...series,
-          silent: !isActive,
-        })),
-      }
-      chartInstance.current.setOption(updatedOption, settings)
-    }
-  }, [option, settings, theme, isActive])
-
-  useEffect(() => {
-    if (chartInstance.current) {
       loading
         ? chartInstance.current.showLoading()
         : chartInstance.current.hideLoading()
     }
   }, [loading])
 
-  useEffect(() => {
-    let scrollTimeout: NodeJS.Timeout
-
-    const handleScroll = () => {
-      setIsScrolling(true)
-      clearTimeout(scrollTimeout)
-      scrollTimeout = setTimeout(() => {
-        setIsScrolling(false)
-      }, 700)
-    }
-
-    window.addEventListener('scroll', handleScroll)
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-      clearTimeout(scrollTimeout)
-    }
-  }, [])
-
-  const handleActivate = () => {
-    if (!isActive) {
-      setActiveChart(id)
-      if (chartInstance.current) {
-        chartInstance.current.setOption({
-          ...option,
-          dataZoom: (option.dataZoom as DatasetComponentOption[])?.map(
-            (zoom) => ({
-              ...zoom,
-              disabled: false,
-              zoomLock: false,
-            })
-          ),
-          series: (option.series as SeriesOption[])?.map((series) => ({
-            ...series,
-            silent: false,
-          })),
-        })
-      }
-    }
-  }
-
   const handleViewChange = (
-    event: React.MouseEvent<HTMLElement>,
+    _: React.MouseEvent<HTMLElement>,
     newView: 'week' | 'month'
   ) => {
-    if (newView !== null) {
-      setCurrentView(newView)
-      const updatedOption = transformCongestionTrackerData(
-        option.response,
-        newView
-      )
-      chartInstance.current?.setOption(updatedOption, {
-        replaceMerge: ['title'],
-      })
-    }
+    if (newView === null) return
+
+    const updatedOption = transformCongestionTrackerData(
+      option.response,
+      newView
+    )
+    chartInstance.current?.setOption(updatedOption, {
+      replaceMerge: ['title'],
+    })
+    setCurrentView(newView)
   }
 
   return (
@@ -197,9 +89,6 @@ export default function CongestionChart({
       }}
       role="presentation"
       aria-hidden="true"
-      onClick={handleActivate}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
     >
       <div
         id={id}
@@ -229,46 +118,6 @@ export default function CongestionChart({
           Week
         </ToggleButton>
       </ToggleButtonGroup>
-      {!hideInteractionMessage && (
-        <>
-          <div
-            style={{
-              position: 'absolute',
-              top: option?.grid?.top || 0,
-              left: option?.grid?.left || 0,
-              right: option?.grid?.right || 0,
-              bottom: option?.grid?.bottom || 0,
-              background: 'rgba(0, 0, 0, 0.3)',
-              display: 'flex',
-              visibility:
-                !isActive && isHovered && isScrolling ? 'visible' : 'hidden',
-              justifyContent: 'center',
-              alignItems: 'center',
-              color: 'white',
-              fontSize: '24px',
-              zIndex: 1,
-              textShadow: '0 0 2px black',
-            }}
-          >
-            Click to enable zoom
-          </div>
-          {isActive && (
-            <div
-              style={{
-                display: isActive ? 'block' : 'none',
-                position: 'absolute',
-                top: option?.grid?.top || 0,
-                left: option?.grid?.left || 0,
-                right: option?.grid?.right || 0,
-                bottom: option?.grid?.bottom || 0,
-                outline: '2px solid #0060df80',
-                zIndex: 1,
-                pointerEvents: 'none',
-              }}
-            />
-          )}
-        </>
-      )}
     </div>
   )
 }
