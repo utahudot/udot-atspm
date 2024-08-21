55f43cee6f433a20a4aeaf03e2beacd3c6543d87
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-08-21 09:42:37 -0600

    Adding retry's and fixing source id for atspm

--- ATSPM/SpeedManagementImporter/Services/Atspm/AtspmDownloaderService.cs
+++ ATSPM/SpeedManagementImporter/Services/Atspm/AtspmDownloaderService.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections.Concurrent;
-using System.Data.SqlClient;
-using ATSPM.Application.Repositories.SpeedManagementRepositories;
+﻿using ATSPM.Application.Repositories.SpeedManagementRepositories;
 using ATSPM.Data.Models.SpeedManagementAggregation;
 using SpeedManagementImporter.Business.Atspm;
+using System.Collections.Concurrent;
+using System.Data.SqlClient;
 
 namespace SpeedManagementImporter.Services.Atspm
 {
@@ -25,11 +25,11 @@ namespace SpeedManagementImporter.Services.Atspm
             var routes = routeEntities.GroupBy(r => r.SegmentId).ToList();
 
             var speeds = new ConcurrentBag<HourlySpeed>();
+
             for (DateTime date = startDate; date < endDate; date = date.AddHours(1))
             {
                 foreach (var route in routes)
                 {
-
                     List<ApproachSpeed> approachSpeeds = new List<ApproachSpeed>();
                     string selectQuery = $@"SELECT [BinStartTime]
                                           ,[SignalId]
@@ -42,38 +42,47 @@ namespace SpeedManagementImporter.Services.Atspm
                                       WHERE BinStartTime BETWEEN '{date}' AND '{date.AddHours(1)}'
                                       AND ApproachId IN ({string.Join(",", route.Select(r => r.EntityId))})";
 
-
-
-                    using (SqlConnection sourceConnection = new SqlConnection(sourceConnectionString))
+                    bool success = false;
+                    while (!success)
                     {
-                        sourceConnection.Open();
-
-                        using (SqlCommand selectCommand = new SqlCommand(selectQuery, sourceConnection))
+                        try
                         {
-
-                            using (SqlDataReader reader = selectCommand.ExecuteReader())
+                            using (SqlConnection sourceConnection = new SqlConnection(sourceConnectionString))
                             {
-                                using (SqlConnection targetConnection = new SqlConnection(sourceConnectionString))
+                                await sourceConnection.OpenAsync();
+
+                                using (SqlCommand selectCommand = new SqlCommand(selectQuery, sourceConnection))
                                 {
-                                    targetConnection.Open();
-                                    while (reader.Read())
+                                    using (SqlDataReader reader = await selectCommand.ExecuteReaderAsync())
                                     {
-                                        approachSpeeds.Add(new ApproachSpeed
+                                        while (await reader.ReadAsync())
                                         {
-                                            ApproachId = (int)reader["ApproachId"],
-                                            SignalId = (string)reader["SignalId"],
-                                            Speed15th = (int)reader["Speed15th"],
-                                            Speed85th = (int)reader["Speed85th"],
-                                            SpeedVolume = (int)reader["SpeedVolume"],
-                                            SummedSpeed = (int)reader["SummedSpeed"]
-                                        });
+                                            approachSpeeds.Add(new ApproachSpeed
+                                            {
+                                                ApproachId = (int)reader["ApproachId"],
+                                                SignalId = (string)reader["SignalId"],
+                                                Speed15th = (int)reader["Speed15th"],
+                                                Speed85th = (int)reader["Speed85th"],
+                                                SpeedVolume = (int)reader["SpeedVolume"],
+                                                SummedSpeed = (int)reader["SummedSpeed"]
+                                            });
+                                        }
                                     }
                                 }
                             }
+                            success = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error reading from database: {ex.Message}. Retrying in 5 minutes...");
+                            approachSpeeds.Clear(); // Clear the list to remove partially read data
+                            await Task.Delay(TimeSpan.FromMinutes(5));
                         }
                     }
+
                     approachSpeeds = approachSpeeds.Where(a => a.SpeedVolume > 0 && a.SummedSpeed > 0).ToList();
                     if (approachSpeeds.Count == 0) { continue; }
+
                     var summedSpeed = approachSpeeds.Sum(a => a.SummedSpeed);
                     var summedVolume = approachSpeeds.Sum(a => a.SpeedVolume);
                     double averageSpeed = summedSpeed / summedVolume;
@@ -90,20 +99,40 @@ namespace SpeedManagementImporter.Services.Atspm
                         SegmentId = route.Key,
                         Violation = Convert.ToInt64(speedLimit < eightyFifthSpeed && speedLimit != 0 ? (long)eightyFifthSpeed - speedLimit : 0),
                         Flow = summedVolume,
-                        SourceId = 3,
+                        SourceId = sourceId,
                         ConfidenceId = 4
                     });
 
                 }
+
                 Console.WriteLine($"{date} for source ATSPM added to list");
+
                 if (speeds.Count > 10000)
+                {
+                    await TryWriteToDatabaseAsync(speeds);
+                }
+            }
+            await TryWriteToDatabaseAsync(speeds);
+        }
+
+        private async Task TryWriteToDatabaseAsync(ConcurrentBag<HourlySpeed> speeds)
+        {
+            bool success = false;
+            while (!success)
+            {
+                try
                 {
                     await hourlySpeedRepository.AddHourlySpeedsAsync(speeds.ToList());
                     speeds.Clear();
+                    Console.WriteLine("List written to database");
+                    success = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error writing to database: {ex.Message}. Retrying in 5 minutes...");
+                    await Task.Delay(TimeSpan.FromMinutes(5));
                 }
             }
-            await hourlySpeedRepository.AddHourlySpeedsAsync(speeds.ToList());
-            //await InsertSpeedRecords(speedRepository, speeds);
         }
     }
 }
