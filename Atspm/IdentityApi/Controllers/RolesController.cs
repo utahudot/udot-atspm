3ec8e7dc6dc1ed0f9b8ae8e7db5f1dd881cf9daf
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2024-08-22 15:21:05 -0600

    Fix roles and claims seed.

--- Atspm/IdentityApi/Controllers/RolesController.cs
+++ Atspm/IdentityApi/Controllers/RolesController.cs
@@ -21,9 +21,7 @@ using Identity.Models.Role;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Data;
-using System.Security.Claims;
 using Utah.Udot.Atspm.Data.Models;
 
 namespace Identity.Controllers
@@ -98,78 +96,5 @@ namespace Identity.Controllers
                 return BadRequest(result?.Errors);
             }
         }
-
-
-        // POST: api/roles/setup
-        [HttpPost("setup")]
-        public async Task<IActionResult> Setup()
-        {
-            try
-            {
-                if (roleManager.Roles.AsNoTracking().Any())
-                {
-                    return BadRequest("Roles already exist.");
-                }
-                await roleManager.CreateAsync(new IdentityRole { Name = "Admin" });
-                await roleManager.CreateAsync(new IdentityRole("UserAdmin"));
-                await roleManager.CreateAsync(new IdentityRole { Name = "RoleAdmin" });
-                await roleManager.CreateAsync(new IdentityRole { Name = "LocationConfigurationAdmin" });
-                await roleManager.CreateAsync(new IdentityRole { Name = "GeneralConfigurationAdmin" });
-                await roleManager.CreateAsync(new IdentityRole { Name = "DataAdmin" });
-                await roleManager.CreateAsync(new IdentityRole { Name = "WatchdogSubscriber" });
-                await roleManager.CreateAsync(new IdentityRole { Name = "ReportAdmin" });
-
-                return Ok();
-
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            //return BadRequest("Could not add claim to role.");
-        }
-
-
-        [HttpPost("setupClaims")]
-        public async Task<IActionResult> SetupClaims()
-        {
-            var userAdminRole = roleManager.FindByNameAsync("UserAdmin").Result;
-            if (userAdminRole == null)
-            {
-                return BadRequest("UserAdmin role not found.");
-            }
-            await roleManager.AddClaimAsync(userAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "User:View"));
-            await roleManager.AddClaimAsync(userAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "User:Edit"));
-            await roleManager.AddClaimAsync(userAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "User:Delete"));
-
-            var adminRole = roleManager.FindByNameAsync("Admin").Result;
-            await roleManager.AddClaimAsync(adminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Admin"));
-
-            var roleAdminRole = await roleManager.FindByNameAsync("RoleAdmin");
-            await roleManager.AddClaimAsync(roleAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Role:View"));
-            await roleManager.AddClaimAsync(roleAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Role:Edit"));
-            await roleManager.AddClaimAsync(roleAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Role:Delete"));
-
-            var locationAdminRole = await roleManager.FindByNameAsync("LocationConfigurationAdmin");
-            await roleManager.AddClaimAsync(locationAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "LocationConfiguration:View"));
-            await roleManager.AddClaimAsync(locationAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "LocationConfiguration:Edit"));
-            await roleManager.AddClaimAsync(locationAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "LocationConfiguration:Delete"));
-
-            var generalAdminRole = await roleManager.FindByNameAsync("GeneralConfigurationAdmin");
-            await roleManager.AddClaimAsync(generalAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "GeneralConfiguration:View"));
-            await roleManager.AddClaimAsync(generalAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "GeneralConfiguration:Edit"));
-            await roleManager.AddClaimAsync(generalAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "GeneralConfiguration:Delete"));
-
-            var dataAdminRole = await roleManager.FindByNameAsync("DataAdmin");
-            await roleManager.AddClaimAsync(dataAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Data:View"));
-            await roleManager.AddClaimAsync(dataAdminRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Data:Edit"));
-
-            var watchdogSubscriberRole = await roleManager.FindByNameAsync("WatchdogSubscriber");
-            await roleManager.AddClaimAsync(watchdogSubscriberRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Watchdog:View"));
-            await roleManager.AddClaimAsync(watchdogSubscriberRole, new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Report:View"));
-
-            return Ok();
-        }
-
     }
 }
