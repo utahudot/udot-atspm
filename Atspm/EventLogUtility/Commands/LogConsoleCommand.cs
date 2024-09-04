e047e3b1f00607f6ecfa229a9940f8dfe52fba4b
Christian Baker <86611690+ChesireFreeThrow@users.noreply.github.com>
2024-09-04 12:50:48 -0600

    Updated LogConsoleCommand

--- Atspm/EventLogUtility/Commands/LogConsoleCommand.cs
+++ Atspm/EventLogUtility/Commands/LogConsoleCommand.cs
@@ -16,14 +16,16 @@
 #endregion
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.CommandLine;
 using System.CommandLine.Hosting;
 using System.CommandLine.NamingConventionBinder;
+using Utah.Udot.Atspm.Data.Enums;
 using Utah.Udot.Atspm.Infrastructure.Services.HostedServices;
 
 namespace Utah.Udot.Atspm.EventLogUtility.Commands
 {
-    public class LogConsoleCommand : Command, ICommandOption<EventLogLoggingConfiguration>
+    public class LogConsoleCommand : Command, ICommandOption<DeviceEventLoggingConfiguration>
     {
         public LogConsoleCommand() : base("log", "Logs data from Location controllers")
         {
@@ -38,34 +40,21 @@ namespace Utah.Udot.Atspm.EventLogUtility.Commands
                     r.ErrorMessage = "Can't use exclude option when also using include option";
             });
 
-            AddArgument(PingControllerArg);
+            AddArgument(PingDeviceArg);
             AddArgument(DeleteLocalFileArg);
 
             AddOption(IncludeOption);
             AddOption(ExcludeOption);
-            AddOption(TypeOption);
+            AddOption(AreaOption);
+            AddOption(JurisdictionOption);
+            AddOption(RegionOption);
+            AddOption(LocationTypeOption);
+            AddOption(DeviceTypeOption);
+            AddOption(TransportProtocolOption);
             AddOption(PathCommandOption);
-
-            //this.SetHandler((d, i, e, p) =>
-            //{
-            //    Console.WriteLine($"{this.Name} is executing");
-
-            //    foreach (var s in i)
-            //    {
-            //        Console.WriteLine($"Extracting event logs for Location {s}");
-            //    }
-
-            //    foreach (var s in e)
-            //    {
-            //        Console.WriteLine($"Excluding event logs for Location {s}");
-            //    }
-
-            //    Console.WriteLine($"Extraction path {p}");
-
-            //}, DateOption, IncludeOption, ExcludeOption, PathCommandOption);
         }
 
-        public Argument<bool> PingControllerArg { get; set; } = new Argument<bool>("ping", "Ping to verify Location controller is online");
+        public Argument<bool> PingDeviceArg { get; set; } = new Argument<bool>("ping", "Ping to verify device is online");
 
         public Argument<bool> DeleteLocalFileArg { get; set; } = new Argument<bool>("delete local", "Delete local file");
 
@@ -73,27 +62,59 @@ namespace Utah.Udot.Atspm.EventLogUtility.Commands
 
         public LocationExcludeCommandOption ExcludeOption { get; set; } = new();
 
-        public LocationTypeCommandOption TypeOption { get; set; } = new();
+        public LocationAreaCommandOption AreaOption { get; set; } = new();
+
+        public LocationJurisdictionCommandOption JurisdictionOption { get; set; } = new();
+
+        public LocationRegionCommandOption RegionOption { get; set; } = new();
+
+        public LocationTypeCommandOption LocationTypeOption { get; set; } = new();
+
+        public DeviceTypeCommandOption DeviceTypeOption { get; set; } = new();
+
+        public TransportProtocolCommandOption TransportProtocolOption { get; set; } = new();
 
         public PathCommandOption PathCommandOption { get; set; } = new();
 
-        public ModelBinder<EventLogLoggingConfiguration> GetOptionsBinder()
+        public ModelBinder<DeviceEventLoggingConfiguration> GetOptionsBinder()
         {
-            var binder = new ModelBinder<EventLogLoggingConfiguration>();
+            var binder = new ModelBinder<DeviceEventLoggingConfiguration>();
 
-            binder.BindMemberFromValue(b => b.Included, IncludeOption);
-            binder.BindMemberFromValue(b => b.Excluded, ExcludeOption);
-            binder.BindMemberFromValue(b => b.ControllerTypes, TypeOption);
+            binder.BindMemberFromValue(b => b.IncludedLocations, IncludeOption);
+            binder.BindMemberFromValue(b => b.ExcludedLocations, ExcludeOption);
+            binder.BindMemberFromValue(b => b.IncludedAreas, AreaOption);
+            binder.BindMemberFromValue(b => b.IncludedJurisdictions, JurisdictionOption);
+            binder.BindMemberFromValue(b => b.IncludedRegions, RegionOption);
+            binder.BindMemberFromValue(b => b.IncludedLocationTypes, LocationTypeOption);
+            binder.BindMemberFromValue(b => b.DeviceType, DeviceTypeOption);
+            binder.BindMemberFromValue(b => b.TransportProtocol, TransportProtocolOption);
             binder.BindMemberFromValue(b => b.Path, PathCommandOption);
 
             return binder;
         }
 
-        public void BindCommandOptions(IServiceCollection services)
+        public void BindCommandOptions(HostBuilderContext host, IServiceCollection services)
         {
             services.AddSingleton(GetOptionsBinder());
-            services.AddOptions<EventLogLoggingConfiguration>().BindCommandLine();
+            services.Configure<DeviceEventLoggingConfiguration>(host.Configuration.GetSection(nameof(DeviceEventLoggingConfiguration)));
+            services.AddOptions<DeviceEventLoggingConfiguration>().BindCommandLine();
             services.AddHostedService<LocationLoggerUtilityHostedService>();
+        }
+    }
+
+    public class DeviceTypeCommandOption : Option<DeviceTypes>
+    {
+        public DeviceTypeCommandOption() : base("--device-type", "Device type to include") 
+        {
+            AddAlias("-dt");
+        }
+    }
+
+    public class TransportProtocolCommandOption : Option<TransportProtocols>
+    {
+        public TransportProtocolCommandOption() : base("--transport-protocol", "Device transport protocol to include")
+        {
+            AddAlias("-tp");
         }
     }
 }
--- Atspm/Infrastructure/Services/HostedServices/SignalLoggerUtilityHostedService.cs
+++ Atspm/Infrastructure/Services/HostedServices/SignalLoggerUtilityHostedService.cs
@@ -19,6 +19,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.Diagnostics;
+using System.Threading.Tasks.Dataflow;
+using Utah.Udot.Atspm.Data.Enums;
+using Utah.Udot.Atspm.Data.Models.EventLogModels;
 
 namespace Utah.Udot.Atspm.Infrastructure.Services.HostedServices
 {
@@ -26,71 +30,200 @@ namespace Utah.Udot.Atspm.Infrastructure.Services.HostedServices
     {
         private readonly ILogger _log;
         private readonly IServiceProvider _serviceProvider;
-        private readonly IOptions<EventLogLoggingConfiguration> _options;
+        private readonly IOptions<DeviceEventLoggingConfiguration> _options;
 
-        public LocationLoggerUtilityHostedService(ILogger<LocationLoggerUtilityHostedService> log, IServiceProvider serviceProvider, IOptions<EventLogLoggingConfiguration> options) =>
+        public LocationLoggerUtilityHostedService(ILogger<LocationLoggerUtilityHostedService> log, IServiceProvider serviceProvider, IOptions<DeviceEventLoggingConfiguration> options) =>
             (_log, _serviceProvider, _options) = (log, serviceProvider, options);
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            try
-            {
-                _log.LogInformation("Log Path: {path}", _options.Value.Path);
+            _log.LogInformation("*********************************Starting Service*********************************");
 
-                using (var scope = _serviceProvider.CreateAsyncScope())
+            _serviceProvider.PrintHostInformation();
+
+
+            //foreach (var d in _options.Value.IncludedLocations)
+            //{
+            //    _log.LogInformation("Including Event Logs for locations: {location}", d);
+            //}
+
+            //foreach (var d in _options.Value.ExcludedLocations)
+            //{
+            //    _log.LogInformation("Excluding Event Logs for locations: {location}", d);
+            //}
+
+            //foreach (var d in _options.Value.IncludedAreas)
+            //{
+            //    _log.LogInformation("Including Event Logs for area: {area}", d);
+            //}
+
+            //foreach (var d in _options.Value.IncludedJurisdictions)
+            //{
+            //    _log.LogInformation("Including Event Logs for jurisdiction: {jurisdiction}", d);
+            //}
+
+            //foreach (var d in _options.Value.IncludedRegions)
+            //{
+            //    _log.LogInformation("Including Event Logs for region: {region}", d);
+            //}
+
+            //foreach (var d in _options.Value.IncludedLocationTypes)
+            //{
+            //    _log.LogInformation("Including Event Logs for location type: {locationtype}", d);
+            //}
+
+            //_log.LogInformation("Filtering event logs by device type: {devicetype}", _options.Value.DeviceType);
+
+            //_log.LogInformation("Filtering event logs by transport protocol: {protocol}", _options.Value.TransportProtocol);
+
+            //Console.WriteLine("*********************************Starting Service*********************************");
+
+
+
+
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var query = scope.ServiceProvider.GetService<IDeviceRepository>().GetActiveDevicesByAllLatestLocations().AsQueryable();
+
+                if (_options.Value.IncludedLocations?.Count() > 0)
                 {
-                    var LocationRepository = scope.ServiceProvider.GetService<ILocationRepository>();
-                    var controllerLoggingService = scope.ServiceProvider.GetService<ILocationControllerLoggerService>();
-
-                    var LocationQuery = LocationRepository.GetLatestVersionOfAllLocations().Where(s => s.ChartEnabled);
-
-                    //if (_options.Value.ControllerTypes != null)
-                    //{
-                    //    foreach (var s in _options.Value.ControllerTypes)
-                    //    {
-                    //        _log.LogInformation("Including Event Logs for Types(s): {type}", s);
-                    //    }
-
-                    //    LocationQuery = LocationQuery.Where(i => _options.Value.ControllerTypes.Any(d => i.ControllerTypeId == d));
-                    //}
-
-                    if (_options.Value.Included != null)
+                    foreach (var d in _options.Value.IncludedLocations)
                     {
-                        foreach (var s in _options.Value.Included)
-                        {
-                            _log.LogInformation("Including Event Logs for Location(s): {Location}", s);
-                        }
-
-                        LocationQuery = LocationQuery.Where(i => _options.Value.Included.Any(d => i.LocationIdentifier == d));
+                        _log.LogInformation("Including Event Logs for locations: {location}", d);
                     }
 
-                    if (_options.Value.Excluded != null)
-                    {
-                        foreach (var s in _options.Value.Excluded)
-                        {
-                            _log.LogInformation("Excluding Event Logs for Location(s): {Location}", s);
-                        }
-
-                        LocationQuery = LocationQuery.Where(i => !_options.Value.Excluded.Contains(i.LocationIdentifier));
-                    }
-
-                    var Locations = LocationQuery.ToList();
-
-                    _log.LogInformation("Number of Locations to Process: {count}", Locations.Count);
-
-                    await controllerLoggingService.ExecuteAsync(Locations, cancellationToken);
+                    query = query.Where(i => _options.Value.IncludedLocations.Any(d => i.Location.LocationIdentifier == d));
                 }
-            }
-            catch (Exception e)
-            {
-                _log.LogError("Exception: {e}", e);
+
+                if (_options.Value.ExcludedLocations?.Count() > 0)
+                {
+                    foreach (var d in _options.Value.ExcludedLocations)
+                    {
+                        _log.LogInformation("Excluding Event Logs for locations: {location}", d);
+                    }
+
+                    query = query.Where(i => !_options.Value.ExcludedLocations.Any(d => i.Location.LocationIdentifier == d));
+                }
+
+                if (_options.Value.IncludedAreas?.Count() > 0)
+                {
+                    foreach (var d in _options.Value.IncludedAreas)
+                    {
+                        _log.LogInformation("Including Event Logs for area: {area}", d);
+                    }
+
+                    query = query.Where(i => _options.Value.IncludedAreas.Any(d => i.Location.Areas.Any(a => a.Name == d)));
+                }
+
+                if (_options.Value.IncludedJurisdictions?.Count() > 0)
+                {
+                    foreach (var d in _options.Value.IncludedJurisdictions)
+                    {
+                        _log.LogInformation("Including Event Logs for jurisdiction: {jurisdiction}", d);
+                    }
+
+                    query = query.Where(i => _options.Value.IncludedJurisdictions.Any(d => i.Location.Jurisdiction.Name == d));
+                }
+
+                if (_options.Value.IncludedRegions?.Count() > 0)
+                {
+                    foreach (var d in _options.Value.IncludedRegions)
+                    {
+                        _log.LogInformation("Including Event Logs for region: {region}", d);
+                    }
+
+                    query = query.Where(i => _options.Value.IncludedRegions.Any(d => i.Location.Region.Description == d));
+                }
+
+                if (_options.Value.IncludedLocationTypes?.Count() > 0)
+                {
+                    foreach (var d in _options.Value.IncludedLocationTypes)
+                    {
+                        _log.LogInformation("Including Event Logs for location type: {locationtype}", d);
+                    }
+
+                    query = query.Where(i => _options.Value.IncludedLocationTypes.Any(d => i.Location.LocationType.Name == d));
+                }
+
+                if (_options.Value.DeviceType != DeviceTypes.Unknown)
+                {
+                    _log.LogInformation("Filtering event logs by device type: {devicetype}", _options.Value.DeviceType);
+
+                    query = query.Where(i => i.DeviceType == _options.Value.DeviceType);
+                }
+
+                if (_options.Value.TransportProtocol != TransportProtocols.Unknown)
+                {
+                    _log.LogInformation("Filtering event logs by transport protocol: {protocol}", _options.Value.TransportProtocol);
+
+                    query = query.Where(i => i.DeviceConfiguration.Protocol == _options.Value.TransportProtocol);
+                }
+
+                var devices = query.ToList();
+
+                Console.WriteLine($"devices: {devices.Count()}");
+
+                foreach (var d in devices)
+                {
+                    Console.WriteLine($"device: {d}");
+                }
+
+
+
+
+
+
+
+
+
+
+
+                //var input = new BufferBlock<Device>();
+
+                //var downloadStep = new DownloadDeviceData(_serviceProvider, new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = _options.Value.MaxDegreeOfParallelism, CancellationToken = cancellationToken });
+                //var processEventLogFileWorkflow = new ProcessEventLogFileWorkflow<IndianaEvent>(_serviceProvider, _options.Value.SaveToDatabaseBatchSize, _options.Value.MaxDegreeOfParallelism);
+
+                //var actionResult = new ActionBlock<Tuple<Device, FileInfo>>(t =>
+                //{
+                //    //Console.WriteLine($"{t.Item1} - {t.Item2.FullName}");
+                //}, new ExecutionDataflowBlockOptions() { MaxDegreeOfParallelism = _options.Value.MaxDegreeOfParallelism, CancellationToken = cancellationToken });
+
+                //input.LinkTo(downloadStep, new DataflowLinkOptions() { PropagateCompletion = true });
+
+                //await Task.Delay(TimeSpan.FromSeconds(1));
+
+                //downloadStep.LinkTo(actionResult, new DataflowLinkOptions() { PropagateCompletion = true });
+
+                //foreach (var d in devices)
+                //{
+                //    input.Post(d);
+                //}
+
+                //input.Complete();
+
+                //try
+                //{
+                //    await actionResult.Completion.ContinueWith(t => Console.WriteLine($"!!!Task actionResult is complete!!! {t.Status}"), cancellationToken);
+                //}
+                //catch (Exception e)
+                //{
+                //    Console.WriteLine($"{actionResult.Completion.Status}---------------{e}");
+                //}
+
+                sw.Stop();
+
+                Console.WriteLine($"*********************************************complete - {sw.Elapsed}");
             }
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            Console.WriteLine();
-            Console.WriteLine($"Operation Completed or Cancelled...");
+            _log.LogInformation("*********************************Stopping Service*********************************");
+            cancellationToken.Register(() => _log.LogInformation("StopAsync has been cancelled"));
 
             return Task.CompletedTask;
         }
