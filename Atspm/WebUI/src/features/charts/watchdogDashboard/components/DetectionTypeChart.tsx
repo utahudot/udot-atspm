01e9b4a723d4ee731bcb39dac0b7b31d40f03652
dkimball <dkimball@avenueconsultants.com>
2024-09-06 16:15:34 -0600

    slight styling change

--- Atspm/WebUI/src/features/charts/watchdogDashboard/components/DetectionTypeChart.tsx
+++ Atspm/WebUI/src/features/charts/watchdogDashboard/components/DetectionTypeChart.tsx
@@ -1,17 +1,15 @@
+import React, { useMemo } from 'react'
 import ApacheEChart from '@/features/charts/components/apacheEChart/ApacheEChart'
 import transformDetectionTypeData from '@/features/charts/watchdogDashboard/watchDogDetectionType.transformer'
 import { Paper } from '@mui/material'
-import React, { useMemo } from 'react'
 
 interface DetectionTypeChartProps {
   data: any // Replace 'any' with the correct type for your data
   isLoading: boolean
 }
 
-const DetectionTypeChart: React.FC<DetectionTypeChartProps> = ({
-  data,
-  isLoading,
-}) => {
+const DetectionTypeChart: React.FC<DetectionTypeChartProps> = ({ data, isLoading }) => {
+  
   const detectionTypeChartOption = useMemo(() => {
     if (data) {
       return transformDetectionTypeData(data)
@@ -20,7 +18,7 @@ const DetectionTypeChart: React.FC<DetectionTypeChartProps> = ({
   }, [data])
 
   return (
-    <Paper elevation={3} sx={{ height: 600 }}>
+    <Paper elevation={3} sx={{ height: 650, padding:1 }}>
       <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
         <h2>Detection Type Breakdown</h2>
       </div>
@@ -29,7 +27,7 @@ const DetectionTypeChart: React.FC<DetectionTypeChartProps> = ({
           id="watchdog-detection-type-chart"
           option={detectionTypeChartOption}
           loading={isLoading}
-          style={{ width: '100%', height: 'calc(100% - 40px)' }}
+          style={{ width: '100%', height: 'calc(100% - 40px)' }} 
           hideInteractionMessage
         />
       )}
