f2f568b496d75f7a0f75fe77e278a51ae8a0406a
kford <kford@avenueconsultants.com>
2024-09-27 15:24:16 -0600

    AVE-1934 - Monthly aggregation fixes
    Adding in new fields

---
+++ Atspm/Data/Models/SpeedManagementModels/MonthlyAggregation/MonthlyAggregationSimplified.cs
@@ -0,0 +1,35 @@
+﻿namespace Utah.Udot.Atspm.Data.Models.SpeedManagementModels.MonthlyAggregation;
+
+public class MonthlyAggregationSimplified
+{
+    public double? AverageSpeed { get; set; }
+    public double? AverageEightyFifthSpeed { get; set; }
+    public long? Violations { get; set; }
+    public long? ExtremeViolations { get; set; }
+    public long? Flow { get; set; }
+    public double? MinSpeed { get; set; }
+    public double? MaxSpeed { get; set; }
+    public double? Variability { get; set; }
+    public double? PercentViolations { get; set; }
+    public double? PercentExtremeViolations { get; set; }
+    public double? AvgSpeedVsSpeedLimit { get; set; }
+    public double? EightyFifthSpeedVsSpeedLimit { get; set; }
+}
+
+public enum TimePeriod
+{
+    AllDay,
+    OffPeak,
+    AmPeak,
+    PmPeak,
+    MidDay,
+    Evening,
+    EarlyMorning
+}
+
+public enum DayType
+{
+    Total,
+    Weekend,
+    Weekday
+}
