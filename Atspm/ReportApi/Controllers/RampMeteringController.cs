112bdf1d3698879c8d16a10e411f143345705785
Jaithra <jbhatia@avenueconsultants.com>
2024-10-15 15:20:49 -0600

    Get most of the api call done

---
+++ Atspm/ReportApi/Controllers/RampMeteringController.cs
@@ -0,0 +1,17 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Utah.Udot.Atspm.Business.RampMetering;
+using Utah.Udot.Atspm.ReportApi.Controllers;
+
+namespace Utah.Udot.ATSPM.ReportApi.Controllers
+{
+    [ApiVersion(1.0)]
+    [ApiController]
+    [Route("v{version:apiVersion}/[controller]")]
+    public class RampMeteringController : ReportControllerBase<RampMeteringOptions, RampMeteringResult>
+    {
+        public RampMeteringController(IReportService<RampMeteringOptions, RampMeteringResult> reportService, ILogger<RampMeteringController> logger) : base(reportService, logger)
+        {
+        }
+    }
+}
---
+++ Atspm/ReportApi/ReportServices/RampMeteringReportService.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Utah.Udot.Atspm.Business.RampMetering;
+
+namespace Utah.Udot.ATSPM.ReportApi.ReportServices
+{
+    public class RampMeteringReportService : ReportServiceBase<RampMeteringOptions, RampMeteringResult>
+    {
+        private readonly IIndianaEventLogRepository controllerEventLogRepository;
+        private readonly ILocationRepository LocationRepository;
+
+        public RampMeteringReportService(IIndianaEventLogRepository controllerEventLogRepository, ILocationRepository locationRepository)
+        {
+            this.controllerEventLogRepository = controllerEventLogRepository;
+            LocationRepository = locationRepository;
+        }
+
+        public override async Task<RampMeteringResult> ExecuteAsync(RampMeteringOptions parameter, IProgress<int> progress = null, CancellationToken cancelToken = default)
+        {
+            var location = LocationRepository.GetLatestVersionOfLocation(parameter.LocationIdentifier, parameter.Start);
+            if (location == null)
+            {
+                //return BadRequest("Location not found");
+                return await Task.FromException<RampMeteringResult>(new NullReferenceException("Location not found"));
+            }
+            var controllerEventLogs = controllerEventLogRepository.GetEventsBetweenDates(location.LocationIdentifier, parameter.Start.AddHours(-12), parameter.End.AddHours(12)).ToList();
+            if (controllerEventLogs.IsNullOrEmpty())
+            {
+                //return Ok("No Controller Event Logs found for Location");
+                return await Task.FromException<RampMeteringResult>(new NullReferenceException("No Controller Event Logs found for Location"));
+            }
+
+            var mainlineAvgSpeedEventsCodes = new List<int>() { 1371 };
+            var mainlineAvgOccurrenceEventsCodes = new List<int>() { 1372 };
+            var mainlineAvgFlowEventsCodes = new List<int>() { 1373 };
+
+            //codes for active rate 1058-1073
+            //codes for base rate 1042-1057
+            // codes for start up warning 1004
+            // codes for shut down warning 1014
+            // codes for L1 Queue On 1171
+            // codes for L2 Queue On 1173
+            // codes for L3 Queue On 1175
+            // codes for L1 Queue Off 1170
+            // codes for L2 Queue Off 1172
+            // codes for L3 Queue Off 1174
+
+            var mainlineAvgSpeedEvents = controllerEventLogs.Where(e => mainlineAvgSpeedEventsCodes.Contains(e.EventCode));
+            var mainlineAvgOccurrenceEvents = controllerEventLogs.Where(e => mainlineAvgOccurrenceEventsCodes.Contains(e.EventCode));
+            var mainlineAvgFlowEvents = controllerEventLogs.Where(e => mainlineAvgFlowEventsCodes.Contains(e.EventCode));
+
+            var mainlineAvgSpeeds = mainlineAvgSpeedEvents.Select(e => new DataPointDateDouble(e.Timestamp, e.EventParam)).ToList();
+            var mainlineAvgOccurrence = mainlineAvgOccurrenceEvents.Select(e => new DataPointDateDouble(e.Timestamp, e.EventParam)).ToList();
+            var mainlineAvgFlow = mainlineAvgFlowEvents.Select(e => new DataPointDateDouble(e.Timestamp, e.EventParam)).ToList();
+
+            var planEvents = controllerEventLogs.GetPlanEvents(
+                parameter.Start.AddHours(-12),
+                parameter.End.AddHours(12)).ToList();
+
+            return new RampMeteringResult(location.LocationIdentifier, parameter.Start, parameter.End)
+            {
+                MainLineAvgFlow = mainlineAvgFlow,
+                MainlineAvgOcc = mainlineAvgOccurrence,
+                MainlineAvgSpeed = mainlineAvgSpeeds,
+            };
+        }
+    }
+}
