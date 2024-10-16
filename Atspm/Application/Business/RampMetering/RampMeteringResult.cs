44a35fe0a1556de8f5c317a888e6c0fc1ef5fbfc
avetraffic <96312362+avetraffic@users.noreply.github.com>
2024-10-16 10:59:25 -0600

    Ramp metering (#97)
    * Get most of the api call done
    
    * Continue working for ramp metering
    
    * fix error
    
    ---------
    
    Co-authored-by: Jaithra <jbhatia@avenueconsultants.com>

---
+++ Atspm/Application/Business/RampMetering/RampMeteringResult.cs
@@ -0,0 +1,20 @@
+﻿using Utah.Udot.Atspm.Business.Common;
+
+namespace Utah.Udot.Atspm.Business.RampMetering
+{
+    public class RampMeteringResult : LocationResult
+    {
+        public RampMeteringResult(string locationId, DateTime start, DateTime end): base(locationId, start, end)
+        { }
+
+        public List<DataPointDateDouble> MainLineAvgFlow { get; set; }
+        public List<DataPointDateDouble> MainlineAvgOcc { get; set; }
+        public List<DataPointDateDouble> MainlineAvgSpeed { get; set; }
+        public List<DataPointDateDouble> StartUpWarning { get; set; }
+        public List<DataPointDateDouble> ShutdownWarning { get; set; }
+        public List<DescriptionWithDataPoints> LanesActiveRate { get; set; }
+        public List<DescriptionWithDataPoints> LanesBaseRate { get; set; }
+        public List<DescriptionWithDataPoints> LanesQueueOnEvents { get; set; }
+        public List<DescriptionWithDataPoints> LanesQueueOffEvents { get; set; }
+    }
+}
