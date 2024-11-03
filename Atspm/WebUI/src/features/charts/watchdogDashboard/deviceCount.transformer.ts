702e2bf28c13ee75c5ea1816bf60a119e48cdfbd
dkimball <dkimball@avenueconsultants.com>
2024-11-02 20:15:45 -0600

    fixed up the watchdog summary report - dk

--- Atspm/WebUI/src/features/charts/watchdogDashboard/deviceCount.transformer.ts
+++ Atspm/WebUI/src/features/charts/watchdogDashboard/deviceCount.transformer.ts
@@ -1,28 +1,11 @@
-// #region license
-// Copyright 2024 Utah Departement of Transportation
-// for WebUI - deviceCount.transformer.ts
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//http://www.apache.org/licenses/LICENSE-2.
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// #endregion
 import { DeviceCount } from '@/features/watchdog/types'
 import { EChartsOption } from 'echarts'
 
 const transformDeviceCountData = (data: DeviceCount[]): EChartsOption => {
   const total = data.reduce((sum, item) => sum + item.count, 0)
-
   const seriesData = data.map((item) => ({
     value: item.count,
-    name: `${item.manufacturer}: \n${item.model} - ${item.firmware}`,
+    name: `${item.manufacturer}`,
   }))
 
   return {
