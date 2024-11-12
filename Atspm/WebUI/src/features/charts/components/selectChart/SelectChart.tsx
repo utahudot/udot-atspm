835148e0da70070823589b04a5cfbaa5cdf33df7
Morgan Stanley <mstanley@avenueconsultants.com>
2024-11-12 15:46:45 -0700

    update location config accessibility

--- Atspm/WebUI/src/features/charts/components/selectChart/SelectChart.tsx
+++ Atspm/WebUI/src/features/charts/components/selectChart/SelectChart.tsx
@@ -197,16 +197,19 @@ const SelectChart = ({
   return (
     <>
       <FormControl fullWidth sx={{ mb: 1 }}>
-        <InputLabel htmlFor="measure-type-label">
+        <InputLabel
+          htmlFor="measure-type-select"
+          id="measure-type-select-label"
+        >
           {location ? 'Measure' : 'Please select a location'}
         </InputLabel>
         <Select
-          inputProps={{ id: 'measure-type-label' }}
+          labelId="measure-type-select-label"
+          id="measure-type-select"
           label="Measure"
+          inputProps={{ id: 'measure-type-select' }}
           value={chartType && chartType in availableCharts ? chartType : ''}
           onChange={handleChartTypeChange}
-          placeholder="Please select a location"
-          displayEmpty
           disabled={!location}
         >
           {Object.keys(availableCharts).map((type) => (
