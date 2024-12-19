68086ed9368474eb92a3cfa1cb692dc69332e919
kford <kford@avenueconsultants.com>
2024-12-18 17:00:06 -0700

    AVE-1061 - New Direct Camera Data Feed Integration
    Adding in the needed information to make a new event.

---
+++ Atspm/Data/Models/EventLogModels/CameraEvent.cs
@@ -0,0 +1,164 @@
+﻿#region license
+// Copyright 2024 Utah Departement of Transportation
+// for Data - Utah.Udot.Atspm.Data.Models.EventLogModels/PedestrianCounter.cs
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+#nullable disable
+
+
+namespace Utah.Udot.Atspm.Data.Models.EventLogModels
+{
+
+
+    public class CameraEvent : EventLogModelBase
+    {
+        /// <summary>
+        /// Zone Identifier
+        /// </summary>
+        public long ZoneId { get; set; } //needs converting to location identifier
+
+        /// <summary>
+        /// Name of the Zone
+        /// </summary>
+        public string ZoneName { get; set; }
+
+        /// <summary>
+        /// Object Type (e.g., Vehicle)
+        /// </summary>
+        public string ObjectType { get; set; }
+
+        /// <summary>
+        /// Speed of the object
+        /// </summary>
+        public double Speed { get; set; }
+
+        /// <summary>
+        /// Length of the object
+        /// </summary>
+        public double Length { get; set; }
+
+        /// <summary>
+        /// Direction of the object
+        /// </summary>
+        public string Direction { get; set; }
+
+        ///<inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            return obj is CameraEvent zoneEvent &&
+                   ZoneId == zoneEvent.ZoneId &&
+                   ZoneName == zoneEvent.ZoneName &&
+                   ObjectType == zoneEvent.ObjectType &&
+                   Speed == zoneEvent.Speed &&
+                   Length == zoneEvent.Length &&
+                   Direction == zoneEvent.Direction;
+        }
+
+        ///<inheritdoc/>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ZoneId, ZoneName, Timestamp, ObjectType, Speed, Length, Direction);
+        }
+
+        ///<inheritdoc/>
+        public override string ToString()
+        {
+            return $"{ZoneId}-{ZoneName}-{Timestamp}-{ObjectType}-{Speed}-{Length}-{Direction}";
+        }
+    }
+
+    public class VisionCameraStatisticsEvent : EventLogModelBase
+    {
+        /// <summary>
+        /// Zone Identifier
+        /// </summary>
+        public long ZoneId { get; set; }
+
+        /// <summary>
+        /// Name of the Zone
+        /// </summary>
+        public string ZoneName { get; set; }
+
+        /// <summary>
+        /// Average Speed in the Zone
+        /// </summary>
+        public double AverageSpeed { get; set; }
+
+        /// <summary>
+        /// Total Volume of Vehicles
+        /// </summary>
+        public int Volume { get; set; }
+
+        /// <summary>
+        /// Occupancy Rate
+        /// </summary>
+        public double Occupancy { get; set; }
+
+        /// <summary>
+        /// Number of Vehicles Going Through
+        /// </summary>
+        public int ThroughCount { get; set; }
+
+        /// <summary>
+        /// Number of Right Turns
+        /// </summary>
+        public int RightTurnCount { get; set; }
+
+        /// <summary>
+        /// Number of Left Turns
+        /// </summary>
+        public int LeftTurnCount { get; set; }
+
+        /// <summary>
+        /// Count of Left to Right Turns
+        /// </summary>
+        public int LeftToRightCount { get; set; }
+
+        /// <summary>
+        /// Count of Right to Left Turns
+        /// </summary>
+        public int RightToLeftCount { get; set; }
+
+        ///<inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            return obj is VisionCameraStatisticsEvent trafficEvent &&
+                   ZoneId == trafficEvent.ZoneId &&
+                   ZoneName == trafficEvent.ZoneName &&
+                   Timestamp == trafficEvent.Timestamp &&
+                   AverageSpeed == trafficEvent.AverageSpeed &&
+                   Volume == trafficEvent.Volume &&
+                   Occupancy == trafficEvent.Occupancy &&
+                   ThroughCount == trafficEvent.ThroughCount &&
+                   RightTurnCount == trafficEvent.RightTurnCount &&
+                   LeftTurnCount == trafficEvent.LeftTurnCount &&
+                   LeftToRightCount == trafficEvent.LeftToRightCount &&
+                   RightToLeftCount == trafficEvent.RightToLeftCount;
+        }
+
+        ///<inheritdoc/>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ZoneId, ZoneName, Timestamp, AverageSpeed, Volume, Occupancy);
+        }
+
+        ///<inheritdoc/>
+        public override string ToString()
+        {
+            return $"{ZoneId}-{ZoneName}-{Timestamp}-{AverageSpeed}-{Volume}-{Occupancy}";
+        }
+    }
+
+}
