11bb1f7b07040b9dec5fd6b83cd029960652ecf2
dkimball <dkimball@avenueconsultants.com>
2024-12-26 15:42:16 -0700

    Maplayer bug fixes

--- Atspm/WebUI/src/pages/admin/map-layers/index.tsx
+++ Atspm/WebUI/src/pages/admin/map-layers/index.tsx
@@ -5,30 +5,24 @@ import {
   usePostMapLayer,
 } from '@/api/config/aTSPMConfigurationApi'
 import { MapLayer } from '@/api/config/aTSPMConfigurationApi.schemas'
-import GenericAdminChart, {
-  pageNameToHeaders,
-} from '@/components/GenericAdminChart'
-import MapLayerCreateEditModal from '@/components/GenericAdminChart/MapLayerCreateEditModal'
+import AdminTable from '@/components/AdminTable/AdminTable'
+import DeleteModal from '@/components/AdminTable/DeleteModal'
 import { ResponsivePageLayout } from '@/components/ResponsivePage'
 import {
   PageNames,
   useUserHasClaim,
   useViewPage,
 } from '@/features/identity/pagesCheck'
-import { TabContext, TabList, TabPanel } from '@mui/lab'
-import { Backdrop, CircularProgress, Tab } from '@mui/material'
-import { GridColDef } from '@mui/x-data-grid'
-import { useState } from 'react'
+import MapLayerCreateEditModal from '@/features/mapLayer/components/MapLayerCreateEditModal'
+import { Backdrop, CircularProgress } from '@mui/material'
 
 const MapLayers = () => {
   const pageAccess = useViewPage(PageNames.MapLayers)
-  const [currentTab, setCurrentTab] = useState('1')
-  const headers: GridColDef[] = pageNameToHeaders.get(
-    PageNames.MapLayers
-  ) as GridColDef[]
 
   const hasLocationsEditClaim = useUserHasClaim('LocationConfiguration:Edit')
-  const hasLocationsDelteClaim = useUserHasClaim('LocationConfiguration:Delete')
+  const hasLocationsDeleteClaim = useUserHasClaim(
+    'LocationConfiguration:Delete'
+  )
 
   const {
     data: mapLayerData,
@@ -45,27 +39,19 @@ const MapLayers = () => {
     return
   }
 
-  const handleChange = (_: React.SyntheticEvent, newValue: string) => {
-    setCurrentTab(newValue)
-  }
-
   const handleCreateMapLayer = async (mapLayerData: MapLayer) => {
-    delete mapLayerData.id
     try {
       await addMapLayer({ data: mapLayerData })
+      fetchMapLayers()
     } catch (error) {
       console.error('Mutation Error:', error)
     }
   }
 
-  const handleDeleteMapLayer = async (mapLayerData: MapLayer) => {
-    const { id } = mapLayerData
-    if (!id) {
-      console.error('Map Layer ID is required for deletion')
-      return
-    }
+  const handleDeleteMapLayer = async (id: number) => {
     try {
       await removeMapLayer({ key: id })
+      fetchMapLayers()
     } catch (error) {
       console.error('Mutation Error:', error)
     }
@@ -77,20 +63,18 @@ const MapLayers = () => {
       return
     }
     try {
-      await updateMapLayer(
-        {
-          key: mapLayerData.id,
-          data: mapLayerData,
-        },
-        {
-          onSuccess: () => {
-            fetchMapLayers()
-          },
-        }
-      )
+      await updateMapLayer({
+        key: mapLayerData.id,
+        data: mapLayerData,
+      })
+      fetchMapLayers()
     } catch (error) {
       console.error('Mutation Error:', error)
     }
+  }
+
+  const onModalClose = () => {
+    //do something?? potentially just delete
   }
 
   if (isLoading) {
@@ -105,45 +89,60 @@ const MapLayers = () => {
     return <div>Error returning data</div>
   }
 
-  const baseType = {
-    name: '',
-    mapLayerUrl: '',
-    showByDefault: '',
-    serviceType: '',
-  }
+  const filteredData = mapLayers.map((obj: MapLayer) => {
+    return {
+      id: obj.id,
+      name: obj.name,
+      mapLayerUrl: obj.mapLayerUrl,
+      showByDefault: obj.showByDefault,
+      serviceType: obj.serviceType,
+    }
+  })
 
+  const headers = ['Name', 'Map Layer Url', 'Show By Default', 'Service Type']
+  const headerKeys = ['name', 'mapLayerUrl', 'showByDefault', 'serviceType']
+
+  const customCellRender = [
+    {
+      headerKey: 'showByDefault',
+      component: (value: any) => <>{value ? 'true' : 'false'}</>,
+    },
+  ]
   return (
-    <ResponsivePageLayout title={'General Admin'} noBottomMargin>
-      <TabContext value={currentTab}>
-        <TabList
-          onChange={handleChange}
-          aria-label="Maps"
-          textColor="primary"
-          indicatorColor="primary"
-        >
-          <Tab label="Map" value="1" />
-        </TabList>
-        <TabPanel value="1" sx={{ padding: '0px' }}>
-          <GenericAdminChart
-            pageName={PageNames.MapLayers}
-            headers={headers}
-            data={mapLayers}
-            baseRowType={baseType}
-            onDelete={handleDeleteMapLayer}
-            onEdit={handleEditMapLayer}
-            onCreate={handleCreateMapLayer}
-            hasEditPrivileges={hasLocationsEditClaim}
-            hasDeletePrivileges={hasLocationsDelteClaim}
-            customModal={
-              <MapLayerCreateEditModal
-                onCreate={handleCreateMapLayer}
-                onEdit={handleEditMapLayer}
-                onDelete={handleDeleteMapLayer}
-              />
-            }
+    <ResponsivePageLayout title="Manage Map Layers" noBottomMargin>
+      <AdminTable
+        pageName="Map Layer"
+        headers={headers}
+        headerKeys={headerKeys}
+        data={filteredData}
+        customCellRender={customCellRender}
+        hasEditPrivileges={hasLocationsEditClaim}
+        hasDeletePrivileges={hasLocationsDeleteClaim}
+        editModal={
+          <MapLayerCreateEditModal
+            isOpen={true}
+            onSave={handleEditMapLayer}
+            onClose={onModalClose}
           />
-        </TabPanel>
-      </TabContext>
+        }
+        createModal={
+          <MapLayerCreateEditModal
+            isOpen={true}
+            onSave={handleCreateMapLayer}
+            onClose={onModalClose}
+          />
+        }
+        deleteModal={
+          <DeleteModal
+            id={0}
+            name={''}
+            objectType="Map Layer"
+            open={false}
+            onClose={onModalClose}
+            onConfirm={handleDeleteMapLayer}
+          />
+        }
+      />
     </ResponsivePageLayout>
   )
 }
