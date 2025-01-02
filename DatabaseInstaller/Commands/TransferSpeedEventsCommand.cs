e11f433b922ec1f2c506b7f26f672595df720ed8
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2025-01-02 08:51:06 -0700

    Adding speed events transfer command.

---
+++ DatabaseInstaller/Commands/TransferSpeedEventsCommand.cs
@@ -0,0 +1,58 @@
+﻿#region license
+// Copyright 2024 Utah Departement of Transportation
+// for DatabaseInstaller - DatabaseInstaller.Commands/MoveEventLogsSqlServerToPostgresCommand.cs
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using DatabaseInstaller.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.CommandLine;
+using System.CommandLine.Hosting;
+using System.CommandLine.NamingConventionBinder;
+
+namespace DatabaseInstaller.Commands
+{
+    public class TransferSpeedEventsCommand : Command, ICommandOption<TransferCommandConfiguration>
+    {
+        public TransferSpeedEventsCommand() : base("transfer-speed", "Transfer speed events from 4.3 to 5.0")
+        {
+            AddOption(SourceOption);
+            AddOption(StartOption);
+            AddOption(EndOption);
+        }
+
+        public Option<string> SourceOption { get; set; } = new("--source", "Connection string for the source SQL Server");
+        public Option<DateTime> StartOption { get; set; } = new("--start", "Start date");
+        public Option<DateTime> EndOption { get; set; } = new("--end", "End date");
+
+        public ModelBinder<TransferCommandConfiguration> GetOptionsBinder()
+        {
+            var binder = new ModelBinder<TransferCommandConfiguration>();
+
+            binder.BindMemberFromValue(b => b.Source, SourceOption);
+            binder.BindMemberFromValue(b => b.Start, StartOption);
+            binder.BindMemberFromValue(b => b .End, EndOption);
+
+            return binder;
+        }
+
+        public void BindCommandOptions(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton(GetOptionsBinder());
+            services.AddOptions<TransferCommandConfiguration>().BindCommandLine();
+            services.AddHostedService<TransferEventLogsHostedService>();
+        }
+    }
+}
