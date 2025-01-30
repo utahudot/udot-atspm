b9e24ec946e22dbffcd9e2a650ce53cf13863116
dkimball <dkimball@avenueconsultants.com>
2025-01-30 16:53:11 -0700

    AVE-2427 Add New "RefreshRate" Column to Map Layer Table

--- Atspm/PostgreSQLDatabaseProvider/Migrations/Config/20250130230507_V05_0_3.Designer.cs
+++ Atspm/PostgreSQLDatabaseProvider/Migrations/Config/20250130230507_V05_0_3.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Utah.Udot.Atspm.Data;
 
 #nullable disable
 
-namespace Utah.Udot.ATSPM.PostgreSQLDatabaseProvider.Migrations
+namespace Utah.Udot.ATSPM.PostgreSQLDatabaseProvider.Migrations.Config
 {
     [DbContext(typeof(ConfigContext))]
-    partial class ConfigContextModelSnapshot : ModelSnapshot
+    [Migration("20250130230507_V05_0_3")]
+    partial class V05_0_3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
