dd6894416d3a4a43e2106c86a02f8b24de8faba0
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2025-02-12 12:28:09 -0700

    AVE-2490 Create API endpoint that pulls TSP data

---
+++ Atspm/Application/Business/TransitSignalPriority/TransitSignalPriorityService.cs
@@ -0,0 +1,56 @@
+﻿#region license
+// Copyright 2024 Utah Departement of Transportation
+// for Application - Utah.Udot.Atspm.Business.PreemptServiceRequest/PreemptServiceRequestService.cs
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using Utah.Udot.Atspm.Business.Common;
+using Utah.Udot.Atspm.Business.PreemptService;
+using Utah.Udot.Atspm.Business.TransitSignalPriority;
+using Utah.Udot.Atspm.Data.Models.EventLogModels;
+
+namespace Utah.Udot.Atspm.Business.TransitSignalPriorityRequest
+{
+    public class TransitSignalPriorityService
+    {
+        private readonly PlanService planService;
+
+        public TransitSignalPriorityService(PlanService planService)
+        {
+            this.planService = planService;
+        }
+
+        public TransitSignalPriorityResult GetChartData(
+            TransitSignalPriorityOptions options,
+            IReadOnlyList<IndianaEvent> planEvents,
+            IReadOnlyList<IndianaEvent> events)
+        {
+            var preemptEvents = events.Where(row => row.EventCode == 102).Select(row => new DataPointForInt(row.Timestamp, row.EventParam));
+            var plans = planService.GetBasicPlans(options.Start, options.End, options.LocationIdentifier, planEvents);
+            IReadOnlyList<Plan> preemptPlans = plans.Select(pl => new PreemptPlan(
+                pl.PlanNumber.ToString(),
+                pl.Start,
+                pl.End,
+                preemptEvents.Count(p => p.Timestamp >= pl.Start && p.Timestamp < pl.End))).ToList();
+            return new TransitSignalPriorityResult(
+                "Preempt Service",
+                options.LocationIdentifier,
+                options.Start,
+                options.End,
+                preemptPlans,
+                preemptEvents.ToList()
+                );
+        }
+    }
+}
