07b4fe9ca608eb5075904c09a13f4f7f3f31aba5
Morgan Stanley <mstanley@avenueconsultants.com>
2025-02-13 21:11:42 -0700

    update types

--- Atspm/WebUI/src/components/MultipleLocationsSelect/MultipleLocationsDisplay.tsx
+++ Atspm/WebUI/src/components/MultipleLocationsSelect/MultipleLocationsDisplay.tsx
@@ -1,4 +1,4 @@
-import { Location } from '@/api/config/aTSPMConfigurationApi.schemas'
+import { SearchLocation as Location } from '@/api/config/aTSPMConfigurationApi.schemas'
 import {
   DragDropContext,
   Draggable,
