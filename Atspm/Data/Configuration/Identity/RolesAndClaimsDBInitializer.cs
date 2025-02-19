833fb8a540d67f79008c7791b29985e66f3653c5
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2025-02-19 16:04:15 -0700

    Fixes for update command

--- Atspm/Data/Configuration/Identity/RolesAndClaimsDBInitializer.cs
+++ Atspm/Data/Configuration/Identity/RolesAndClaimsDBInitializer.cs
@@ -33,29 +33,31 @@ namespace Utah.Udot.Atspm.Data.Configuration.Identity
         }
         public static async Task SeedRolesAndClaims(IServiceProvider serviceProvider, string connectionString)
         {
-
+            // Seed roles
             using (var scope = serviceProvider.CreateScope())
             {
                 var logger = scope.ServiceProvider.GetRequiredService<ILogger<RolesAndClaimsDBInitializer>>();
-
                 var identityContext = scope.ServiceProvider.GetRequiredService<IdentityContext>();
-                // Set the connection string dynamically
-                identityContext.Database.GetDbConnection().ConnectionString = connectionString;
+
+                // If a connection string is provided, update the context's connection string.
+                if (!string.IsNullOrEmpty(connectionString))
+                {
+                    identityContext.Database.GetDbConnection().ConnectionString = connectionString;
+                }
+
                 var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
-
-
-                logger.LogInformation("Starting role and claim seeding process.");
+                logger.LogInformation("Starting role seeding process.");
 
                 if (!roleManager.Roles.Any())
                 {
                     // Create roles
                     var roles = new List<string>
-                    {
-                        "Admin", "UserAdmin", "RoleAdmin",
-                        "LocationConfigurationAdmin", "GeneralConfigurationAdmin",
-                        "DataAdmin", "WatchdogSubscriber", "ReportAdmin"
-                    };
+            {
+                "Admin", "UserAdmin", "RoleAdmin",
+                "LocationConfigurationAdmin", "GeneralConfigurationAdmin",
+                "DataAdmin", "WatchdogSubscriber", "ReportAdmin"
+            };
 
                     foreach (var role in roles)
                     {
@@ -76,78 +78,80 @@ namespace Utah.Udot.Atspm.Data.Configuration.Identity
                 }
             }
 
+            // Seed claims for roles
             using (var claimScope = serviceProvider.CreateScope())
             {
                 var logger = claimScope.ServiceProvider.GetRequiredService<ILogger<RolesAndClaimsDBInitializer>>();
-
                 var identityContext = claimScope.ServiceProvider.GetRequiredService<IdentityContext>();
-                // Set the connection string dynamically
-                identityContext.Database.GetDbConnection().ConnectionString = connectionString;
-                var roleManager = claimScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-                // Setup claims
-                var claims = new Dictionary<string, List<Claim>>
-                {
-                    { "UserAdmin", new List<Claim>
-                        {
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "User:View"),
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "User:Edit"),
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "User:Delete")
-                        }
-                    },
-                    { "Admin", new List<Claim>
-                        {
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Admin")
-                        }
-                    },
-                    { "RoleAdmin", new List<Claim>
-                        {
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Role:View"),
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Role:Edit"),
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Role:Delete")
-                        }
-                    },
-                    { "LocationConfigurationAdmin", new List<Claim>
-                        {
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "LocationConfiguration:View"),
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "LocationConfiguration:Edit"),
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "LocationConfiguration:Delete")
-                        }
-                    },
-                    { "GeneralConfigurationAdmin", new List<Claim>
-                        {
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "GeneralConfiguration:View"),
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "GeneralConfiguration:Edit"),
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "GeneralConfiguration:Delete")
-                        }
-                    },
-                    { "DataAdmin", new List<Claim>
-                        {
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Data:View"),
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Data:Edit")
-                        }
-                    },
-                    { "WatchdogSubscriber", new List<Claim>
-                        {
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Watchdog:View"),
-                            new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Report:View")
-                        }
-                    }
-                };
 
-                // Loop through roles and add claims, using separate scopes for each role
+                // Update connection string if provided.
+                if (!string.IsNullOrEmpty(connectionString))
+                {
+                    identityContext.Database.GetDbConnection().ConnectionString = connectionString;
+                }
+
+                var roleManager = claimScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                // Define claims for each role
+                var claims = new Dictionary<string, List<Claim>>
+        {
+            { "UserAdmin", new List<Claim>
+                {
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "User:View"),
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "User:Edit"),
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "User:Delete")
+                }
+            },
+            { "Admin", new List<Claim>
+                {
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Admin")
+                }
+            },
+            { "RoleAdmin", new List<Claim>
+                {
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Role:View"),
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Role:Edit"),
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Role:Delete")
+                }
+            },
+            { "LocationConfigurationAdmin", new List<Claim>
+                {
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "LocationConfiguration:View"),
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "LocationConfiguration:Edit"),
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "LocationConfiguration:Delete")
+                }
+            },
+            { "GeneralConfigurationAdmin", new List<Claim>
+                {
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "GeneralConfiguration:View"),
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "GeneralConfiguration:Edit"),
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "GeneralConfiguration:Delete")
+                }
+            },
+            { "DataAdmin", new List<Claim>
+                {
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Data:View"),
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Data:Edit")
+                }
+            },
+            { "WatchdogSubscriber", new List<Claim>
+                {
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Watchdog:View"),
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Report:View")
+                }
+            }
+        };
+
+                // Loop through each role and add claims.
                 foreach (var roleClaims in claims)
                 {
-                    // Set the connection string dynamically
-                    identityContext.Database.GetDbConnection().ConnectionString = connectionString;
-                    var test = identityContext.Database.GetDbConnection().ConnectionString;
                     var scopedRoleManager = claimScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
                     var role = await scopedRoleManager.FindByNameAsync(roleClaims.Key);
                     if (role != null)
                     {
                         foreach (var claim in roleClaims.Value)
                         {
-                            // Check if the claim already exists for the role
+                            // Check if the claim already exists for the role.
                             var existingClaims = await scopedRoleManager.GetClaimsAsync(role);
                             if (!existingClaims.Any(c => c.Type == claim.Type && c.Value == claim.Value))
                             {
@@ -164,12 +168,12 @@ namespace Utah.Udot.Atspm.Data.Configuration.Identity
                     {
                         logger.LogWarning($"Role '{roleClaims.Key}' not found while adding claims.");
                     }
-                    //}
                 }
 
                 logger.LogInformation("Role and claim seeding process completed.");
             }
         }
+
     }
 }
 
