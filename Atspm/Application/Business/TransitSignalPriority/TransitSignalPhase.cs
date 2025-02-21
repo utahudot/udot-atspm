d47b43262e1e782913885b1ff5d0308f9aa413d2
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2025-02-21 13:00:26 -0700

    New Cycle function

--- Atspm/Application/Business/TransitSignalPriority/TransitSignalPhase.cs
+++ Atspm/Application/Business/TransitSignalPriority/TransitSignalPhase.cs
@@ -3,6 +3,9 @@
     public class TransitSignalPhase
     {
         public int PhaseNumber { get; set; }
+        public double MinGreen { get; set; }
+        public double Yellow { get; set; }
+        public double RedClearance { get; set; }
         public double MinTime { get; set; }
         public double ProgrammedSplit { get; set; }
         public double PercentileSplit85th { get; set; }
@@ -11,5 +14,16 @@
         public double PercentMaxOutsForceOffs { get; set; }
         public double PercentGapOuts { get; set; }
         public double PercentSkips { get; set; }
+        public double? RecommendedTSPMax { get; set; }
+        public double SkipsGreaterThan70TSPMax { get; set; }
+        public double ForceOffsGreaterThan40TSPMax { get; set; }
+        public double ForceOffsGreaterThan60TSPMax { get; set; }
+        public double ForceOffsGreaterThan80TSPMax { get; set; }
+        public int MaxReduction { get; set; }
+        public int MaxExtension { get; set; }
+        public int PriorityMin { get; set; }
+        public int PriorityMax { get; set; }
+        public string Notes { get; set; }
+
     }
 }
