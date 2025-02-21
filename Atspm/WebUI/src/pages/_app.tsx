790ab72f80c783c3d33a904bcf64ffd1aa783454
Morgan Stanley <mstanley@avenueconsultants.com>
2025-02-21 14:13:31 -0700

    Revert "test branch"
    This reverts commit de638e32a4b6aec8707542883534e477b31d961d.

--- Atspm/WebUI/src/pages/_app.tsx
+++ Atspm/WebUI/src/pages/_app.tsx
@@ -20,8 +20,6 @@ export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
   const [isAxiosInitialized, setIsAxiosInitialized] = useState(false)
 
-  console.log('test')
-
   useEffect(() => {
     const initialize = async () => {
       await initializeAxiosInstances()
