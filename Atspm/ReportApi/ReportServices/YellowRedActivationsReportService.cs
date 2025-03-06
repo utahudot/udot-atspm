4da7a6fa03edaf3ba0badddc50f5278bc8a72c88
dkimball <dkimball@avenueconsultants.com>
2025-03-06 14:14:21 -0700

    AVE-2501 Split Monitor Chart Headers are showing weird phase names

--- Atspm/ReportApi/ReportServices/YellowRedActivationsReportService.cs
+++ Atspm/ReportApi/ReportServices/YellowRedActivationsReportService.cs
@@ -16,6 +16,7 @@
 #endregion
 
 using Utah.Udot.Atspm.Business.YellowRedActivations;
+using Utah.Udot.Atspm.Data.Enums;
 using Utah.Udot.Atspm.Data.Models.EventLogModels;
 
 namespace Utah.Udot.Atspm.ReportApi.ReportServices
@@ -119,7 +120,7 @@ namespace Utah.Udot.Atspm.ReportApi.ReportServices
                 detectorEvents,
                 planEvents);
             viewModel.LocationDescription = LocationDescription;
-            viewModel.ApproachDescription = phaseDetail.GetApproachDescription();
+            viewModel.ApproachDescription = phaseDetail.GetApproachDescriptionWithMovements(DetectionTypes.LLS);
             return viewModel;
         }
 
