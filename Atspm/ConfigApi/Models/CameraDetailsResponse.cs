e467da99d3d489857894b1e8672be63c9af7b21c
kford <kford@avenueconsultants.com>
2025-04-02 11:49:04 -0600

    AVE-2370
    Create a dropdown of cameras
    
    Modify the endpoint to allow to only have the Ids returned.

---
+++ Atspm/ConfigApi/Models/CameraDetailsResponse.cs
@@ -0,0 +1,33 @@
+﻿#region license
+// Copyright 2025 Utah Departement of Transportation
+// for ConfigApi - Utah.Udot.ATSPM.ConfigApi.Models/DeviceGroup.cs
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+namespace Utah.Udot.ATSPM.ConfigApi.Models
+{
+    public class CameraDetailsInfo
+    {
+        public string Index { get; set; }
+        public string DeviceId { get; set; }
+        public string Online { get; set; }
+        public string IndexUrl { get; set; }
+        public string IdUrl { get; set; }
+    }
+
+    public class CameraDetailsResponse
+    {
+        public List<CameraDetailsInfo> Cameras { get; set; } = new();
+    }
+}
