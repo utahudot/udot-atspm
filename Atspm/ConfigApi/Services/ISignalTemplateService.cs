270320098155ff384784d577f0a0543995519be9
kford <kford@avenueconsultants.com>
2025-06-05 11:29:00 -0600

    AVE-2893 - Gilbert branch -> new repo (bug fixes)
    Another one was missing so I added it

---
+++ Atspm/ConfigApi/Services/ISignalTemplateService.cs
@@ -0,0 +1,9 @@
+﻿using Utah.Udot.Atspm.ValueObjects;
+
+namespace Utah.Udot.Atspm.ConfigApi.Services
+{
+    public interface ISignalTemplateService
+    {
+        TemplateLocationModifiedDto SyncNewLocationDetectorsAndApproaches(int locationId);
+    }
+}
