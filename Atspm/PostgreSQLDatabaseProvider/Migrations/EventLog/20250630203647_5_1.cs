f18ae5722ea768995a62e9a53651551cf343d4ae
Christian Baker <christianbaker@utah.gov>
2025-07-03 06:49:07 -0600

    Fixed CompressedEventLogSpecification

---
+++ Atspm/PostgreSQLDatabaseProvider/Migrations/EventLog/20250630203647_5_1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Utah.Udot.ATSPM.PostgreSQLDatabaseProvider.Migrations.EventLog
+{
+    /// <inheritdoc />
+    public partial class _5_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CompressedEvents",
+                table: "CompressedEvents");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CompressedEvents",
+                table: "CompressedEvents",
+                columns: new[] { "LocationIdentifier", "DeviceId", "DataType", "Start", "End" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CompressedEvents",
+                table: "CompressedEvents");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CompressedEvents",
+                table: "CompressedEvents",
+                columns: new[] { "LocationIdentifier", "DeviceId", "Start", "End" });
+        }
+    }
+}
