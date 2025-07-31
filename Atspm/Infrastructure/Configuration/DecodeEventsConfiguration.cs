4a37e97285e14b258214845ebcc5f0c257efd2aa
Christian Baker <christianbaker@utah.gov>
2025-07-31 13:27:14 -0600

    Add DecodeEventsCommand for event log processing
    This commit introduces a new `DecodeEventsCommand` in the `EventLogCommands.cs` file, enabling the import and decoding of event logs. A corresponding class `DecodeEventsCommand` is created to handle command options and service registration.
    
    Additionally, a new configuration class `DecodeEventsConfiguration` is added to specify the path for event logs. The `DecodeEventLogHostedService` class is introduced to process the logs, grouping them by device identifiers and sending them to a decoding workflow.
    
    A new workflow class `DecodeEventLogWorkflow` is also added, defining the steps for decoding event logs. License comments have been included in the new files to comply with copyright requirements.

---
+++ Atspm/Infrastructure/Configuration/DecodeEventsConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Utah.Udot.Atspm.Infrastructure.Configuration
+{
+    public class DecodeEventsConfiguration
+    {
+        /// <summary>
+        /// Path to local directory where event logs are saved
+        /// </summary>
+        public string Path { get; set; } = System.IO.Path.GetTempPath();
+    }
+}
