335f131b49cbe0208abc628c4858195ba6d19fef
kford <kford@avenueconsultants.com>
2026-03-16 16:29:26 -0600

    Adding the column in the database.

---
+++ Atspm/PostgreSQLDatabaseProvider/Migrations/Config/20260316222003_5.2.1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Utah.Udot.ATSPM.PostgreSQLDatabaseProvider.Migrations.Config
+{
+    /// <inheritdoc />
+    public partial class _521 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Key",
+                table: "WatchDogLogEvents");
+
+            migrationBuilder.DropColumn(
+                name: "Key",
+                table: "WatchDogIgnoreEvents");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Key",
+                table: "WatchDogLogEvents",
+                type: "text",
+                unicode: false,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Key",
+                table: "WatchDogIgnoreEvents",
+                type: "text",
+                unicode: false,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Key",
+                table: "WatchDogLogEvents");
+
+            migrationBuilder.DropColumn(
+                name: "Key",
+                table: "WatchDogIgnoreEvents");
+        }
+    }
+}
