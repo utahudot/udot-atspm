fe9ec49606495db1d91eb400467ba2360d93c179
kford <kford@avenueconsultants.com>
2026-04-07 16:42:28 -0600

    AVE-3520 - Export the Aggregations and place them in our database tool
    adding in a upload for the aggregations. This is still a work in progress so it will take some more work. but for now it is just there and is to test the certs on the cloud.

--- Atspm/DataApi/Controllers/AggregationController.cs
+++ Atspm/DataApi/Controllers/AggregationController.cs
@@ -17,6 +17,7 @@
 
 using Asp.Versioning;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Utah.Udot.Atspm.Repositories.ConfigurationRepositories;
 using Utah.Udot.ATSPM.DataApi.Controllers;
 
@@ -33,5 +34,100 @@ namespace Utah.Udot.Atspm.DataApi.Controllers
         : DataControllerBase<CompressedAggregationBase, AggregationModelBase>(repository, locations, log)
     {
         private readonly IAggregationRepository _repository = repository;
+
+        /// <summary>
+        /// Uploads aggregated data to the database
+        /// </summary>
+        /// <param name="request">The upload request containing aggregation data</param>
+        /// <returns>Result of the upload operation</returns>
+        [HttpPost("UploadAggregations")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(UploadResult), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [RequestSizeLimit(long.MaxValue)]
+        [RequestFormLimits(MultipartBodyLengthLimit = long.MaxValue)]
+        [DisableRequestSizeLimit]
+        public async Task<IActionResult> UploadAggregations([FromBody] UploadAggregationsRequest request)
+        {
+            try
+            {
+                var results = new UploadResult();
+
+                foreach (var aggregationData in request.Aggregations)
+                {
+                    // Note: You'll need to implement the actual save logic based on your repository
+                    // This is a placeholder - adjust according to your data layer
+                    _log.LogInformation($"Processing upload for {aggregationData.Type} with {aggregationData.RecordCount} records");
+
+                    // TODO: Deserialize and save the data
+                    //var data = JsonSerializer.Deserialize<List<AggregationModelBase>>(aggregationData.JsonData);
+                    // await _repository.SaveAggregationsAsync(data);
+
+                    results.ProcessedTypes.Add(aggregationData.Type);
+                    results.TotalRecordsProcessed += aggregationData.RecordCount;
+                }
+
+                _log.LogInformation($"Upload completed. Processed {results.TotalRecordsProcessed} records across {results.ProcessedTypes.Count} types");
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                _log.LogError(ex, "Upload failed");
+                return BadRequest(new ProblemDetails
+                {
+                    Title = "Upload failed",
+                    Detail = ex.Message,
+                    Status = StatusCodes.Status400BadRequest
+                });
+            }
+        }
+    }
+
+    /// <summary>
+    /// Request model for uploading aggregations
+    /// </summary>
+    public class UploadAggregationsRequest
+    {
+        /// <summary>
+        /// List of aggregation data to upload
+        /// </summary>
+        public List<AggregationUploadData> Aggregations { get; set; } = new();
+    }
+
+    /// <summary>
+    /// Individual aggregation data for upload
+    /// </summary>
+    public class AggregationUploadData
+    {
+        /// <summary>
+        /// The aggregation type name
+        /// </summary>
+        public string Type { get; set; } = string.Empty;
+
+        /// <summary>
+        /// JSON data as string
+        /// </summary>
+        public string JsonData { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Number of records (for logging)
+        /// </summary>
+        public int RecordCount { get; set; }
+    }
+
+    /// <summary>
+    /// Result of upload operation
+    /// </summary>
+    public class UploadResult
+    {
+        /// <summary>
+        /// Types that were processed
+        /// </summary>
+        public List<string> ProcessedTypes { get; set; } = new();
+
+        /// <summary>
+        /// Total records processed
+        /// </summary>
+        public int TotalRecordsProcessed { get; set; }
     }
 }
