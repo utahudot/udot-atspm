5f42e95784ff5f193ba9ae26d68e9de6c68dd194
Morgan Stanley <mstanley@avenueconsultants.com>
2026-05-21 12:22:00 -0600

    Add version status to about page

--- Atspm/WebUI/src/api/config/index.ts
+++ Atspm/WebUI/src/api/config/index.ts
@@ -1,19 +1,3 @@
-// #region license
-// Copyright 2026 Utah Departement of Transportation
-// for WebUI - index.ts
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//http://www.apache.org/licenses/LICENSE-2.
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// #endregion
 export * from './approach/approach';
 export * from './config-api.schemas';
 export * from './area/area';
@@ -37,6 +21,6 @@ export * from './region/region';
 export * from './route/route';
 export * from './route-distance/route-distance';
 export * from './route-location/route-location';
-export * from './version-history/version-history';
-export * from './watch-dog-ignore-event/watch-dog-ignore-event';
 export * from './usage-entry/usage-entry';
+export * from './version/version';
+export * from './watch-dog-ignore-event/watch-dog-ignore-event';
