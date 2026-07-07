fdbf61057d3c11a11e997faea5cb48a115b0e1ba
Christian Baker <christianbaker@utah.gov>
2026-07-07 10:45:35 -0600

    updated headers

--- Atspm/Infrastructure/LogMessages/WatchDogPmLogMessages.cs
+++ Atspm/Infrastructure/LogMessages/WatchDogPmLogMessages.cs
@@ -1,13 +1,13 @@
 #region license
 // Copyright 2026 Utah Departement of Transportation
 // for Infrastructure - Utah.Udot.Atspm.Infrastructure.LogMessages/WatchDogPmLogMessages.cs
-//
+// 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 // http://www.apache.org/licenses/LICENSE-2.
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
