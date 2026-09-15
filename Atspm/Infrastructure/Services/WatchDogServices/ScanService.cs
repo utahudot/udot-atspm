8db9a5dd76f4d48c5203e4e2ca99b87546fe6ba6
kford-avenueconsultants <kford@avenueconsultants.com>
2026-09-15 14:18:15 -0600

    Ave 3661   watch dog bugs (#64)
    * AVE-3661 - WatchDog bugs
    
    Adding in all Errors fix
    
    * Set the PM peeak start hour to 17 and the End hour to 18

--- Atspm/Infrastructure/Services/WatchDogServices/ScanService.cs
+++ Atspm/Infrastructure/Services/WatchDogServices/ScanService.cs
@@ -22,7 +22,6 @@ using Utah.Udot.Atspm.Business.Watchdog;
 using Utah.Udot.Atspm.Common;
 using Utah.Udot.Atspm.Data.Enums;
 using Utah.Udot.Atspm.Data.Models.IdentityModels;
-using Utah.Udot.Atspm.Infrastructure.LogMessages;
 using Utah.Udot.Atspm.Repositories;
 
 namespace Utah.Udot.ATSPM.Infrastructure.Services.WatchDogServices
@@ -129,7 +128,7 @@ namespace Utah.Udot.ATSPM.Infrastructure.Services.WatchDogServices
             var finalRecurring = new List<WatchDogLogEventWithCountAndDate>();
             var finalDayBefore = new List<WatchDogLogEvent>();
             // PM
-            if (emailOptions.EmailPmErrors)
+            if (emailOptions.EmailPmErrors || emailOptions.EmailAllErrors)
             {
                 var scanDateForErrors = emailOptions.PmScanDate;
                 //since they can be on different days we need to check that the pm scan ran not just the day exists
@@ -168,7 +167,7 @@ namespace Utah.Udot.ATSPM.Infrastructure.Services.WatchDogServices
             }
 
             // AM
-            if (emailOptions.EmailAmErrors)
+            if (emailOptions.EmailAmErrors || emailOptions.EmailAllErrors)
             {
                 var scanDateForErrors = emailOptions.AmScanDate;
                 //since they can be on different days we need to check that the am scan ran not just the day exists
