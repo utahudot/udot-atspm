db2b9e92fe6b73dfaa464b6f068452e41bdffee8
dloweAvenueConsultants <dlowe@avenueconsultants.com>
2026-09-23 12:47:45 -0600

    Remove EventLogUtility changes from plan service branch

--- Atspm/EventLogUtility/Program.cs
+++ Atspm/EventLogUtility/Program.cs
@@ -16,7 +16,6 @@
 #endregion
 
 using Google.Cloud.Diagnostics.Common;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.CommandLine.Builder;
@@ -43,13 +42,6 @@ cmdBuilder.UseHost(a =>
     return Host.CreateDefaultBuilder(a)
     //.UseConsoleLifetime()
     .ApplyVolumeConfiguration()
-
-    .ConfigureAppConfiguration((h, c) =>
-    {
-        c.AddUserSecrets<Program>(optional: true); // Load secrets first
-        c.AddCommandLine(args);                    // Override with command-line args
-
-    })
     .ConfigureLogging((h, l) =>
     {
         if (OperatingSystem.IsWindows())
