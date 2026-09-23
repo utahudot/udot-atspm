3cf087e921902bdaf66b6bc593e393652ef12327
Christian Baker <christianbaker@utah.gov>
2026-09-23 14:01:03 -0600

    Removed eventlogutility program changes

--- Atspm/EventLogUtility/Program.cs
+++ Atspm/EventLogUtility/Program.cs
@@ -23,16 +23,17 @@ using Microsoft.Extensions.Logging;
 using System.CommandLine.Builder;
 using System.CommandLine.Hosting;
 using System.CommandLine.Parsing;
+using System.Diagnostics;
 using Utah.Udot.Atspm.EventLogUtility.Commands;
 using Utah.Udot.Atspm.Infrastructure.Extensions;
 
 //gitactions: IIII
 
-//if (OperatingSystem.IsWindows())
-//{
-//    if (!EventLog.SourceExists("Atspm"))
-//        EventLog.CreateEventSource(AppDomain.CurrentDomain.FriendlyName, "Atspm");
-//}
+if (OperatingSystem.IsWindows())
+{
+    if (!EventLog.SourceExists("Atspm"))
+        EventLog.CreateEventSource(AppDomain.CurrentDomain.FriendlyName, "Atspm");
+}
 
 var rootCmd = new EventLogCommands();
 var cmdBuilder = new CommandLineBuilder(rootCmd);
